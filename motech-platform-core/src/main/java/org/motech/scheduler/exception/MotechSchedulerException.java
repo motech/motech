1a2a03e42e8e0a5dc528e56462eed5762f007573
Igor <iopushnyev@2paths.com>
2011-02-17 17:15:21 -0800

    MOTECH-238 Added MotechSchedulerException classes

---
+++ motech-platform-core/src/main/java/org/motech/scheduler/exception/MotechSchedulerException.java
@@ -0,0 +1,26 @@
+package org.motech.scheduler.exception;
+
+/**
+ *
+ * User: Igor (iopushnyev@2paths.com)
+ * Date: 17/02/11
+ * Time: 4:20 PM
+ *
+ */
+public class MotechSchedulerException extends RuntimeException{
+
+    public MotechSchedulerException() {
+    }
+
+    public MotechSchedulerException(String message) {
+        super(message);
+    }
+
+    public MotechSchedulerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MotechSchedulerException(Throwable cause) {
+        super(cause);
+    }
+}
