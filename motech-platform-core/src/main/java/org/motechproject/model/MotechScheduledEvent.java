c400cbf3b50f998d25ba5db67bf7d732d1af397f
Igor <iopushnyev@2paths.com>
2011-02-18 14:37:59 -0800

    MOTECH-238 added input data validation

--- motech-platform-core/src/main/java/org/motechproject/model/MotechScheduledEvent.java
+++ motech-platform-core/src/main/java/org/motechproject/model/MotechScheduledEvent.java
@@ -8,12 +8,16 @@ import java.util.Map;
  * Motech Scheduled Event data carrier class,
  * Instance of this class with event specific data will be send by Motech Scheduler when a scheduled event is fired
  *
+ * This class is immutable
+ *
  * User:Igor (iopushnyev@2paths.com)
  * Date: 16/02/11
  * Time: 1:43 PM
  *
  */
-public class MotechScheduledEvent {
+public final class MotechScheduledEvent {
+
+    public static final String EVENT_TYPE_KEY_NAME = "eventType";
 
     private String jobId;
     private String eventType;
@@ -23,9 +27,20 @@ public class MotechScheduledEvent {
      * Constructor
      * @param jobId - ID of the scheduled job that generated this event.
      * @param eventType - event type: Pill Reminder, Appointment Reminder ...
-     * @param parameters - a Map<String, Object> of additional parameters stored as job details
+     * @param parameters - a Map<String, Object> of additional parameters
+     *
+     * @throws IllegalArgumentException if given jobId or entityType is null
      */
     public MotechScheduledEvent(String jobId, String eventType, HashMap<String, Object> parameters) {
+
+        if (jobId == null) {
+            throw new IllegalArgumentException("jobId can not be null");
+        }
+
+        if (eventType == null) {
+            throw new IllegalArgumentException("eventType can not be null");
+        }
+
         this.jobId = jobId;
         this.eventType = eventType;
         this.parameters = parameters;
@@ -40,6 +55,9 @@ public class MotechScheduledEvent {
     }
 
     public Map<String, Object> getParameters() {
+        if (parameters == null ) {
+            return Collections.emptyMap();
+        }
         return Collections.unmodifiableMap(parameters);
     }
 }
