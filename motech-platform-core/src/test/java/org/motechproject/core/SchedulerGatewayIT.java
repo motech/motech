09a8da03950b396e582bd9f4c34f1db9e37cf42c
Rob LaRubbio <rob@pugdogdev.com>
2011-02-24 12:31:19 -0800

    Add names to modules. Include the failsafe plugin and rename the schedule test. Add the motech archiva repo.

--- motech-platform-core/src/test/java/org/motechproject/core/SchedulerGatewayIT.java
+++ motech-platform-core/src/test/java/org/motechproject/core/SchedulerGatewayIT.java
@@ -37,8 +37,13 @@ import org.motechproject.model.MotechScheduledEvent;
 import org.motechproject.model.RunOnceSchedulableJob;
 import org.motechproject.model.SchedulableJob;
 import org.motechproject.scheduler.MotechSchedulerGateway;
-import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 
@@ -49,38 +54,25 @@ import java.util.Date;
  * Time: 11:00 AM
  *
  */
-public class SchedulerGatewayIntegrationTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"/integrationCommon.xml",
+                                 "/schedulerOutboundChannelAdapter.xml"})
+public class SchedulerGatewayIT {
 
-     public static void main(String[] args) {
+    @Autowired
+    private MotechSchedulerGateway motechSchedulerGateway;
 
-
-
-        AbstractApplicationContext ctx
-                = new ClassPathXmlApplicationContext(new String[]{"/integrationCommon.xml",
-                                                                  "/schedulerOutboundChannelAdapter.xml"});
-
-
-        MotechSchedulerGateway motechSchedulerGateway = (MotechSchedulerGateway) ctx.getBean("motechSchedulerGateway");
-
-          MotechScheduledEvent scheduledEvent = new MotechScheduledEvent("test_1", "testEvent", null);
+    @Test
+    public void testMotechScheduler() {
+        MotechScheduledEvent scheduledEvent = new MotechScheduledEvent("test_1", "testEvent", null);
         SchedulableJob schedulableJob = new SchedulableJob(scheduledEvent, "0/5 0 * * * ?");
 
-         // In order to run the test uncomment the following code
+        motechSchedulerGateway.scheduleJob(schedulableJob);
 
-        /* motechSchedulerGateway.scheduleJob(schedulableJob);
+        motechSchedulerGateway.unscheduleJob("test_1");
+        
+        RunOnceSchedulableJob runOnceSchedulableJob = new RunOnceSchedulableJob(scheduledEvent, new Date((new Date().getTime()+5000)));
 
-         motechSchedulerGateway.unscheduleJob("test_1");
-
-         RunOnceSchedulableJob runOnceSchedulableJob = new RunOnceSchedulableJob(scheduledEvent, new Date((new Date().getTime()+5000)));
-
-         motechSchedulerGateway.scheduleRunOnceJob(runOnceSchedulableJob);*/
-    }
-
-    /**
-     * This is a fake test method to make CI happy
-     */
-    @Test
-    public void fakeTest() {
-
+        motechSchedulerGateway.scheduleRunOnceJob(runOnceSchedulableJob);
     }
 }
