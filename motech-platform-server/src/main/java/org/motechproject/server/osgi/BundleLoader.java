a581136c77b2e601bf6a0da5da150145a3e08786
Ricky <wlricky@hotmail.com>
2011-02-24 09:24:39 -0800

    MOTECH-260 added a custom loader for log4j in OSGi

---
+++ motech-platform-server/src/main/java/org/motechproject/server/osgi/BundleLoader.java
@@ -0,0 +1,18 @@
+package org.motechproject.server.osgi;
+
+import org.osgi.framework.Bundle;
+
+/**
+ * Interface for custom bundle loading processes
+ * 
+ * @author Ricky Wang
+ */
+public interface BundleLoader {
+	
+	/**
+	 * @param bundle
+	 * @throws Exception
+	 */
+	public void loadBundle(Bundle bundle) throws Exception;
+	
+}
---
+++ motech-platform-server/src/main/java/org/motechproject/server/osgi/Log4JBundleLoader.java
@@ -0,0 +1,61 @@
+package org.motechproject.server.osgi;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.log4j.xml.DOMConfigurator;
+import org.osgi.framework.Bundle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ * Custom log4j configuration loader
+ * 
+ * Merge bundle specific configurations to the existing if a bundle contains log4j.xml
+ * 
+ * @author Ricky Wang
+ */
+public class Log4JBundleLoader implements BundleLoader {
+
+	private static Logger logger = LoggerFactory.getLogger(Log4JBundleLoader.class);
+	
+	//default log4j configuration file
+	private String log4jConf = "log4j.xml";
+
+	@Override
+	public void loadBundle(Bundle bundle) throws Exception {
+		URL log4jUrl = bundle.getResource(log4jConf);
+		if (log4jUrl != null) {
+			URLConnection conn = log4jUrl.openConnection();
+			InputStream log4jStream = conn.getInputStream();
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			db.setEntityResolver(new EntityResolver() {
+				public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
+					return new InputSource(new StringReader(""));
+				}
+			});
+
+			Document log4jDoc = db.parse(log4jStream);
+			DOMConfigurator.configure(log4jDoc.getDocumentElement());
+
+			log4jStream.close();
+			logger.debug("Added log4j configuration for [" + bundle.getLocation() + "]");
+		}
+	}
+
+	public void setLog4jConf(String log4jConf) {
+		this.log4jConf = log4jConf;
+	}
+
+}
