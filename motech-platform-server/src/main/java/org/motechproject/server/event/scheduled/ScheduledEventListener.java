9a72092563b1a96e2bf3a008b7330e28efdf2c13
Ricky <wlricky@hotmail.com>
2011-03-04 16:04:10 -0800

    MOTECH-260 moved event listener related classes into the server api module

--- motech-platform-server/src/main/java/org/motechproject/server/event/scheduled/ScheduledEventListener.java
+++
@@ -1,7 +0,0 @@
-package org.motechproject.server.event.scheduled;
-
-import org.motechproject.model.MotechScheduledEvent;
-
-public interface ScheduledEventListener {
-    public void handle(MotechScheduledEvent event);
-}
