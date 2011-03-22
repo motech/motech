485511a20884842cb593b1896b393ad96b1183ab
yyonkov <yyonkov@2paths.com>
2011-03-22 11:49:52 -0700

    fix broken test

--- motech-appointment-reminder/src/test/java/org/motechproject/server/ScheduleAppointmentReminderTest.java
+++ motech-appointment-reminder/src/test/java/org/motechproject/server/ScheduleAppointmentReminderTest.java
@@ -1,25 +1,46 @@
 package org.motechproject.server;
 
+import static org.junit.Assert.assertNotNull;
+
 import java.util.Arrays;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.motechproject.dao.PatientDao;
+import org.motechproject.model.Patient;
 import org.motechproject.server.event.EventListenerRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "/testApplicationContext.xml" })
+@ContextConfiguration(locations = { "/ApplicationContext.xml", "/testApplicationContext.xml" })
 public class ScheduleAppointmentReminderTest {
 	@Autowired
 	private ScheduleAppointmentReminderHandler handler;
 	@Autowired
 	private EventListenerRegistry registry;
+	@Autowired
+	private PatientDao patientDao;
 	@Test
 	public void testSubscription() {
 		registry.registerListener(handler, Arrays.asList(ScheduleAppointmentReminderEventType.getInstance()));
 		System.out.print(handler.getIdentifier());
 	}
+//    @Test
+    public void testGetAll() throws Exception {
 
+        String id = "1234";
+
+        Patient patient = new Patient();
+        patient.setId(id);
+        patientDao.add(patient);
+
+        patient = patientDao.get(id);
+        System.out.print(patient);
+        assertNotNull(patient);
+        
+        patientDao.remove(patient);
+
+    }
 }
