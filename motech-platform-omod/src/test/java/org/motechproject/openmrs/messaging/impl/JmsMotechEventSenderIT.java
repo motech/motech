bed40abb60450a2fb8eb88f58800e8e2ddd7465a
Ricky <wlricky@hotmail.com>
2011-03-25 15:53:37 -0700

    MOTECH-278 added JMX MBean for JMS testing

--- motech-platform-omod/src/test/java/org/motechproject/openmrs/messaging/impl/JmsMotechEventSenderIT.java
+++ motech-platform-omod/src/test/java/org/motechproject/openmrs/messaging/impl/JmsMotechEventSenderIT.java
@@ -1,8 +1,13 @@
 package org.motechproject.openmrs.messaging.impl;
 
+import static junit.framework.Assert.assertEquals;
+
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.activemq.broker.jmx.QueueViewMBean;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.motechproject.model.MotechEvent;
 import org.motechproject.openmrs.messaging.MotechEventSender;
@@ -13,7 +18,20 @@ public class JmsMotechEventSenderIT extends BaseModuleContextSensitiveTest {
 
 	@Autowired
 	private MotechEventSender motechEventSender;
+	
+	@Autowired
+	private QueueViewMBean queueMBean;
 
+    @Before
+    public void setup() throws Exception {
+    	queueMBean.purge();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    	queueMBean.purge();
+    }
+	
 	@Test
 	public void testSend() throws Exception {
 		
@@ -22,8 +40,11 @@ public class JmsMotechEventSenderIT extends BaseModuleContextSensitiveTest {
 		
 		MotechEvent motechEvent = new MotechEvent("123", "test", parameters);
 		
+		assertEquals(0, queueMBean.getQueueSize());
+
 		motechEventSender.send(motechEvent);
 		
-		//TODO: add some assertions
+		Thread.sleep(1000);
+		assertEquals(1, queueMBean.getQueueSize());
 	}
 }
