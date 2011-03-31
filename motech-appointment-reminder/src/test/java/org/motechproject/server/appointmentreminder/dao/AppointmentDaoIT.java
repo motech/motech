5f5ed62f45ea640acd41bd4b6c221af309e8097f
yyonkov <yyonkov@2paths.com>
2011-03-30 17:12:26 -0700

    MOTECH-300: Refactored Appointment domain object and moved it to appointment-reminder module, added AppointmentDao and integration DAO CRUD + findsByPatientId test.

---
+++ motech-appointment-reminder/src/test/java/org/motechproject/server/appointmentreminder/dao/AppointmentDaoIT.java
@@ -0,0 +1,77 @@
+/**
+ * MOTECH PLATFORM OPENSOURCE LICENSE AGREEMENT
+ *
+ * Copyright (c) 2010-11 The Trustees of Columbia University in the City of
+ * New York and Grameen Foundation USA.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of Grameen Foundation USA, Columbia University, or
+ * their respective contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY GRAMEEN FOUNDATION USA, COLUMBIA UNIVERSITY
+ * AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL GRAMEEN FOUNDATION
+ * USA, COLUMBIA UNIVERSITY OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.motechproject.server.appointmentreminder.dao;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.motechproject.server.appointmentreminder.model.Appointment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Appointment DAO test
+ * @author yyonkov
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"/testApplicationContext.xml"})
+public class AppointmentDaoIT {
+	@Autowired
+	private AppointmentDao dao;
+
+	@Test
+	public void testCRUDandFindByParrentId() {
+		String appId = "000111"; 
+		String patientId = "0001";
+		Appointment app = new Appointment();
+		Appointment app1 = new Appointment();
+		app.setId(appId);
+		app1.setId("xxx");
+		dao.add(app);
+		dao.add(app1);
+		app = dao.get(appId);
+		assertNotNull(app);
+		app.setPatientArrived(true);
+		app.setPatientId(patientId);
+		dao.update(app);
+		List<Appointment> apps = dao.findByPatientId(patientId);
+		assertNotNull(apps);
+		System.out.print(apps.size());
+		assertTrue(apps.size()==1);
+		dao.remove(app);
+		dao.remove(app1);
+	}
+}
