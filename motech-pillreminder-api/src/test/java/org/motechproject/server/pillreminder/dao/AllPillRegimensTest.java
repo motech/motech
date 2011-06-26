0d0edababcf4c89be43261e174618ca58d03ea4c
vijayaraj <vijayaraj@yahoo.com>
2011-06-26 20:43:47 +0530

    #21 Added AllRegimens unit test as IT cannot be run in build cycle <vijay>

---
+++ motech-pillreminder-api/src/test/java/org/motechproject/server/pillreminder/dao/AllPillRegimensTest.java
@@ -0,0 +1,33 @@
+package org.motechproject.server.pillreminder.dao;
+
+import org.ektorp.CouchDbConnector;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.motechproject.server.pillreminder.domain.PillRegimen;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.motechproject.server.pillreminder.util.TestUtil.newDate;
+
+public class AllPillRegimensTest {
+
+    @Mock
+    CouchDbConnector couchDbConnector;
+    private AllPillRegimens allPillRegimens;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+        allPillRegimens = new AllPillRegimens(couchDbConnector);
+    }
+
+    @Test
+    public void shouldAddPillRegimen() {
+        PillRegimen pillRegimen = new PillRegimen("123", newDate(2011, 1, 1), newDate(2011, 2, 1), null);
+        allPillRegimens.add(pillRegimen);
+        verify(couchDbConnector).create(pillRegimen);
+    }
+
+
+}
