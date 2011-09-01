47185fbd7925684b2e1b2a22e9ca1636cd5696c4
Rob LaRubbio <rob@larubbio.org>
2011-09-01 13:53:58 -0700

    Get some stragglers that should have been moved to the new module but were not.

--- voxeo/src/test/java/org/motechproject/server/voxeo/web/CcmlControllerTest.java
+++ voxeo/src/test/java/org/motechproject/server/voxeo/web/CcmlControllerTest.java
@@ -29,7 +29,7 @@
  * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
  * OF SUCH DAMAGE.
  */
-package org.motechproject.server.tama.web;
+package org.motechproject.server.voxeo.web;
 
 import org.junit.Assert;
 import org.junit.Before;
