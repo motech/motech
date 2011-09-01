d9e9e33eee8b06071bf6df09ca96d926ed7ce28b
Rob LaRubbio <rob@larubbio.org>
2011-09-01 13:11:51 -0700

    Some intitial work at implementing the Voxeo IVR module.

---
+++ motech-platform-ivr/src/main/java/org/motechproject/server/service/ivr/voxeo/domain/PhoneCall.java
@@ -0,0 +1,123 @@
+package org.motechproject.server.service.ivr.voxeo.domain;
+
+import org.codehaus.jackson.annotate.JsonProperty;
+import org.ektorp.support.TypeDiscriminator;
+import org.motechproject.model.MotechAuditableDataObject;
+import org.motechproject.server.service.ivr.CallRequest;
+import org.motechproject.server.service.ivr.CallDetailRecord;
+
+import java.util.Date;
+import java.util.Set;
+
+@TypeDiscriminator("doc.type === 'CALL_DETAIL_RECORD'")
+public class PhoneCall extends MotechAuditableDataObject {
+    public enum Direction {
+	    INCOMING, OUTGOING
+    }
+
+    @JsonProperty("type")
+    private String type = "CALL_DETAIL_RECORD";
+    private String sessionId;
+    private Date startDate;
+    private Date endDate;
+    private Date answerDate;
+    private Integer duration;
+    private CallDetailRecord.Disposition disposition;
+    private Direction direction;
+    private CallRequest callRequest;
+    private Set<PhoneCallEvent> events;
+
+    public PhoneCall() {
+    }
+
+    public PhoneCall(CallRequest callRequest) {
+        this.callRequest = callRequest;
+    }
+
+    public String getSessionId()
+    {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId)
+    {
+        this.sessionId = sessionId;
+    }
+
+    public Date getStartDate()
+    {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate)
+    {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate()
+    {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate)
+    {
+        this.endDate = endDate;
+    }
+
+    public Date getAnswerDate()
+    {
+        return answerDate;
+    }
+
+    public void setAnswerDate(Date answerDate)
+    {
+        this.answerDate = answerDate;
+    }
+
+    public Integer getDuration()
+    {
+        return new Integer((int) ((endDate.getTime() - startDate.getTime()) / 1000));
+    }
+
+    public CallRequest getCallRequest()
+    {
+        return callRequest;
+    }
+
+    public void setCallRequest(CallRequest callRequest)
+    {
+        this.callRequest = callRequest;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public CallDetailRecord.Disposition getDisposition()
+    {
+        return disposition;
+    }
+
+    public void setDisposition(CallDetailRecord.Disposition disposition)
+    {
+        this.disposition = disposition;
+    }
+
+    public Direction getDirection()
+    {
+        return direction;
+    }
+
+    public void setDirection(Direction direction)
+    {
+        this.direction = direction;
+    }
+
+    public void addEvent(PhoneCallEvent phoneCallEvent) {
+        events.add(phoneCallEvent);
+    }
+}
