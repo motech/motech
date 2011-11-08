7b51549f6cb2d69380dba3aa2a3e49159bb0ddb9
Geet <geetchan@thoughtworks.com>
2011-11-08 12:26:00 +0530

    Geet/Khaarthi Added a method to MRSUSerAdaptor

--- motech-mrs-api/src/main/java/org/motechproject/mrs/services/MRSUserAdaptor.java
+++ motech-mrs-api/src/main/java/org/motechproject/mrs/services/MRSUserAdaptor.java
@@ -2,8 +2,10 @@ package org.motechproject.mrs.services;
 
 import org.motechproject.mrs.exception.UserAlreadyExistsException;
 import org.motechproject.mrs.model.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public interface MRSUserAdaptor {
     public void changeCurrentUserPassword(String currentPassword, String newPassword);
     public String saveUser(User mrsUser) throws UserAlreadyExistsException;
+    public String setNewPasswordForUser(String emailID) throws UsernameNotFoundException;
 }
