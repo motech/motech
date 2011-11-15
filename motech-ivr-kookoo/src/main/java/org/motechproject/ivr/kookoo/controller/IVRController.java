cf9d9505ab30593fd95fe71f848a777ca36449a5
Alok Simha <lukasimha@gmail.com>
2011-11-15 17:41:51 +0530

    <Shruthi/Luka> #83: Introduced explicit Dial Event on SafeIVRController

--- motech-ivr-kookoo/src/main/java/org/motechproject/ivr/kookoo/controller/IVRController.java
+++ motech-ivr-kookoo/src/main/java/org/motechproject/ivr/kookoo/controller/IVRController.java
@@ -51,8 +51,6 @@ public class IVRController {
                     String url = AllIVRURLs.springTransferUrlToEmptyResponse();
                     logger.info(String.format("Transferring to %s", url));
                     return url;
-                case Dial:
-                    return AllIVRURLs.springTransferUrl(callFlowController.urlFor(ivrContext), IVREvent.GotDTMF.toString().toLowerCase());
                 case GotDTMF:
                     kookooCallDetailRecordsService.appendEvent(ivrContext.callDetailRecordId(), ivrEvent, ivrContext.userInput());
             }
