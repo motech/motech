e149fb3b686425725c2db40a70b870e079e90d61
Balaji Damodaran <damodaran.balaji@gmail.com>
2011-12-01 14:54:35 +0530

    Fixed password generation logic <Alex/Balaji>

--- motech-openmrs-api/src/main/java/org/motechproject/openmrs/model/Password.java
+++ motech-openmrs-api/src/main/java/org/motechproject/openmrs/model/Password.java
@@ -13,6 +13,13 @@ public class Password {
             'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
             'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8',
             '9', '0'};
+    private static final char[] PASSWORD_LOWER_CHARS = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+            'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+            'w', 'x', 'y', 'z'};
+
+    private static final char[] PASSWORD_UPPER_CHARS = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+            'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+            'W', 'X', 'Y', 'Z'};
     private static final char[] PASSWORD_NUMBER_CHARS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
     public Password(Integer length) {
@@ -21,10 +28,12 @@ public class Password {
 
     public String create() {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < length - 1; i++) {
+        for (int i = 0; i < length - 3; i++) {
             sb.append(PASSWORD_CHARS[((int) (Math.random() * PASSWORD_CHARS.length))]);
         }
         sb.append(PASSWORD_NUMBER_CHARS[((int) (Math.random() * PASSWORD_NUMBER_CHARS.length))]);
+        sb.append(PASSWORD_LOWER_CHARS[((int) (Math.random() * PASSWORD_NUMBER_CHARS.length))]);
+        sb.append(PASSWORD_UPPER_CHARS[((int) (Math.random() * PASSWORD_NUMBER_CHARS.length))]);
         logger.info("password: " + sb.toString());
         return sb.toString();
     }
