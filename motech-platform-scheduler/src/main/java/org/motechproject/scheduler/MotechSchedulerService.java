80c44b7ca7994f814eddd2a070ffd0f6a87f05ef
unknown <vsingh@.corporate.thoughtworks.com>
2011-12-14 10:41:06 +0530

    Vivek Singh | #938 | Creating primitive idempotent operations ...for CouchDb and Quartz. Removing duplicate code in MSSImpl.

--- motech-platform-scheduler/src/main/java/org/motechproject/scheduler/MotechSchedulerService.java
+++ motech-platform-scheduler/src/main/java/org/motechproject/scheduler/MotechSchedulerService.java
@@ -1,34 +1,3 @@
-/**
- * MOTECH PLATFORM OPENSOURCE LICENSE AGREEMENT
- *
- * Copyright (c) 2011 Grameen Foundation USA.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- *
- * 3. Neither the name of Grameen Foundation USA, nor its respective contributors
- * may be used to endorse or promote products derived from this software without
- * specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY GRAMEEN FOUNDATION USA AND ITS CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL GRAMEEN FOUNDATION USA OR ITS CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
- * OF SUCH DAMAGE.
- */
 package org.motechproject.scheduler;
 
 import org.motechproject.model.CronSchedulableJob;
@@ -57,6 +26,13 @@ public interface MotechSchedulerService {
     public void scheduleJob(CronSchedulableJob cronSchedulableJob);
 
     /**
+     * Same as scheduleJob, except that it would update existing job if one exists instead of creating a new one
+     *
+     * @param cronSchedulableJob
+     */
+    public void safeScheduleJob(CronSchedulableJob cronSchedulableJob);
+
+    /**
      * Updates MotechEvent data of the job defined by jobIb in the given instance of that class
      *
      * @param motechEvent
@@ -83,7 +59,19 @@ public interface MotechSchedulerService {
      */
     public void scheduleRepeatingJob(RepeatingSchedulableJob repeatingSchedulableJob);
 
+    /**
+     * Same as scheduleRepeatingJob, except that it would update existing job if one exists instead of creating a new one
+     * @param repeatingSchedulableJob
+     */
+    public void safeScheduleRepeatingJob(RepeatingSchedulableJob repeatingSchedulableJob);
+
     public void scheduleRunOnceJob(RunOnceSchedulableJob schedulableJob);
+
+    /**
+     * Same as scheduleRunOnceJob, except that it would update existing job if one exists instead of creating a new one
+     * @param schedulableJob
+     */
+    public void safeScheduleRunOnceJob(RunOnceSchedulableJob schedulableJob);
 
     /**
      * Unschedules a job with the given job ID
