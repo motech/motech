2cdbfc47cceb91b97061cbb3ab47b0c539a25a1e
Manohar Akula <manohar.akula@gmail.com>
2011-12-15 19:42:02 +0530

    sanchit,manohar| #944| client can configure max_retries and retry interval for failures

--- motech-sms-smpp/src/main/java/org/motechproject/sms/smpp/ManagedSmslibService.java
+++ motech-sms-smpp/src/main/java/org/motechproject/sms/smpp/ManagedSmslibService.java
@@ -17,22 +17,43 @@ import java.util.Properties;
 public class ManagedSmslibService {
 
     public static final String GATEWAY_ID = "smpp_gateway";
+    public static final String HOST = "host";
+    public static final String PORT = "port";
+    public static final String SYSTEM_ID = "system_id";
+    public static final String PASSWORD = "password";
+    public static final String MAX_RETRIES = "max_retries";
+    public static final String RETRY_INTERVAL_SECS = "retry_interval_secs";
 
-	private Service service;
-    private Properties properties;
+    private Service smslibService;
+    private Properties smsProperties;
+    private Properties smppProperties;
     private static final String temporaryGroup = "temporary_group";
 
     @Autowired
-	public ManagedSmslibService(Service smslibService, @Qualifier("smppProperties") Properties properties) {
-		this.service = smslibService;
-        this.properties = properties;
+    public ManagedSmslibService(Service smslibService, @Qualifier("smsProperties") Properties smsProperties, @Qualifier("smppProperties") Properties smppProperties) {
+        this.smslibService = smslibService;
+        this.smsProperties = smsProperties;
+        this.smppProperties = smppProperties;
+        configureSmsLib();
         registerGateway();
-	}
+    }
+
+    private void configureSmsLib() {
+        String maxRetriesProperty = smsProperties.getProperty(MAX_RETRIES);
+        if (maxRetriesProperty != null)
+            smslibService.getSettings().OUTBOUND_RETRIES = Integer.parseInt(maxRetriesProperty);
+        String retryIntervalSecs = smsProperties.getProperty(RETRY_INTERVAL_SECS);
+        if (retryIntervalSecs != null)
+            smslibService.getSettings().OUTBOUND_RETRY_WAIT = Integer.parseInt(retryIntervalSecs) * 1000;
+    }
 
     private void registerGateway() {
-        JSMPPGateway jsmppGateway = new JSMPPGateway(GATEWAY_ID, property("host"), Integer.parseInt(property("port")), new BindAttributes(property("system_id"), property("password"), null, BindAttributes.BindType.TRANSCEIVER));
+        JSMPPGateway jsmppGateway = new JSMPPGateway(GATEWAY_ID,
+                smppProperties.getProperty(HOST),
+                Integer.parseInt(smppProperties.getProperty(PORT)),
+                new BindAttributes(smppProperties.getProperty(SYSTEM_ID), smppProperties.getProperty(PASSWORD), null, BindAttributes.BindType.TRANSCEIVER));
         try {
-            service.addGateway(jsmppGateway);
+            smslibService.addGateway(jsmppGateway);
         } catch (GatewayException e) {
             // This should never really happen as SmsLib service will always be in STOPPED state at this point.
         }
@@ -40,29 +61,25 @@ public class ManagedSmslibService {
 
     @PostConstruct
     public void connect() throws SMSLibException, IOException, InterruptedException {
-        service.startService();
+        smslibService.startService();
     }
 
     @PreDestroy
     public void disconnect() throws SMSLibException, IOException, InterruptedException {
-        service.stopService();
+        smslibService.stopService();
     }
 
-	public void queueMessage(List<String> recipients, final String message) throws GatewayException, IOException, TimeoutException, InterruptedException {
-		service.createGroup(temporaryGroup);
-		for (String recipient : recipients)
-			service.addToGroup(temporaryGroup, recipient);
+    public void queueMessage(List<String> recipients, final String message) throws GatewayException, IOException, TimeoutException, InterruptedException {
+        smslibService.createGroup(temporaryGroup);
+        for (String recipient : recipients)
+            smslibService.addToGroup(temporaryGroup, recipient);
 
-        service.queueMessage(new OutboundMessage() {{
+        smslibService.queueMessage(new OutboundMessage() {{
             setRecipient(temporaryGroup);
             setText(message);
-            setGatewayId(GATEWAY_ID);
+            //setGatewayId(GATEWAY_ID);
         }});
 
-        service.removeGroup(temporaryGroup);
-	}
-
-    private String property(String name) {
-        return properties.get(name).toString();
+        smslibService.removeGroup(temporaryGroup);
     }
 }
