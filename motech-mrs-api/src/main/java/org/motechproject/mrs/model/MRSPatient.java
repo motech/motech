d160fe3b14f1a8521eefc5d7e5e36fe59c235173
deepans <deepans@thoughtworks.com>
2011-12-22 17:41:15 +0530

    Deepan,Geet | GN #336 | Added motechId to MRSPatient for patient search

--- motech-mrs-api/src/main/java/org/motechproject/mrs/model/MRSPatient.java
+++ motech-mrs-api/src/main/java/org/motechproject/mrs/model/MRSPatient.java
@@ -5,18 +5,20 @@ public class MRSPatient {
     private String id;
     private MRSFacility facility;
     private MRSPerson person;
+    private String motechId;
 
     public MRSPatient(String id) {
         this.id = id;
     }
 
-    public MRSPatient(MRSPerson person, MRSFacility mrsFacility) {
+    public MRSPatient(String motechId, MRSPerson person, MRSFacility mrsFacility) {
         this.facility = mrsFacility;
         this.person = person;
+        this.motechId = motechId;
     }
 
-    public MRSPatient(String patientId,MRSPerson mrsPerson,MRSFacility mrsFacility) {
-        this(mrsPerson,mrsFacility);
+    public MRSPatient(String patientId, String motechId, MRSPerson mrsPerson, MRSFacility mrsFacility) {
+        this(motechId, mrsPerson, mrsFacility);
         this.id = patientId;
     }
 
@@ -32,15 +34,20 @@ public class MRSPatient {
         return person;
     }
 
+    public String getMotechId() {
+        return motechId;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof MRSPatient)) return false;
 
         MRSPatient that = (MRSPatient) o;
 
         if (facility != null ? !facility.equals(that.facility) : that.facility != null) return false;
         if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (motechId != null ? !motechId.equals(that.motechId) : that.motechId != null) return false;
         if (person != null ? !person.equals(that.person) : that.person != null) return false;
 
         return true;
@@ -51,6 +58,7 @@ public class MRSPatient {
         int result = id != null ? id.hashCode() : 0;
         result = 31 * result + (facility != null ? facility.hashCode() : 0);
         result = 31 * result + (person != null ? person.hashCode() : 0);
+        result = 31 * result + (motechId != null ? motechId.hashCode() : 0);
         return result;
     }
 }
--- motech-openmrs-api/src/test/java/org/motechproject/openmrs/util/PatientTestUtil.java
+++ motech-openmrs-api/src/test/java/org/motechproject/openmrs/util/PatientTestUtil.java
@@ -14,7 +14,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class PatientTestUtil {
 
-    public org.openmrs.Patient setUpOpenMRSPatient(Person person, String first, String middle, String last, String address1, Date birthdate, boolean birthdateEstimated, String gender, MRSFacility facility) {
+    private FacilityTestUtil facilityTestUtil = new FacilityTestUtil();
+
+    public org.openmrs.Patient setUpOpenMRSPatient(Person person, String first, String middle, String last, String address1, Date birthdate, boolean birthdateEstimated, String gender, MRSFacility facility, String motechId) {
         PersonName personName = new PersonName(first, middle, last);
         person.addName(personName);
         setAddress(person, address1);
@@ -22,7 +24,7 @@ public class PatientTestUtil {
         mrsPatient.setBirthdate(birthdate);
         mrsPatient.setBirthdateEstimated(birthdateEstimated);
         mrsPatient.setGender(gender);
-        mrsPatient.addIdentifier(new PatientIdentifier(null, null, new Location(Integer.parseInt(facility.getId()))));
+        mrsPatient.addIdentifier(new PatientIdentifier(motechId, null, new Location(Integer.parseInt(facility.getId()))));
         return mrsPatient;
     }
 
@@ -34,7 +36,7 @@ public class PatientTestUtil {
         person.setAddresses(addresses);
     }
 
-    public void verifyReturnedPatient(String first, String middle, String last, String address1, Date birthdate, Boolean birthDateEstimated, String gender, MRSFacility facility, MRSPatient actualPatient) {
+    public void verifyReturnedPatient(String first, String middle, String last, String address1, Date birthdate, Boolean birthDateEstimated, String gender, MRSFacility facility, MRSPatient actualPatient, String motechId) {
         MRSPerson actualMRSPerson = actualPatient.getPerson();
         assertThat(actualMRSPerson.getFirstName(), is(first));
         assertThat(actualMRSPerson.getLastName(), is(last));
@@ -43,5 +45,17 @@ public class PatientTestUtil {
         assertThat(actualMRSPerson.getDateOfBirth(), is(birthdate));
         assertThat(actualMRSPerson.getGender(), is(gender));
         assertThat(actualPatient.getFacility(), is(equalTo(facility)));
+    }
+
+    public void assertEqualsForOpenMrsPatient(Patient openMrsPatient1, Patient openMRSPatient2) {
+        assertThat(openMrsPatient1.getPersonName().getGivenName(), is(equalTo(openMRSPatient2.getPersonName().getGivenName())));
+        assertThat(openMrsPatient1.getPersonName().getMiddleName(), is(equalTo(openMRSPatient2.getPersonName().getMiddleName())));
+        assertThat(openMrsPatient1.getPersonName().getFamilyName(), is(equalTo(openMRSPatient2.getPersonName().getFamilyName())));
+        assertThat(openMrsPatient1.getPersonAddress().getAddress1(), is(equalTo(openMRSPatient2.getPersonAddress().getAddress1())));
+        assertThat(openMrsPatient1.getBirthdate(), is(equalTo(openMRSPatient2.getBirthdate())));
+        assertThat(openMrsPatient1.getBirthdateEstimated(), is(equalTo(openMRSPatient2.getBirthdateEstimated())));
+        assertThat(openMrsPatient1.getGender(), is(equalTo(openMRSPatient2.getGender())));
+        assertThat(openMrsPatient1.getPatientIdentifier().getIdentifier(), is(equalTo(openMRSPatient2.getPatientIdentifier().getIdentifier())));
+        facilityTestUtil.assertEqualsForOpenMrsLocation(openMrsPatient1.getPatientIdentifier().getLocation(), openMRSPatient2.getPatientIdentifier().getLocation());
     }
 }
