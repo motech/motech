c325af93b680a08902d0ed48e9467817cbaed924
vijayaraj <vijayaraj@yahoo.com>
2011-12-26 14:33:27 +0530

    sushmita, vijay | #991 | made 'from' method private and exposed 'readFromString'

--- motech-platform-common/src/main/java/org/motechproject/dao/MotechJsonReader.java
+++ motech-platform-common/src/main/java/org/motechproject/dao/MotechJsonReader.java
@@ -33,7 +33,11 @@ public class MotechJsonReader {
         }
     }
 
-    public Object from(String text, Type ofType, Map<Type, Object> typeAdapters) {
+    public Object readFromString(String text, Type ofType) {
+        return from(text, ofType, standardTypeAdapters);
+    }
+
+    private Object from(String text, Type ofType, Map<Type, Object> typeAdapters) {
         GsonBuilder gsonBuilder = new GsonBuilder();
         for (Map.Entry<Type, Object> entry : typeAdapters.entrySet()) {
             gsonBuilder.registerTypeAdapter(entry.getKey(), entry.getValue());
