46d967b10e903ee9412afba8eaf890a16a7be0f4
Srivathsava <srivathr@thoughtworks.com>
2012-02-08 19:35:43 +0530

    Sri, Luka | #0000 | Changing the constant key value - CALL_STATE

--- motech-ivr-kookoo/src/main/java/org/motechproject/ivr/kookoo/eventlogging/CallEventConstants.java
+++ motech-ivr-kookoo/src/main/java/org/motechproject/ivr/kookoo/eventlogging/CallEventConstants.java
@@ -4,5 +4,5 @@ public class CallEventConstants {
     public static final String CUSTOM_DATA_LIST = "responseXML";
     public static final String TREE_NAME = "treeName";
     public static final String DTMF_DATA = "dtmfData";
-    public static final String CALL_STATE = "callState";
+    public static final String CALL_STATE = "call_state";
 }
