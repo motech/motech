957a3e20723f52d933960ed7b2c199579dcfff3a
shruthi <shruthidipali@gmail.com>
2012-02-17 10:49:10 +0530

    Shruthi, Luka | #40 | Removed Appointment and Visit services. Client would use the repositories directly.

--- motech-appointments-api/src/main/java/org/motechproject/appointments/api/EventKeys.java
+++ motech-appointments-api/src/main/java/org/motechproject/appointments/api/EventKeys.java
@@ -13,25 +13,15 @@ public class EventKeys
 
     public final static String APPOINTMENT_ID_KEY = "AppointmentID";
     public final static String REMINDER_ID_KEY = "ReminderID";
-    public final static String VISIT_ID_KEY = "VisitID";
     public final static String JOB_ID_KEY = "JobID";
 
     public final static String BASE_SUBJECT = "org.motechproject.server.appointments.";
 
-    public final static String APPOINTMENT_WILDCARD_SUBJECT = BASE_SUBJECT + "appointment.*";
-    public final static String APPOINTMENT_CREATED_SUBJECT = BASE_SUBJECT + "appointment.created";
-    public final static String APPOINTMENT_UPDATED_SUBJECT = BASE_SUBJECT + "appointment.updated";
     public final static String APPOINTMENT_DELETED_SUBJECT = BASE_SUBJECT + "appointment.deleted";
 
-    public final static String REMINDER_WILDCARD_SUBJECT = BASE_SUBJECT + "reminder.*";
     public final static String REMINDER_CREATED_SUBJECT = BASE_SUBJECT + "reminder.created";
     public final static String REMINDER_UPDATED_SUBJECT = BASE_SUBJECT + "reminder.updated";
     public final static String REMINDER_DELETED_SUBJECT = BASE_SUBJECT + "reminder.deleted";
-
-    public final static String VISIT_WILDCARD_SUBJECT = BASE_SUBJECT + "visit.*";
-    public final static String VISIT_CREATED_SUBJECT = BASE_SUBJECT + "visit.created";
-    public final static String VISIT_UPDATED_SUBJECT = BASE_SUBJECT + "visit.updated";
-    public final static String VISIT_DELETED_SUBJECT = BASE_SUBJECT + "visit.deleted";
 
     public final static String APPOINTMENT_REMINDER_EVENT_SUBJECT = BASE_SUBJECT + "reminder";
     public static final String EXTERNAL_ID_KEY = "external.id";
--- motech-appointments-api/src/main/java/org/motechproject/appointments/api/context/AppointmentsContext.java
+++ motech-appointments-api/src/main/java/org/motechproject/appointments/api/context/AppointmentsContext.java
@@ -1,8 +1,6 @@
 package org.motechproject.appointments.api.context;
 
-import org.motechproject.appointments.api.AppointmentService;
 import org.motechproject.appointments.api.ReminderService;
-import org.motechproject.appointments.api.VisitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,23 +8,7 @@ import org.springframework.stereotype.Component;
 public class AppointmentsContext
 {
     @Autowired
-    private AppointmentService appointmentService;
-
-    @Autowired
     private ReminderService reminderService;
-
-    @Autowired
-    private VisitService visitService;
-
-    public AppointmentService getAppointmentService()
-    {
-        return appointmentService;
-    }
-
-    public void setAppointmentService(AppointmentService appointmentService)
-    {
-        this.appointmentService = appointmentService;
-    }
 
     public ReminderService getReminderService()
     {
@@ -36,16 +18,6 @@ public class AppointmentsContext
     public void setReminderService(ReminderService reminderService)
     {
         this.reminderService = reminderService;
-    }
-
-    public VisitService getVisitService()
-    {
-        return visitService;
-    }
-
-    public void setVisitService(VisitService visitService)
-    {
-        this.visitService = visitService;
     }
 
     public static AppointmentsContext getInstance(){
