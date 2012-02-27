38bcd1810e44e639748fe59cc2e9ec91c5b932c8
shruthi <shruthidipali@gmail.com>
2012-02-27 17:12:28 +0530

    Luka, Dileep, Shruthi | #1277 | Encoding the reply url when initiating a call using kookoo

--- motech-ivr-kookoo/src/test/java/org/motechproject/ivr/kookoo/KookooCallServiceImplTest.java
+++ motech-ivr-kookoo/src/test/java/org/motechproject/ivr/kookoo/KookooCallServiceImplTest.java
@@ -48,7 +48,7 @@ public class KookooCallServiceImplTest {
         ivrService.initiateCall(new CallRequest(phoneNumber, params, CALLBACK_URL));
 
         String apiKey = "api_key=KKbedce53758c2e0b0e9eed7191ec2a466";
-        String replyUrl = "&url=http://localhost/tama/ivr/reply?dataMap={\"external_id\":\"external_id\",\"is_outbound_call\":\"true\"}";
+        String replyUrl = "&url=http%3A%2F%2Flocalhost%2Ftama%2Fivr%2Freply%3FdataMap%3D%7B%22external_id%22%3A%22external_id%22%2C%22is_outbound_call%22%3A%22true%22%7D";
         String phoneNo = "&phone_no=9876543211";
         String callbackUrl = "&callback_url=http://localhost/tama/ivr/reply/callback?external_id=external_id";
         verify(httpClient).executeMethod(argThat(new GetMethodMatcher("http://kookoo/outbound.php?" + apiKey + replyUrl + phoneNo + callbackUrl)));
@@ -63,7 +63,7 @@ public class KookooCallServiceImplTest {
         ivrService.initiateCall(new CallRequest(phoneNumber, params, CALLBACK_URL));
 
         String apiKey = "api_key=KKbedce53758c2e0b0e9eed7191ec2a466";
-        String replyUrl = "&url=http://localhost/tama/ivr/reply?dataMap={\"external_id\":\"external_id\",\"hero\":\"batman\",\"is_outbound_call\":\"true\"}";
+        String replyUrl = "&url=http%3A%2F%2Flocalhost%2Ftama%2Fivr%2Freply%3FdataMap%3D%7B%22external_id%22%3A%22external_id%22%2C%22hero%22%3A%22batman%22%2C%22is_outbound_call%22%3A%22true%22%7D";
         String phoneNo = "&phone_no=9876543211";
         String callbackUrl = "&callback_url=http://localhost/tama/ivr/reply/callback?external_id=external_id";
         verify(httpClient).executeMethod(argThat(new GetMethodMatcher("http://kookoo/outbound.php?" + apiKey + replyUrl + phoneNo + callbackUrl)));
