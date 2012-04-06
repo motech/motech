283ec76143a1e0eff5da9311488328a9a2702b96
Manohar Akula <manohar.akula@gmail.com>
2012-04-06 15:47:17 +0530

    manohar| #1520| adding javadocs for sms smpp module

--- motech-sms-smpp/src/main/java/org/motechproject/sms/smpp/constants/EventDataKeys.java
+++ motech-sms-smpp/src/main/java/org/motechproject/sms/smpp/constants/EventDataKeys.java
@@ -1,8 +1,20 @@
 package org.motechproject.sms.smpp.constants;
 
 public class EventDataKeys {
+    /**
+     * Indicates the sender of the message
+     */
     public static final String SENDER = "sender";
+    /**
+     * Indicates the recipient for the message
+     */
     public static final String RECIPIENT = "recipient";
+    /**
+     * Indicates the message text for an inbound sms
+     */
     public static final String INBOUND_MESSAGE = "inbound_message";
-	public static final String TIMESTAMP = "timestamp";
+    /**
+     * the timestamp when the sender sent out the message
+     */
+    public static final String TIMESTAMP = "timestamp";
 }
