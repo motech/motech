3b33780a8929893ba7c7f6b1aefb87f0b9c5a6bc
Manohar Akula <manohar.akula@gmail.com>
2012-04-30 16:47:34 +0530

    manohar| #1516| adding java docs for CampaignRequest

--- motech-message-campaign/src/main/java/org/motechproject/server/messagecampaign/contract/CampaignRequest.java
+++ motech-message-campaign/src/main/java/org/motechproject/server/messagecampaign/contract/CampaignRequest.java
@@ -7,6 +7,11 @@ import org.motechproject.model.Time;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * \ingroup MessageCampaign
+ *
+ * This is the document to enroll an entity into a campaign
+ */
 public class CampaignRequest {
     private String externalId;
     private String campaignName;
@@ -18,6 +23,13 @@ public class CampaignRequest {
     public CampaignRequest() {
     }
 
+    /**
+     * Creates a request for enrollment into a campaign. Holds all fields required for the enrollment.
+     * @param externalId - a client defined id to identify the enrollment
+     * @param campaignName - the campaign into which the entity should be enrolled
+     * @param reminderTime - time of day at which the alert must be raised. can be overridden by campaign's deliverTime, unless 24.hour.repeating.campaign.strategy is true
+     * @param referenceDate - the date the campaign has started for this enrollment. it can be in the past resulting in a delayed enrollment.
+     */
     public CampaignRequest(String externalId, String campaignName, Time reminderTime, LocalDate referenceDate) {
         this.externalId = externalId;
         this.campaignName = campaignName;
@@ -26,11 +38,27 @@ public class CampaignRequest {
         userPreferredDays = new ArrayList<DayOfWeek>();
     }
 
+    /**
+     * Creates a request for enrollment into a campaign. Holds all fields required for the enrollment.
+     * @param externalId - a client defined id to identify the enrollment
+     * @param campaignName - the campaign into which the entity should be enrolled
+     * @param reminderTime - time of day at which the alert must be raised. can be overridden by campaign's deliverTime, unless 24.hour.repeating.campaign.strategy is true
+     * @param referenceDate - the date the campaign has started for this enrollment. it can be in the past resulting in a delayed enrollment.
+     * @param startOffset - the offset should be used in case of a delayed enrollment to specify where the enrollment begins w.r.t the referenceDate.
+     */
     public CampaignRequest(String externalId, String campaignName, Time reminderTime, LocalDate referenceDate, Integer startOffset) {
         this(externalId, campaignName, reminderTime, referenceDate);
         this.startOffset = startOffset;
     }
 
+    /**
+     * Creates a request for enrollment into a campaign. Holds all fields required for the enrollment.
+     * @param externalId - a client defined id to identify the enrollment
+     * @param campaignName - the campaign into which the entity should be enrolled
+     * @param reminderTime - time of day at which the alert must be raised. can be overridden by campaign's deliverTime, unless 24.hour.repeating.campaign.strategy is true
+     * @param referenceDate - the date the campaign has started for this enrollment. it can be in the past resulting in a delayed enrollment.
+     * @param userPreferredDays - specify a list of days that override the days defined by the campaign. The enrollment will have alerts raised on these days only.
+     */
     public CampaignRequest(String externalId, String campaignName, Time reminderTime, LocalDate referenceDate, List<DayOfWeek> userPreferredDays) {
         this(externalId, campaignName, reminderTime, referenceDate);
         this.userPreferredDays.addAll(userPreferredDays);
