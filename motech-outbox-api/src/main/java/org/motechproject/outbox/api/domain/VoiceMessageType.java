81f8273af9308056171df1b3f7a6707bfa7b182b
Srivatsa Katta <vatsa.katta@gmail.com>
2012-05-09 10:15:32 +0530

    Katta | #1516 | Added documentation to the OutboundVoiceMessage

--- motech-outbox-api/src/main/java/org/motechproject/outbox/api/domain/VoiceMessageType.java
+++ motech-outbox-api/src/main/java/org/motechproject/outbox/api/domain/VoiceMessageType.java
@@ -3,37 +3,77 @@ package org.motechproject.outbox.api.domain;
 public class VoiceMessageType {
     private String voiceMessageTypeName;
     private String templateName;
-    private boolean canBeSaved; // indicates if this type of messages allowed to be saved by patients in they voice outbox
-    private boolean canBeReplayed; // indicates if this type of messages allowed to be replayed by a patient request after been played once
+    private boolean canBeSaved;
+    private boolean canBeReplayed;
 
+    /**
+     * Identifies the type of the message.
+     *
+     * @return a String specifying the type of the message.
+     */
     public String getVoiceMessageTypeName() {
         return voiceMessageTypeName;
     }
 
+    /**
+     * Property to categorize the message
+     *
+     * @param voiceMessageTypeName a string specifying the category/type of the message
+     */
     public void setVoiceMessageTypeName(String voiceMessageTypeName) {
         this.voiceMessageTypeName = voiceMessageTypeName;
     }
 
+    /**
+     * Identifies the template to be used to build the message.
+     *
+     * @return a String identifying the template
+     */
     public String getTemplateName() {
         return templateName;
     }
 
+    /**
+     * Name of the template to be used to build this message.
+     *
+     * @param templateName a string specifying the template
+     */
     public void setTemplateName(String templateName) {
         this.templateName = templateName;
     }
 
+    /**
+     * Indicates if the message can be saved or not in the party's outbox
+     *
+     * @return a boolean
+     */
     public boolean isCanBeSaved() {
         return canBeSaved;
     }
 
+    /**
+     * This property will let messages to be saved by the party in their outbox
+     *
+     * @param canBeSaved a boolean specifying that the message can be saved
+     */
     public void setCanBeSaved(boolean canBeSaved) {
         this.canBeSaved = canBeSaved;
     }
 
+    /**
+     * Indicates if the message can be replayed by the party or not.
+     *
+     * @return a boolean
+     */
     public boolean isCanBeReplayed() {
         return canBeReplayed;
     }
 
+    /**
+     * This property will let the messages to be replayed after the message has been played once
+     *
+     * @param canBeReplayed a boolean specifying that the message can be replayed
+     */
     public void setCanBeReplayed(boolean canBeReplayed) {
         this.canBeReplayed = canBeReplayed;
     }
@@ -48,6 +88,4 @@ public class VoiceMessageType {
                 ", canBeReplayed=" + canBeReplayed +
                 '}';
     }
-
-
 }
