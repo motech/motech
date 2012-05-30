4f23a9fc72a3c2d2c4ce37b8990c8eb04e5ff24d
Łukasz Lewczyński <llewczynski@soldevelo.com>
2012-05-30 17:28:08 +0200

    llewczynski | Renamed motech-platform-common to motech-platform-common-api (cherry picked from commit 137d458e3c878d317b14e8b4db07e09f2305535e)
    Conflicts:
    
    	motech-appointments/pom.xml
    	motech-cmslite-api/pom.xml
    	motech-commcare-api/pom.xml
    	motech-mobileforms-api/pom.xml
    	motech-platform-decisiontree-common/pom.xml
    	motech-testing-utils/pom.xml

---
+++ motech-platform-common-api/src/main/java/org/motechproject/dao/MotechBaseRepository.java
@@ -0,0 +1,84 @@
+package org.motechproject.dao;
+
+import org.ektorp.BulkDeleteDocument;
+import org.ektorp.CouchDbConnector;
+import org.ektorp.ViewQuery;
+import org.ektorp.support.CouchDbRepositorySupport;
+import org.ektorp.support.GenerateView;
+import org.motechproject.model.MotechBaseDataObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class MotechBaseRepository<T extends MotechBaseDataObject> extends CouchDbRepositorySupport<T> {
+    private Class<T> type;
+
+    protected MotechBaseRepository(Class<T> type, CouchDbConnector db) {
+        super(type, db);
+        this.type = type;
+        initStandardDesignDocument();
+    }
+
+    protected void addOrReplace(T entity, String businessFieldName, String businessId) {
+        List<T> entities = entities(businessFieldName, businessId);
+        if (entities.size() == 0) add(entity);
+        else if (entities.size() == 1) {
+            T entityInDb = entities.get(0);
+            entity.setId(entityInDb.getId());
+            entity.setRevision(entityInDb.getRevision());
+            update(entity);
+        }
+        else throw new BusinessIdNotUniqueException(businessFieldName, businessId);
+    }
+
+    private List<T> entities(String businessFieldName, String businessId) {
+        String viewName = String.format("by_%s", businessFieldName);
+        ViewQuery q = createQuery(viewName).key(businessId).includeDocs(true);
+        return db.queryView(q, type);
+    }
+
+    public void removeAll(String fieldName, String value) {
+        List<T> entities = entities(fieldName, value);
+        removeAll(entities);
+    }
+
+    private void removeAll(List<T> entities) {
+        List<BulkDeleteDocument> bulkDeleteQueue = new ArrayList<BulkDeleteDocument>(entities.size());
+        for (T entity : entities) {
+            bulkDeleteQueue.add(BulkDeleteDocument.of(entity));
+        }
+        db.executeBulk(bulkDeleteQueue);
+    }
+
+    public void removeAll() {
+        removeAll(getAll());
+    }
+
+    public void safeRemove(T entity) {
+        if (contains(entity.getId()))
+            remove(entity);
+    }
+
+    @Override
+    @GenerateView
+    public List<T> getAll() {
+        return super.getAll();
+    }
+
+    protected List<T> getAll(int limit) {
+        ViewQuery q = createQuery("all").limit(limit).includeDocs(true);
+        return db.queryView(q, type);
+    }
+
+    protected T singleResult(List<T> resultSet) {
+        return (resultSet == null || resultSet.isEmpty()) ? null : resultSet.get(0);
+    }
+
+    public List<T> queryViewWithKeyList(String viewName, List<String> keys) {
+        return db.queryView(createQuery(viewName)
+                .includeDocs(true)
+                .keys(keys),
+                type);
+    }
+}
