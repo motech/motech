5a267ace53d346f31d3359be061770c4aaa478d3
Soham Ghosh <sohamghosh@gmail.com>
2012-06-14 18:30:33 +0530

    Soham,Manohar| #42| moving web security module from contrib
    Change-Id: I4228a7a60322aca6db4775a96ee9a7988c49496b

---
+++ motech-web-security/src/main/java/org/motechproject/security/repository/AllMotechUsersCouchdbImpl.java
@@ -0,0 +1,61 @@
+package org.motechproject.security.repository;
+
+import org.ektorp.CouchDbConnector;
+import org.ektorp.ViewQuery;
+import org.ektorp.support.GenerateView;
+import org.ektorp.support.View;
+import org.motechproject.dao.MotechBaseRepository;
+import org.motechproject.security.domain.MotechUser;
+import org.motechproject.security.domain.MotechUserCouchdbImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@View(name = "all", map = "function(doc) { emit(doc._id, doc); }")
+public class AllMotechUsersCouchdbImpl extends MotechBaseRepository<MotechUserCouchdbImpl> implements AllMotechUsers {
+
+    @Autowired
+    protected AllMotechUsersCouchdbImpl(@Qualifier("webSecurityDbConnector") CouchDbConnector db) {
+        super(MotechUserCouchdbImpl.class, db);
+        initStandardDesignDocument();
+    }
+
+    @Override
+    @View(name = "by_userName", map = "function(doc) { if (doc.type ==='MotechUser') { emit(doc.userName, doc._id); }}")
+    public MotechUser findByUserName(String userName) {
+        if (userName == null)
+            return null;
+        userName = userName.toLowerCase();
+        ViewQuery viewQuery = createQuery("by_userName").key(userName).includeDocs(true);
+        return singleResult(db.queryView(viewQuery, MotechUserCouchdbImpl.class));
+    }
+
+    @Override
+    @View(name = "find_by_role", map = "function(doc) {if (doc.type ==='MotechUser') {for(i in doc.roles) {emit(doc.roles[i], [doc._id]);}}}")
+    public List<? extends MotechUser> findByRole(String role) {
+        if (role == null)
+            return null;
+        ViewQuery viewQuery = createQuery("find_by_role").key(role).includeDocs(true);
+        return db.queryView(viewQuery, MotechUserCouchdbImpl.class);
+    }
+
+    @Override
+    public void add(MotechUser user) {
+        if (findByUserName(user.getUserName()) != null)
+            return;
+        super.add((MotechUserCouchdbImpl) user);
+    }
+
+    @Override
+    public void update(MotechUser motechUser) {
+        super.update((MotechUserCouchdbImpl) motechUser);
+    }
+
+    @Override
+    public void remove(MotechUser motechUser) {
+        super.remove((MotechUserCouchdbImpl) motechUser);
+    }
+}
---
+++ motech-web-security/src/test/java/org/motechproject/security/authentication/MotechAuthenticationProviderTest.java
@@ -0,0 +1,96 @@
+package org.motechproject.security.authentication;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.motechproject.security.domain.MotechUser;
+import org.motechproject.security.domain.MotechUserCouchdbImpl;
+import org.motechproject.security.repository.AllMotechUsers;
+import org.motechproject.security.service.MotechUserProfile;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import static java.util.Arrays.asList;
+import static junit.framework.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class MotechAuthenticationProviderTest {
+
+    @Mock
+    private AllMotechUsers allMotechUsers;
+    @Mock
+    private MotechPasswordEncoder passwordEncoder;
+
+    private MotechAuthenticationProvider authenticationProvider;
+
+    @Before
+    public void setup() {
+        initMocks(this);
+        authenticationProvider = new MotechAuthenticationProvider(allMotechUsers, passwordEncoder);
+    }
+
+    @Test
+    public void shouldRetrieveUserFromDatabase() {
+        MotechUser motechUser = new MotechUserCouchdbImpl("bob", "encodedPassword", "entity_1", asList("some_role"));
+        when(allMotechUsers.findByUserName("bob")).thenReturn(motechUser);
+
+        MotechAuthenticationProvider authenticationProvider = new MotechAuthenticationProvider(allMotechUsers, passwordEncoder);
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken("bob", "password");
+        UserDetails userDetails = authenticationProvider.retrieveUser("bob", authentication);
+
+        assertEquals("encodedPassword", userDetails.getPassword());
+        assertEquals(motechUser.getUserName(), ((MotechUserProfile) authentication.getDetails()).getUserName());
+    }
+
+    @Test(expected = AuthenticationException.class)
+    public void shouldThrowExceptionIfUserDoesntExist() {
+        when(allMotechUsers.findByUserName("bob")).thenReturn(null);
+
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken("bob", "password");
+        authenticationProvider.retrieveUser("bob", authentication);
+    }
+
+    @Test(expected = AuthenticationException.class)
+    public void shouldThrowExceptionIfUserIsInactive() {
+        MotechUserCouchdbImpl motechUser = new MotechUserCouchdbImpl("bob", "encodedPassword", "entity_1", asList("some_role"));
+        motechUser.setActive(false);
+        when(allMotechUsers.findByUserName("bob")).thenReturn(motechUser);
+
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken("bob", "password");
+        authenticationProvider.retrieveUser("bob", authentication);
+    }
+
+    @Test
+    public void shouldAuthenticateUser() {
+        when(passwordEncoder.isPasswordValid("encodedPassword", "password")).thenReturn(true);
+
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken("bob", "password");
+        UserDetails user = mock(UserDetails.class);
+        when(user.getPassword()).thenReturn("encodedPassword");
+
+        authenticationProvider.additionalAuthenticationChecks(user, authentication);
+    }
+
+    @Test(expected = AuthenticationException.class)
+    public void shouldNotAuthenticateEmptyPassword() {
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken("bob", "");
+        UserDetails user = mock(UserDetails.class);
+        when(user.getPassword()).thenReturn("encodedPassword");
+
+        authenticationProvider.additionalAuthenticationChecks(user, authentication);
+    }
+
+    @Test(expected = AuthenticationException.class)
+    public void shouldNotAuthenticateWrongPassword() {
+        when(passwordEncoder.isPasswordValid("encodedPassword", "password")).thenReturn(false);
+
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken("bob", "");
+        UserDetails user = mock(UserDetails.class);
+        when(user.getPassword()).thenReturn("encodedPassword");
+
+        authenticationProvider.additionalAuthenticationChecks(user, authentication);
+    }
+}
