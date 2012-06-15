c20e8bf855f36abcda9d67a19c7886bb6b731496
Paweł Gesek <pgesek@soldevelo.com>
2012-06-15 14:39:01 +0200

    pgesek | #41 | Improved debian script
    Change-Id: Icc87cbac0729f8a977ed50f9ddc2688afab3857f

--- packaging/debian/debian-build.sh
+++ packaging/debian/debian-build.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 MOTECH_VERSION=0.11_1
-TMP_DIR=/tmp/$$
-BUILD_DIR=`pwd`
+TMP_DIR=/tmp/motech-debian-build-$$
 WARNAME=motech-platform-server.war
-WAR_PATCHDIR=$BUILD_DIR/warpatch
 MOTECH_PACKAGENAME="motech-base_$MOTECH_VERSION.deb"
+CURRENT_DIR=`pwd`
 
 # exit on non-zero exit code
 set -e
 
 # Set motech directory
 MOTECH_BASE=../..
-if [ $# -gt 1 ]; then
+if [ $# -gt 0 ]; then
 	MOTECH_BASE=$1
 fi
 
+cd $MOTECH_BASE
+MOTECH_BASE=`pwd`
+
+PACKAGING_DIR=$MOTECH_BASE/packaging/debian
+WAR_PATCHDIR=$PACKAGING_DIR/warpatch
 MOTECH_WAR=$MOTECH_BASE/motech-platform-server/target/$WARNAME
 
 if [ ! -f $MOTECH_WAR ]; then
@@ -40,11 +44,10 @@ done
 
 # zip .war
 zip -r $WARNAME * 1>/dev/null
-mv $WARNAME $BUILD_DIR/motech-base/var/lib/motech/webapps/
 
-# clean up
-cd $BUILD_DIR
-rm -r $TMP_DIR
+# copy motech-base
+cp -r $PACKAGING_DIR/motech-base .
+mv $WARNAME ./motech-base/var/lib/motech/webapps/
 
 # set up permissions
 find ./motech-base -type d | xargs chmod 755  # for directories
@@ -55,23 +58,22 @@ chmod 755 ./motech-base/DEBIAN/postrm
 chmod 755 ./motech-base/DEBIAN/control
 chmod 755 ./motech-base/etc/init.d/motech
 
-
-
 # Build package
 echo "Building package"
 fakeroot dpkg-deb --build motech-base
 
-if [ ! -d target ]; then
-	mkdir target
+if [ ! -d $PACKAGING_DIR/target ]; then
+	mkdir $PACKAGING_DIR/target
 fi
 
-mv motech-base.deb target/$MOTECH_PACKAGENAME
+mv motech-base.deb $PACKAGING_DIR/target/$MOTECH_PACKAGENAME
 
 # Check package for problems
 echo "Checking package with lintian"
-lintian -i target/$MOTECH_PACKAGENAME
+lintian -i $PACKAGING_DIR/target/$MOTECH_PACKAGENAME
 
 echo "Done! Created $MOTECH_PACKAGENAME"
 
-#clean up war
-rm $BUILD_DIR/motech-base/var/lib/motech/webapps/$WARNAME
+#clean up 
+cd $CURRENT_DIR
+rm -r $TMP_DIR
