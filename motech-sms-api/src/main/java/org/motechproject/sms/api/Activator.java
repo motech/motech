3e1c94ba47e68dedf5b897349221c821af860fa7
Vinkesh Banka <vinkesh.banka@gmail.com>
2012-06-19 18:17:01 +0530

    Banka, Dileep | Split Spring context into OSGi and non-OSGi contexts for SmsApi module
    Change-Id: Ie6e7c88013a9f8816645b7471297f83bf5c1c753

--- motech-sms-api/src/main/java/org/motechproject/sms/api/Activator.java
+++ motech-sms-api/src/main/java/org/motechproject/sms/api/Activator.java
@@ -45,7 +45,7 @@ import org.springframework.web.servlet.DispatcherServlet;
 
 public class Activator implements BundleActivator {
     private static Logger logger = LoggerFactory.getLogger(Activator.class);
-    private static final String CONTEXT_CONFIG_LOCATION = "applicationSmsAPI.xml";
+    private static final String CONTEXT_CONFIG_LOCATION = "applicationSmsAPIBundle.xml";
     private static final String SERVLET_URL_MAPPING = "/smsapi";
     private ServiceTracker tracker;
     private ServiceReference httpService;
