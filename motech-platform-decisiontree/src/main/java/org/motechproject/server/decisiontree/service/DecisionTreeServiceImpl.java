3b2726679843081270a41206c244be317a27fb2d
Dileep Bapat <dileepbapat@gmail.com>
2012-06-26 14:33:51 +0530

    Dileep | Fixed issue with decision tree traversal.
    Change-Id: I1563546c901484f208398b75d7bf12b8f5048825

--- motech-platform-decisiontree/src/main/java/org/motechproject/server/decisiontree/service/DecisionTreeServiceImpl.java
+++ motech-platform-decisiontree/src/main/java/org/motechproject/server/decisiontree/service/DecisionTreeServiceImpl.java
@@ -7,7 +7,9 @@ import org.motechproject.server.decisiontree.TreeNodeLocator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DecisionTreeServiceImpl implements DecisionTreeService {
     private Logger logger = LoggerFactory.getLogger((this.getClass()));
 
