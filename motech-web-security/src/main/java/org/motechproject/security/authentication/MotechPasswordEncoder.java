8cfa683e218ecf833b36471a560002dc1b22452a
Manohar Akula <manohar.akula@gmail.com>
2012-06-29 14:29:29 +0530

    using bcrypt for encoding
    Change-Id: If37f378b7c9fb85c4397eeb65c700a76eb2b36a0

--- motech-web-security/src/main/java/org/motechproject/security/authentication/MotechPasswordEncoder.java
+++ motech-web-security/src/main/java/org/motechproject/security/authentication/MotechPasswordEncoder.java
@@ -1,22 +1,18 @@
 package org.motechproject.security.authentication;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @Qualifier("passwordEncoder")
-public class MotechPasswordEncoder extends Md5PasswordEncoder {
-
-    @Value("${password.salt}")
-    private String salt;
+public class MotechPasswordEncoder extends BCryptPasswordEncoder {
 
     public String encodePassword(String rawPassword) {
-        return super.encodePassword(rawPassword, salt);
+        return super.encode(rawPassword);
     }
 
     public boolean isPasswordValid(String encPassword, String rawPassword) {
-        return super.isPasswordValid(encPassword, rawPassword, salt);
+        return super.matches(rawPassword, encPassword);
     }
 }
