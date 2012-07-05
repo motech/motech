299d3282994bea960df679326a12c2b52e26f312
Dileep Bapat <dileepbapat@gmail.com>
2012-07-05 15:48:54 +0530

    Banka,Dileep| Card #80 | Add dial out support to decision tree.
    Change-Id: I55dcb0612d7c0b633c2708445d7ee7b9a303444f

---
+++ motech-platform-decisiontree-common/src/test/java/org/motechproject/decisiontree/model/DialStatusTest.java
@@ -0,0 +1,14 @@
+package org.motechproject.decisiontree.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class DialStatusTest {
+    @Test
+    public void shouldCheckValidityOfDialStatus() throws Exception {
+        assertTrue(DialStatus.isValid("completed"));
+        assertFalse(DialStatus.isValid("someInvalidKey"));
+    }
+}
