a28f204b3d1a32cfb8587d79fdbdf80ded210100
Dileep Bapat <dileepbapat@gmail.com>
2012-07-09 12:36:55 +0530

    Banka,Dileep| Card #80 | Add dial out support to decision tree.
    Change-Id: I55dcb0612d7c0b633c2708445d7ee7b9a303444f

--- motech-ivr-verboice/src/test/java/org/motechproject/server/verboice/TestIVRHandler.java
+++ motech-ivr-verboice/src/test/java/org/motechproject/server/verboice/TestIVRHandler.java
@@ -1,12 +1,15 @@
 package org.motechproject.server.verboice;
 
 import org.motechproject.server.verboice.domain.VerboiceHandler;
+import org.motechproject.server.verboice.domain.VerboiceResponse;
 
 import java.util.Map;
 
 public class TestIVRHandler implements VerboiceHandler{
     @Override
     public String handle(Map<String, String> parameters) {
-        return "verboice response xml";
+        final VerboiceResponse verboiceResponse = new VerboiceResponse();
+        verboiceResponse.playUrl("http://example.com/fileHello.wav");
+        return verboiceResponse.toXMLString();
     }
 }
---
+++ motech-platform-decisiontree-common/src/main/java/org/motechproject/decisiontree/model/DialStatus.java
@@ -0,0 +1,30 @@
+package org.motechproject.decisiontree.model;
+
+public enum DialStatus {
+    completed,
+    failed,
+    busy,
+    noAnswer("no-answer"),;
+    private String title;
+
+    private DialStatus() {
+    }
+
+    DialStatus(String s) {
+        title = s;
+    }
+
+    public static boolean isValid(String key) {
+        for (DialStatus dialStatus : values()) {
+            if (dialStatus.toString().equals(key)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return title == null ? name() : title;
+    }
+}
