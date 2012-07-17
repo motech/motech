b88ec96c15ce3d7e42ae970dd5dddaed2f887166
Russell <russell.gillenjr@maine.edu>
2012-07-17 12:37:21 -0400

    RGillen | #136 | EventListenerRegistry is now exported as an OSGi service
    Change-Id: I75214da3daa84e7ede0b4bacd19760f210e11aef

--- motech-platform-server-api/src/main/java/org/motechproject/server/event/EventListenerRegistry.java
+++ motech-platform-server-api/src/main/java/org/motechproject/server/event/EventListenerRegistry.java
@@ -1,18 +1,19 @@
 package org.motechproject.server.event;
 
+import java.util.List;
+import java.util.Set;
 import org.motechproject.metrics.MetricsAgent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.*;
-
+import org.springframework.stereotype.Service;
 
 /**
- * This class acts as a registry for all scheduled event listeners. One can register themselves to listen for
- * a specific set of event types.
+ * This class acts as a registry for all scheduled event listeners. One can
+ * register themselves to listen for a specific set of event types.
  */
-public class EventListenerRegistry {
+@Service
+public class EventListenerRegistry implements EventListenerRegistryService {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     private EventListenerTree listenerTree = new EventListenerTree();
@@ -28,7 +29,8 @@ public class EventListenerRegistry {
     }
 
     /**
-     * Register an event listener to be notified when events of a given type are received via the Server JMS Event Queue
+     * Register an event listener to be notified when events of a given type are
+     * received via the Server JMS Event Queue
      *
      * @param listener the listener instance
      * @param subjects the event types that a listener is interested in
@@ -47,7 +49,7 @@ public class EventListenerRegistry {
             throw new IllegalArgumentException(errorMessage);
         }
 
-        // Add the listener to the  list of those interested in each event type
+        // Add the listener to the list of those interested in each event type
         for (String subject : subjects) {
             registerListener(listener, subject);
         }
@@ -68,20 +70,23 @@ public class EventListenerRegistry {
 
         final long startTime = metricsAgent.startTimer();
         listenerTree.addListener(listener, subject);
-        metricsAgent.stopTimer("motech.listener-registry.addListener", startTime);
+        metricsAgent.stopTimer("motech.listener-registry.addListener",
+                startTime);
     }
 
     /**
-     * Retrieve a list of event listeners for a given event type. If there are no listeners, an empty list is
-     * returned.
+     * Retrieve a list of event listeners for a given event type. If there are
+     * no listeners, an empty list is returned.
      *
      * @param subject The event type that you are seeking listeners for
-     * @return A list of scheduled event listeners that are interested in that event
+     * @return A list of scheduled event listeners that are interested in that
+     *         event
      */
     public Set<EventListener> getListeners(String subject) {
         final long startTime = metricsAgent.startTimer();
         Set<EventListener> ret = listenerTree.getListeners(subject);
-        metricsAgent.stopTimer("motech.listener-registry.getListeners", startTime);
+        metricsAgent.stopTimer("motech.listener-registry.getListeners",
+                startTime);
 
         return ret;
     }
@@ -96,7 +101,8 @@ public class EventListenerRegistry {
 
         final long startTime = metricsAgent.startTimer();
         boolean ret = listenerTree.hasListener(subject);
-        metricsAgent.stopTimer("motech.listener-registry.hasListener", startTime);
+        metricsAgent.stopTimer("motech.listener-registry.hasListener",
+                startTime);
 
         return ret;
     }
@@ -111,7 +117,8 @@ public class EventListenerRegistry {
 
         final long startTime = metricsAgent.startTimer();
         int ret = listenerTree.getListenerCount(subject);
-        metricsAgent.stopTimer("motech.listener-registry.hasListener", startTime);
+        metricsAgent.stopTimer("motech.listener-registry.hasListener",
+                startTime);
 
         return ret;
     }
