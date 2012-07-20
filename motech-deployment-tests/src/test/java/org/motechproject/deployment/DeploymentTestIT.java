2161caec794a3e22e74052e7cdd3e8427605129e
Łukasz Lewczyński <llewczynski@soldevelo.com>
2012-07-20 15:31:07 +0200

    llewczynski | #94 | Fixed deployment test
    Change-Id: Id10148b26c2d3c616cc7e8d2eaaa768fbe6185f8

--- motech-deployment-tests/src/test/java/org/motechproject/deployment/DeploymentTestIT.java
+++ motech-deployment-tests/src/test/java/org/motechproject/deployment/DeploymentTestIT.java
@@ -1,24 +1,25 @@
 package org.motechproject.deployment;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class DeploymentTestIT {
-    private static final String EXCEPTION_PATTERN = "^([A-Za-z]+\\.)+[A-Za-z]+(Exception|Error):";
-    private static final String LOG_FILE;
+    @Test
+    public void shouldNotFoundExceptionsDuringStartup() throws Exception {
+        for(File log : getFileLogs()) {
+            assertTrue(String.format("File not found: %s.", log.getAbsolutePath()), log.exists());
+            assertTrue("File cannot be read", log.canRead());
+            assertTrue(String.format("Errors appeared during MoTeCH project startup. Please check %s", log.getPath()), log.length() == 0);
+        }
+    }
 
-    static {
+    private File[] getFileLogs() {
+        File[] logs = new File[2];
         StringBuilder currentDir = new StringBuilder(System.getProperty("user.dir"));
         Pattern pattern = Pattern.compile("(.*/)(motech-deployment-tests)(/)?$");
         Matcher matcher = pattern.matcher(currentDir);
@@ -33,45 +34,12 @@ public class DeploymentTestIT {
             currentDir.append("motech-deployment-tests/");
         }
 
-        currentDir.append("target/tomcat/logs/catalina.out");
+        currentDir.append("target/tomcat/logs/");
 
-        LOG_FILE = currentDir.toString();
-    }
+        logs[0] = new File(currentDir.toString() + "catalina.out");
+        logs[1] = new File(currentDir.toString() + "Motech.log");
 
-    @Test
-    @Ignore // TODO enable this test when all bundles will be fixed
-    public void shouldNotFoundExceptionsDuringStartup() throws Exception {
-        List<String> errors = new ArrayList<>();
-        File file = new File(LOG_FILE);
-
-        assertTrue(String.format("File not found: %s.", file.getAbsolutePath()), file.exists());
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            Pattern pattern = Pattern.compile(EXCEPTION_PATTERN);
-            String currentLine = "";
-
-            while ((currentLine = reader.readLine()) != null) {
-                if (pattern.matcher(currentLine).find()) {
-                    errors.add(currentLine);
-                }
-            }
-        }
-
-        assertEquals("Errors appeared during MoTeCH project startup: " + createErrorsList(errors), 0, errors.size());
-    }
-
-    private String createErrorsList(List<String> errors) {
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < errors.size(); ++i) {
-            sb.append("[ ").append(errors.get(i)).append(" ]");
-
-            if (i < errors.size() - 1) {
-                sb.append(" ; ");
-            }
-        }
-
-        return sb.toString();
+        return logs;
     }
 
 }
