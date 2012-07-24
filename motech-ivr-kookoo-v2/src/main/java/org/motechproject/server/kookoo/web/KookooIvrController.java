a3608f8a09d41e020696e267566c871375cbc504
Dileep Bapat <dileepbapat@gmail.com>
2012-07-24 15:45:12 +0530

    Dileep | Propagating session id for kookoo.
    Change-Id: Ic4eb5de74c14f7a62a96c14f53b8c40422380338

--- motech-ivr-kookoo-v2/src/main/java/org/motechproject/server/kookoo/web/KookooIvrController.java
+++ motech-ivr-kookoo-v2/src/main/java/org/motechproject/server/kookoo/web/KookooIvrController.java
@@ -20,16 +20,17 @@ public class KookooIvrController {
     public String handleRequest(HttpServletRequest request) {
         String treeName = request.getParameter("tree");
         String event = request.getParameter("event");
+        String sessionId = request.getParameter("sid");
         String digits = null;
         if ("GotDTMF".equals(event))
             digits = request.getParameter("data");
         String treePath = request.getParameter("trP");
         String language = request.getParameter("ln");
-        return redirectToDecisionTree(treeName, digits, treePath, language, request.getServletPath());
+        return redirectToDecisionTree(treeName, digits, treePath, language, sessionId, request.getServletPath());
     }
 
-    private String redirectToDecisionTree(String treeName, String digits, String treePath, String language, String servletPath) {
+    private String redirectToDecisionTree(String treeName, String digits, String treePath, String language, String sessionId, String servletPath) {
         final String transitionKey = digits == null ? "" : "&trK=" + digits;
-        return format("forward:%s%s?type=kookoo&tree=%s&trP=%s&ln=%s%s", servletPath, DECISIONTREE_URL, treeName, treePath, language, transitionKey).replaceAll("//", "/");
+        return format("forward:%s%s?type=kookoo&tree=%s&trP=%s&ln=%s&flowSessionId=%s%s", servletPath, DECISIONTREE_URL, treeName, treePath, language, sessionId, transitionKey).replaceAll("//", "/");
     }
 }
