ca1e5e261ab8e30c57976d082a127df4a90a1cad
Łukasz Lewczyński <llewczynski@soldevelo.com>
2012-07-26 11:52:34 +0200

    llewczynski | #133 | Split modules into osgi and non-osgi modules
    Change-Id: I0ab01f698fb9f84ddd66e3246dcfa9f264c6dab7

---
+++ motech-rules-api-bundle/src/main/java/org/motechproject/rules/osgi/RuleBundleLoader.java
@@ -0,0 +1,54 @@
+package org.motechproject.rules.osgi;
+
+import org.apache.commons.io.FilenameUtils;
+import org.motechproject.rules.service.KnowledgeBaseManager;
+import org.motechproject.server.osgi.BundleLoader;
+import org.osgi.framework.Bundle;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Enumeration;
+
+/**
+ * Custom log4j configuration loader
+ * <p/>
+ * Merge bundle specific configurations to the existing if a bundle contains
+ * log4j.xml
+ *
+ * @author Ricky Wang
+ */
+public class RuleBundleLoader implements BundleLoader {
+
+    // default rule folder
+    private String ruleFolder = "/rules";
+
+    @Autowired
+    private KnowledgeBaseManager knowledgeBaseManager;
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void loadBundle(Bundle bundle) throws Exception {
+        Enumeration<URL> e = bundle.findEntries(ruleFolder, "*", false);
+        String symbolicName = bundle.getSymbolicName();
+        if (e != null) {
+            while (e.hasMoreElements()) {
+                URL url = e.nextElement();
+                URLConnection conn = url.openConnection();
+                InputStream inputStream = conn.getInputStream();
+                knowledgeBaseManager.addOrUpdateRule(FilenameUtils.getName(url.getFile()), symbolicName, inputStream);
+                inputStream.close();
+            }
+        }
+    }
+
+    public void setRuleFolder(String ruleFolder) {
+        this.ruleFolder = ruleFolder;
+    }
+
+    public void setKnowledgeBaseManager(KnowledgeBaseManager knowledgeBaseManager) {
+        this.knowledgeBaseManager = knowledgeBaseManager;
+    }
+
+}
