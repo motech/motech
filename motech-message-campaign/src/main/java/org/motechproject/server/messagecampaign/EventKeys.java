a1fdc11ec28f0d72d817b2613bfdbc074d644df2
Soham Ghosh <sohamghosh@gmail.com>
2012-08-02 13:17:55 +0530

    manohar| #35| a few fixes. Fixed build failure.
    Change-Id: I888535a88c13c7ccbeef06073a749957743feaaa

--- motech-message-campaign/src/main/java/org/motechproject/server/messagecampaign/EventKeys.java
+++ motech-message-campaign/src/main/java/org/motechproject/server/messagecampaign/EventKeys.java
@@ -1,18 +1,20 @@
 package org.motechproject.server.messagecampaign;
 
-public class EventKeys {
+public final class EventKeys {
 
-    public final static String SCHEDULE_JOB_ID_KEY = "JobID";
-    public final static String CAMPAIGN_NAME_KEY = "CampaignName";
-    public final static String MESSAGE_NAME_KEY = "MessageName";
-    public final static String MESSAGE_FORMATS = "MessageFormats";
-    public final static String MESSAGE_LANGUAGES = "MessageLanguages";
-    public final static String EXTERNAL_ID_KEY = "ExternalID";
+    public static final String SCHEDULE_JOB_ID_KEY = "JobID";
+    public static final String CAMPAIGN_NAME_KEY = "CampaignName";
+    public static final String MESSAGE_NAME_KEY = "MessageName";
+    public static final String MESSAGE_FORMATS = "MessageFormats";
+    public static final String MESSAGE_LANGUAGES = "MessageLanguages";
+    public static final String EXTERNAL_ID_KEY = "ExternalID";
     public static final String MESSAGE_KEY = "MessageKey";    
     public static final String GENERATED_MESSAGE_KEY = "GenMsgKey";
 
-    public final static String BASE_SUBJECT = "org.motechproject.server.messagecampaign.";
-    public final static String SEND_MESSAGE = BASE_SUBJECT + "fired-campaign-message";
-    public final static String CAMPAIGN_COMPLETED = BASE_SUBJECT + "campaign-completed";
+    public static final String BASE_SUBJECT = "org.motechproject.server.messagecampaign.";
+    public static final String SEND_MESSAGE = BASE_SUBJECT + "fired-campaign-message";
+    public static final String CAMPAIGN_COMPLETED = BASE_SUBJECT + "campaign-completed";
 
+    private EventKeys() {
+    }
 }
