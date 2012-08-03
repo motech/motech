41628ce71d0925191b87c2276b117de5a92565e0
Vinkesh Banka <vinkesh.banka@gmail.com>
2012-08-03 12:54:48 +0530

    Banka | Major overhaul of Admin UI. Adding Bootstrap CSS framework.
    Change-Id: I129424c6ebe8ee96d1743e41fba1c26b9bb577c2

--- motech-admin-bundle/src/main/resources/webapp/js/app.js
+++ motech-admin-bundle/src/main/resources/webapp/js/app.js
@@ -3,9 +3,10 @@
 /* App Module */
 
 angular.module('motech-admin', ['bundleServices', 'messageServices', 'platformSettingsServices', 'localization',
-    'motech-widgets', 'moduleSettingsServices', 'ngCookies']).config(['$routeProvider', function($routeProvider) {
+    'motech-widgets', 'moduleSettingsServices', 'ngCookies', 'bootstrap']).config(['$routeProvider', function($routeProvider) {
       $routeProvider.
           when('/bundles', {templateUrl: 'partials/bundles.html', controller: BundleListCtrl}).
+          when('/messages', {templateUrl: 'partials/messages.html', controller: StatusMsgCtrl}).
           when('/settings', {templateUrl: 'partials/settings.html', controller: SettingsCtrl}).
           when('/bundle/:bundleId', {templateUrl: 'partials/bundle.html', controller: ModuleCtrl}).
           when('/bundleSettings/:bundleId', {templateUrl: 'partials/bundleSettings.html', controller: BundleSettingsCtrl}).
