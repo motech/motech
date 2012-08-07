b46e1ff79b04561ccd060679a245885e22a629d5
Paweł Gesek <pgesek@soldevelo.com>
2012-08-07 11:53:55 +0200

    pgesek | #176 | Moving debian packaging to a maven module, mapping Motech to root
    Change-Id: I7f8d38f63b5aa81809ba953d7960ccf54bed73e5

---
+++ motech-deb/src/main/debian/modules/build-modules.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Exit on non-zero exit code
+set -e
+
+export BUNDLE_DIR=usr/share/motech/.motech/bundles
+
+export TMP_DIR=/tmp/motech-packaging-$$
+mkdir $TMP_DIR
+
+export START_DIR=`pwd`
+
+export MOTECH_BASE
+export CONTENT_DIR
+export MOTECH_VERSION
+
+cd $TMP_DIR
+
+$CONTENT_DIR/modules/motech-demo/build.sh
+
+rm -r $TMP_DIR
