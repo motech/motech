72d15a0fa6e0ec7be0ec13214b0c8714d6ff3fff
Łukasz Lewczyński <llewczynski@soldevelo.com>
2012-08-07 11:42:48 +0200

    llewczynski | #132 | Moved: 1) functional modules to modules folder (grouped similar modules) 2) platform components to platform folder
    Change-Id: I8864b0dffe2879a35d8d3147fbebc44901451e5d

---
+++ modules/cmslite/cmslite-api/src/test/java/org/motechproject/cmslite/api/web/ResourceControllerIT.java
@@ -0,0 +1,95 @@
+package org.motechproject.cmslite.api.web;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.motechproject.cmslite.api.model.CMSLiteException;
+import org.motechproject.cmslite.api.model.StreamContent;
+import org.motechproject.cmslite.api.model.StringContent;
+import org.motechproject.cmslite.api.repository.AllStreamContents;
+import org.motechproject.cmslite.api.repository.AllStringContents;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/applicationCmsLiteApi.xml")
+public class ResourceControllerIT {
+
+    public static final String NAME = "background.wav";
+    public static final String LANGUAGE = "en";
+
+    @Mock
+    HttpServletResponse response;
+
+    @Mock
+    ServletOutputStream servletOutputStream;
+
+    @Mock
+    PrintWriter printWriter;
+
+    @Autowired
+    AllStreamContents allStreamContents;
+
+    @Autowired
+    AllStringContents allStringContents;
+
+    @Autowired
+    ResourceController controller;
+
+    @Before
+    public void setUp() throws IOException {
+        initMocks(this);
+    }
+
+    @Test
+    public void shouldGetStreamContent() throws IOException, CMSLiteException {
+        addStreamContent("/" + NAME);
+        when(response.getOutputStream()).thenReturn(servletOutputStream);
+
+        controller.getStreamContent(LANGUAGE, NAME, response);
+
+        verify(response).setStatus(HttpServletResponse.SC_OK);
+        verify(response).setContentType("audio/x-wav");
+        verify(response).setHeader("Accept-Ranges", "bytes");
+        verify(response).setContentLength(240044); // Length of the wav file
+        verify(servletOutputStream).close();
+    }
+
+    @Test
+    public void shouldGetStringContent() throws IOException, CMSLiteException {
+        addStringContent("stringContent");
+        when(response.getWriter()).thenReturn(printWriter);
+
+        controller.getStringContent(LANGUAGE, NAME, response);
+
+        verify(response).setStatus(HttpServletResponse.SC_OK);
+        verify(response).setContentType("text/plain");
+        verify(response).setContentLength(13); // Length of the string text
+        verify(printWriter).print("stringContent");
+        verify(printWriter).close();
+    }
+
+    private void addStreamContent(String pathToFile) throws IOException, CMSLiteException {
+        try (InputStream in = this.getClass().getResourceAsStream(pathToFile)) {
+            StreamContent streamContent = new StreamContent(LANGUAGE, NAME, in, "checksum", "audio/x-wav");
+            allStreamContents.addContent(streamContent);
+        }
+    }
+
+    private void addStringContent(String stringValue) throws CMSLiteException {
+        StringContent stringContent = new StringContent(LANGUAGE, NAME, stringValue);
+        allStringContents.addContent(stringContent);
+    }
+}
--- motech-pillreminder-api/src/main/java/org/motechproject/server/pillreminder/api/builder/MedicineBuilder.java
+++
@@ -1,9 +0,0 @@
-package org.motechproject.server.pillreminder.api.builder;
-
-import org.motechproject.server.pillreminder.api.contract.MedicineRequest;
-import org.motechproject.server.pillreminder.api.domain.Medicine;
-public class MedicineBuilder {
-    public Medicine createFrom(MedicineRequest medicineRequest) {
-        return new Medicine(medicineRequest.getName(), medicineRequest.getStartDate(), medicineRequest.getEndDate());
-    }
-}
