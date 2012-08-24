4f55eb18efdcc7273946c068c05bbe655e92e25c
Łukasz Lewczyński <llewczynski@soldevelo.com>
2012-08-24 10:10:55 +0200

    llewczynski | #236 | Overriding toString() method on MilestoneAlert class
    Change-Id: I7e3b2dae8a964e7a2816770c0ee9636080bf26a5

--- modules/scheduletracking/api/src/test/java/org/motechproject/scheduletracking/api/domain/MilestoneAlertTest.java
+++ modules/scheduletracking/api/src/test/java/org/motechproject/scheduletracking/api/domain/MilestoneAlertTest.java
@@ -13,14 +13,29 @@ public class MilestoneAlertTest {
 
     @Test
     public void shouldCalcuateAndStoreAlertDates() {
-        final Milestone milestone = new Milestone("M1", weeks(1), weeks(1), weeks(1), weeks(1));
+        final String milestoneName = "M1";
+        final Milestone milestone = new Milestone(milestoneName, weeks(1), weeks(1), weeks(1), weeks(1));
         final DateTime referenceDateTime = newDateTime(2000, 1, 1, 0, 0, 0);
         final MilestoneAlert milestoneAlert = MilestoneAlert.fromMilestone(milestone, referenceDateTime);
 
-        assertThat(milestoneAlert.getMilestoneName(), is(equalTo("M1")));
-        assertThat(milestoneAlert.getEarliestDateTime(), is(equalTo(newDateTime(2000, 1, 1, 0, 0, 0))));
-        assertThat(milestoneAlert.getDueDateTime(), is(equalTo(newDateTime(2000, 1, 8, 0, 0, 0))));
-        assertThat(milestoneAlert.getLateDateTime(), is(equalTo(newDateTime(2000, 1, 15, 0, 0, 0))));
-        assertThat(milestoneAlert.getDefaultmentDateTime(), is(equalTo(newDateTime(2000, 1, 22, 0, 0, 0))));
+        final DateTime earliestDateTime = newDateTime(2000, 1, 1, 0, 0, 0);
+        final DateTime dueDateTime = newDateTime(2000, 1, 8, 0, 0, 0);
+        final DateTime lateDateTime = newDateTime(2000, 1, 15, 0, 0, 0);
+        final DateTime defaultmentDateTime = newDateTime(2000, 1, 22, 0, 0, 0);
+
+        String milestoneAlertToString = "MilestoneAlert{" +
+                "milestoneName='" + milestoneName + '\'' +
+                ", earliestDateTime=" + earliestDateTime +
+                ", dueDateTime=" + dueDateTime +
+                ", lateDateTime=" + lateDateTime +
+                ", defaultmentDateTime=" + defaultmentDateTime +
+                '}';
+
+        assertThat(milestoneAlert.getMilestoneName(), is(equalTo(milestoneName)));
+        assertThat(milestoneAlert.getEarliestDateTime(), is(equalTo(earliestDateTime)));
+        assertThat(milestoneAlert.getDueDateTime(), is(equalTo(dueDateTime)));
+        assertThat(milestoneAlert.getLateDateTime(), is(equalTo(lateDateTime)));
+        assertThat(milestoneAlert.getDefaultmentDateTime(), is(equalTo(defaultmentDateTime)));
+        assertThat(milestoneAlert.toString(), is(equalTo(milestoneAlertToString)));
     }
 }
