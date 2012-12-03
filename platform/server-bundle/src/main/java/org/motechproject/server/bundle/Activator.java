c99d6d40d65c1c1a7acf4ef2a0122ed62b338483
Dileep Bapat <dileepbapat@gmail.com>
2012-12-03 12:14:27 +0530

    Dileep,Soham| Using common Activator in server-bundle.
    Change-Id: I4e0ffc029ddc5f45e604b501072e9bb11245b546

--- platform/server-bundle/src/main/java/org/motechproject/server/bundle/Activator.java
+++ platform/server-bundle/src/main/java/org/motechproject/server/bundle/Activator.java
@@ -1,100 +1,12 @@
 package org.motechproject.server.bundle;
 
-import org.motechproject.osgi.web.MotechOsgiWebApplicationContext;
-import org.motechproject.osgi.web.ServletRegistrationException;
-import org.motechproject.server.startup.StartupManager;
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
-import org.osgi.service.http.HttpService;
-import org.osgi.util.tracker.ServiceTracker;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.servlet.DispatcherServlet;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Activator implements BundleActivator {
-    private static Logger logger = LoggerFactory.getLogger(Activator.class);
-    private static final String CONTEXT_CONFIG_LOCATION = "applicationPlatformServerBundle.xml";
-    private static final String SERVLET_URL_MAPPING = "/server";
-    private static final String RESOURCE_URL_MAPPING = String.format("%s/resources", SERVLET_URL_MAPPING);
-    private ServiceTracker tracker;
-    private ServiceReference httpService;
-
-    private static BundleContext bundleContext;
-
-    @Override
-    public void start(BundleContext context) {
-        bundleContext = context;
-
-        this.tracker = new ServiceTracker(context,
-                HttpService.class.getName(), null) {
-
-            @Override
-            public Object addingService(ServiceReference ref) {
-                Object service = super.addingService(ref);
-                serviceAdded((HttpService) service);
-                return service;
-            }
-
-            @Override
-            public void removedService(ServiceReference ref, Object service) {
-                serviceRemoved((HttpService) service);
-                super.removedService(ref, service);
-            }
-        };
-        this.tracker.open();
-    }
-
-    public void stop(BundleContext context) {
-        this.tracker.close();
-
-        if (httpService != null) {
-            HttpService service = (HttpService) context.getService(httpService);
-            serviceRemoved(service);
-        }
-    }
-
-    public static class ServerApplicationContext extends MotechOsgiWebApplicationContext {
-        public ServerApplicationContext() {
-            super();
-            setBundleContext(Activator.bundleContext);
-        }
-    }
-
-    public void serviceAdded(HttpService service) {
-        try {
-            DispatcherServlet dispatcherServlet = new DispatcherServlet();
-            dispatcherServlet.setContextConfigLocation(CONTEXT_CONFIG_LOCATION);
-            dispatcherServlet.setContextClass(ServerApplicationContext.class);
-            ClassLoader old = Thread.currentThread().getContextClassLoader();
-            try {
-                Thread.currentThread().setContextClassLoader(getClass().getClassLoader());
-                service.registerServlet(SERVLET_URL_MAPPING, dispatcherServlet, null, null);
-                service.registerResources(RESOURCE_URL_MAPPING, "/webapp/resources", null);
-                logger.debug("Servlet registered");
-            } finally {
-                Thread.currentThread().setContextClassLoader(old);
-            }
-            startMotech();
-        } catch (Exception e) {
-            throw new ServletRegistrationException(e);
-        }
-    }
-
-    private void serviceRemoved(HttpService service) {
-        service.unregister(SERVLET_URL_MAPPING);
-        logger.debug("Servlet unregistered");
-    }
-
-    private void startMotech() {
-        StartupManager startupManager = StartupManager.getInstance();
-
-        logger.debug("Starting MoTeCH...");
-        startupManager.startup();
-
-        if (startupManager.canLaunchBundles()) {
-            logger.info("Launching MOTECH bundles...");
-            //OsgiListener.getOsgiService().startMotechBundles();
-        }
+public class Activator extends org.motechproject.osgi.web.Activator {
+    protected Map<String, String> resourceMappings() {
+        Map<String, String> mapping = new HashMap<>();
+        mapping.put("/server/resources", "/webapp/resources");
+        return mapping;
     }
 }
