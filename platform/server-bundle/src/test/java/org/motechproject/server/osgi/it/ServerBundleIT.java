7bbbde48479754386928a0b8fc167fbf845817c6
Dileep Bapat <dileepbapat@gmail.com>
2012-12-10 15:18:28 +0530

    Dileep, Soham | 0297 | Server bundle IT.
    Change-Id: I9bd3eca3c0f7a69997870d9e3bfdc12d153ac469

--- platform/server-bundle/src/test/java/org/motechproject/server/osgi/it/ServerBundleIT.java
+++ platform/server-bundle/src/test/java/org/motechproject/server/osgi/it/ServerBundleIT.java
@@ -1,23 +1,39 @@
 package org.motechproject.server.osgi.it;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.motechproject.server.ui.ModuleRegistrationData;
 import org.motechproject.server.ui.UIFrameworkService;
 import org.motechproject.testing.osgi.BaseOsgiIT;
 import org.osgi.framework.ServiceReference;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 public class ServerBundleIT extends BaseOsgiIT {
 
-    public void testStartServer() {
+    public void testUIFrameworkService() {
         ServiceReference serviceReference = bundleContext.getServiceReference(UIFrameworkService.class.getName());
         assertNotNull(serviceReference);
         UIFrameworkService service = (UIFrameworkService) bundleContext.getService(serviceReference);
         assertNotNull(service);
+        final ModuleRegistrationData registrationData = new ModuleRegistrationData("testUIFrameworkService", "/testUIFrameworkService");
+        service.registerModule(registrationData);
+        assertEquals(registrationData, service.getModuleData(registrationData.getModuleName()));
+    }
+
+    public void testController() throws IOException {
+        HttpClient client = new DefaultHttpClient();
+        final String response = client.execute(new HttpGet("http://localhost:8080/server/lang"), new BasicResponseHandler());
+        assertEquals("en", response);
     }
 
     @Override
     protected List<String> getImports() {
+        // Packages in the test jar are excluded by default.
         return Arrays.asList("org.motechproject.server.ui");
     }
 }
