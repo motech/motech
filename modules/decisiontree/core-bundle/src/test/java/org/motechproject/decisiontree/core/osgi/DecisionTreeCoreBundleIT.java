a49436804a6fcdac94d782f34424789641a797e3
Soham Ghosh <sohamghosh@gmail.com>
2012-12-19 13:55:29 +0530

    Soham | 0469 | DecisionTree bundle ITs.
    Change-Id: I614089dbf521355ba692253929db9e0003d10980

---
+++ modules/decisiontree/core-bundle/src/test/java/org/motechproject/decisiontree/core/osgi/DecisionTreeCoreBundleIT.java
@@ -0,0 +1,24 @@
+package org.motechproject.decisiontree.core.osgi;
+
+import org.motechproject.decisiontree.core.DecisionTreeService;
+import org.motechproject.decisiontree.core.model.Tree;
+import org.motechproject.testing.osgi.BaseOsgiIT;
+import org.osgi.framework.ServiceReference;
+
+public class DecisionTreeCoreBundleIT extends BaseOsgiIT {
+
+    public void testDecisionTreeService() {
+        ServiceReference serviceReference = bundleContext.getServiceReference(DecisionTreeService.class.getName());
+        assertNotNull(serviceReference);
+        DecisionTreeService decisionTreeService = (DecisionTreeService) bundleContext.getService(serviceReference);
+        assertNotNull(decisionTreeService);
+        String treeName = "DecisionTreeCoreBundleIT";
+        Tree tree = new Tree().setName(treeName);
+        try {
+            decisionTreeService.saveDecisionTree(tree);
+            assertEquals(tree, decisionTreeService.getDecisionTree(tree.getId()));
+        } finally {
+            decisionTreeService.deleteDecisionTree(tree.getId());
+        }
+    }
+}
---
+++ modules/decisiontree/server-bundle/src/test/java/org/motechproject/decisiontree/server/osgi/DecisionTreeServerBundleIT.java
@@ -0,0 +1,36 @@
+package org.motechproject.decisiontree.server.osgi;
+
+import org.motechproject.decisiontree.core.model.CallStatus;
+import org.motechproject.decisiontree.server.service.DecisionTreeServer;
+import org.motechproject.decisiontree.server.service.FlowSessionService;
+import org.motechproject.testing.osgi.BaseOsgiIT;
+import org.osgi.framework.ServiceReference;
+import org.springframework.web.servlet.ModelAndView;
+
+public class DecisionTreeServerBundleIT extends BaseOsgiIT {
+
+    public void testDecisionTreeServer() {
+        ServiceReference serviceReference = bundleContext.getServiceReference(DecisionTreeServer.class.getName());
+        assertNotNull(serviceReference);
+        DecisionTreeServer decisionTreeServer = (DecisionTreeServer) bundleContext.getService(serviceReference);
+        assertNotNull(decisionTreeServer);
+
+        serviceReference = bundleContext.getServiceReference(FlowSessionService.class.getName());
+        assertNotNull(serviceReference);
+        FlowSessionService flowSessionService = (FlowSessionService) bundleContext.getService(serviceReference);
+        assertNotNull(flowSessionService);
+
+        String sessionId = "123a";
+        String phoneNumber = "1234567890";
+        String provider = "freeivr";
+
+        try {
+            ModelAndView mnv = decisionTreeServer.getResponse(sessionId, phoneNumber, provider, "sometree", CallStatus.Disconnect.toString(), "en");
+            assertNotNull(mnv);
+            assertNotNull(mnv.getViewName());
+            assertTrue(mnv.getViewName().contains(provider));
+        } finally {
+            flowSessionService.removeCallSession(sessionId);
+        }
+    }
+}
