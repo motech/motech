da6f8c18940bcdd030ace0c7ad3b347e32fae515
Dileep Bapat <dileepbapat@gmail.com>
2013-02-14 12:04:14 +0530

    Dileep | 524  | Fixing connect exception on bundle IT
    Change-Id: I503a7441d0f8300b5e48d18e610534ca53e79a61

--- modules/outbox/outbox-bundle/src/test/java/org/motechproject/server/outbox/osgi/it/VoiceOutboxServiceBundleIT.java
+++ modules/outbox/outbox-bundle/src/test/java/org/motechproject/server/outbox/osgi/it/VoiceOutboxServiceBundleIT.java
@@ -1,10 +1,16 @@
 package org.motechproject.server.outbox.osgi.it;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.motechproject.outbox.api.domain.OutboundVoiceMessage;
 import org.motechproject.outbox.api.service.VoiceOutboxService;
 import org.motechproject.testing.osgi.BaseOsgiIT;
+import org.motechproject.testing.utils.HttpServiceCheck;
 import org.osgi.framework.ServiceReference;
 
+import java.io.IOException;
 import java.util.UUID;
 
 public class VoiceOutboxServiceBundleIT extends BaseOsgiIT {
@@ -30,12 +36,12 @@ public class VoiceOutboxServiceBundleIT extends BaseOsgiIT {
         }
     }
 
-    // TODO: Fix org.apache.http.conn.HttpHostConnectException: Connection to http://localhost:8080 refused
-    /*public void testController() throws IOException {
+    public void testController() throws IOException, InterruptedException {
+        HttpServiceCheck.waitForLocalPortToListen(8080, 30);
         HttpClient client = new DefaultHttpClient();
         final String response = client.execute(new HttpGet("http://localhost:8080/outbox/vxml/outboxMessage?pId=123"), new BasicResponseHandler());
         assertTrue(response.contains("There are no pending messages in your outbox"));
-    }*/
+    }
 
     @Override
     protected String[] getConfigLocations() {
