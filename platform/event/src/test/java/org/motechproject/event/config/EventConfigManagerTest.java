227b6ba75c2a23d685078c3014cbdd48f0e98d23
Paweł Gesek <pgesek@soldevelo.com>
2013-03-19 09:36:48 +0100

    pgesek | #581 | Activemq multi-tenant: Prefix queue with current username, use activemq.properties from .motech/conf
    Change-Id: I85f11c7424aa0111dda8e5c757a4c406e4122d82

---
+++ platform/event/src/test/java/org/motechproject/event/config/EventConfigManagerTest.java
@@ -0,0 +1,69 @@
+package org.motechproject.event.config;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static org.junit.Assert.assertEquals;
+
+public class EventConfigManagerTest {
+
+    private EventConfigManager eventConfigManager = new EventConfigManagerImpl();
+
+    private File configFile;
+
+    private String currentUserPrefix;
+
+    @Before
+    public void setUp() throws IOException {
+        currentUserPrefix = System.getProperty("user.name") + "_";
+
+        configFile = File.createTempFile("activemq", "config");
+        FileUtils.writeStringToFile(configFile,
+                "queue.for.events=QueueForEvents\n" + "queue.for.scheduler=QueueForScheduler");
+    }
+
+    @After
+    public void tearDown() {
+        FileUtils.deleteQuietly(configFile);
+    }
+
+    @Test
+    public void shouldLoadActivemqPropertiesFromFs() {
+        eventConfigManager.setActivemqConfigLocation(configFile.getAbsolutePath());
+
+        Properties activeMqConfig = eventConfigManager.getActivemqConfig();
+
+        assertEquals(2, activeMqConfig.size());
+        assertEquals(currentUserPrefix + "QueueForEvents", activeMqConfig.getProperty("queue.for.events"));
+        assertEquals(currentUserPrefix + "QueueForScheduler", activeMqConfig.getProperty("queue.for.scheduler"));
+    }
+
+    @Test
+    public void shouldFallbackToClasspathResource() throws IOException {
+        eventConfigManager.setActivemqConfigLocation(configFile.getAbsolutePath());
+
+        FileUtils.deleteQuietly(configFile);
+
+        Properties activeMqConfig = eventConfigManager.getActivemqConfig();
+
+        Properties activemqConfigFromClasspath = new Properties();
+        try (InputStream in = getClass().getClassLoader().getResourceAsStream("activemq.properties")) {
+            activemqConfigFromClasspath.load(in);
+        }
+
+        // take prefixing with username into consideration
+        activemqConfigFromClasspath.setProperty("queue.for.events", currentUserPrefix +
+                activemqConfigFromClasspath.getProperty("queue.for.events"));
+        activemqConfigFromClasspath.setProperty("queue.for.scheduler", currentUserPrefix +
+                activemqConfigFromClasspath.getProperty("queue.for.scheduler"));
+
+        assertEquals(activemqConfigFromClasspath, activeMqConfig);
+    }
+}
