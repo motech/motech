477bae51b36e64aaae7ee2c5b60899d208722cd1
Łukasz Lewczyński <llewczynski@soldevelo.com>
2013-04-02 12:48:26 +0200

    llewczynski | #504 | Merge alerts-api and alerts-api-bundle
    Change-Id: I24a2932e6c283a5547f1576a7c01fe6ebc3a510b

--- modules/alerts/api/src/test/java/org/motechproject/server/alerts/osgi/AlertsApiBundleIT.java
+++ modules/alerts/api/src/test/java/org/motechproject/server/alerts/osgi/AlertsApiBundleIT.java
@@ -1,4 +1,4 @@
-package org.motechproject.server.alerts.service.osgi;
+package org.motechproject.server.alerts.osgi;
 
 import org.motechproject.server.alerts.contract.AlertCriteria;
 import org.motechproject.server.alerts.contract.AlertService;
@@ -10,6 +10,8 @@ import org.osgi.framework.ServiceReference;
 
 import java.util.List;
 import java.util.UUID;
+
+import static java.util.Arrays.asList;
 
 public class AlertsApiBundleIT extends BaseOsgiIT {
 
@@ -25,5 +27,12 @@ public class AlertsApiBundleIT extends BaseOsgiIT {
         assertTrue(alerts != null && alerts.size() == 1);
         assertEquals(externalId, alerts.get(0).getExternalId());
         // Delete the doc in the post-integration phase
+    }
+
+    @Override
+    protected List<String> getImports() {
+        return asList(
+                "org.motechproject.server.alerts.domain"
+        );
     }
 }
