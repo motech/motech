9f25f05ccf0d38d9f3ef1325d2a0d78d16bcfe66
Russell <russell.gillenjr@maine.edu>
2013-05-15 14:59:00 -0400

    RGillen | #777 | IVR controller should consider a call inbound only if flow session does not already exist
    Change-Id: I0d295dce39a73aa213c88fb5fb7f4eb289792895

--- modules/ivr/verboice/src/test/java/org/motechproject/server/verboice/web/VerboiceIVRControllerTest.java
+++ modules/ivr/verboice/src/test/java/org/motechproject/server/verboice/web/VerboiceIVRControllerTest.java
@@ -8,11 +8,9 @@ import org.motechproject.callflow.domain.FlowSessionRecord;
 import org.motechproject.callflow.service.CallFlowServer;
 import org.motechproject.callflow.service.FlowSessionService;
 import org.motechproject.decisiontree.core.FlowSession;
-import org.motechproject.server.verboice.VerboiceIVRService;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
-
 import static junit.framework.Assert.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -22,9 +20,6 @@ public class VerboiceIVRControllerTest {
 
     @InjectMocks
     private VerboiceIVRController verboiceIvrController = new VerboiceIVRController();
-
-    @Mock
-    private VerboiceIVRService verboiceIVRService;
     @Mock
     private FlowSessionService flowSessionService;
     @Mock
@@ -92,27 +87,5 @@ public class VerboiceIVRControllerTest {
         verboiceIvrController.handle(request, new MockHttpServletResponse());
 
         verify(flowSessionService).findOrCreate("123a", "1234567890");
-    }
-
-    @Test
-    public void shouldupdateSessionWithVerboiceSidForOutgoingCallback() {
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        request.setParameter("motech_call_id", "123a");
-        request.setParameter("CallSid", "456b");
-        request.setParameter("From", "1234567890");
-        request.setParameter("tree", "sometree");
-        request.setParameter("ln", "en");
-
-        FlowSession flowSession = new FlowSessionRecord("123a", "1234567890");
-        when(flowSessionService.getSession("123a")).thenReturn(flowSession);
-        FlowSession newFlowSession = new FlowSessionRecord("123a", "1234567890");
-        when(flowSessionService.updateSessionId("123a", "456b")).thenReturn(newFlowSession);
-
-        ModelAndView view = new ModelAndView();
-        when(decisionTreeServer.getResponse("456b", "1234567890", "verboice", "sometree", null, "en")).thenReturn(view);
-
-        verboiceIvrController.handle(request, new MockHttpServletResponse());
-
-        verify(flowSessionService).updateSessionId("123a", "456b");
     }
 }
