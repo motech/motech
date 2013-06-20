297657bf9eff3cfc993496f2ac97c66e18007356
Sebastian Brudzinski <sbrudzinski@soldevelo.com>
2013-06-20 10:05:51 +0200

    830 | BUG: 'Admin User' role should be renamed 'User Admin'
    https://trello.com/c/XypOURHN
    
    Renamed the role and ensured that exisiting roles will be renamed.
    
    Change-Id: If21138e136674a68d636a259069aefd623346349

--- platform/web-security/web-security/src/main/java/org/motechproject/security/Initialize.java
+++ platform/web-security/web-security/src/main/java/org/motechproject/security/Initialize.java
@@ -1,21 +1,26 @@
 package org.motechproject.security;
 
 import org.ektorp.CouchDbConnector;
+import org.motechproject.security.domain.MotechUser;
+import org.motechproject.security.domain.MotechRole;
 import org.motechproject.security.domain.MotechPermission;
 import org.motechproject.security.domain.MotechPermissionCouchdbImpl;
-import org.motechproject.security.domain.MotechRole;
 import org.motechproject.security.domain.MotechRoleCouchdbImpl;
 import org.motechproject.security.repository.AllMotechPermissions;
 import org.motechproject.security.repository.AllMotechRoles;
+import org.motechproject.security.repository.AllMotechUsers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 
 
 public class Initialize {
     private static final String WEB_SECURITY = "websecurity";
+    private static final String USER_ADMIN = "User Admin";
+    private static final String ADMIN_USER = "Admin User";
 
     @Autowired
     private AllMotechPermissions allMotechPermissions;
@@ -24,27 +29,61 @@ public class Initialize {
     private AllMotechRoles allMotechRoles;
 
     @Autowired
+    private AllMotechUsers allMotechUsers;
+
+
+    @Autowired
     public void initialize(@Qualifier("webSecurityDbConnector") CouchDbConnector db) throws IOException {
+
+        //change role name only if it doesn't exist yet, to prevent multiple roles of the same name
+        MotechRole userAdminRole = null;
+        userAdminRole = allMotechRoles.findByRoleName(USER_ADMIN);
+        if (userAdminRole==null) {
+            changeRoleNameToUserAdmin();
+        }
+
         //initialize startup permission for admin user
-        MotechPermission addUserPerrmision = new MotechPermissionCouchdbImpl("addUser", WEB_SECURITY);
+        MotechPermission addUserPermission = new MotechPermissionCouchdbImpl("addUser", WEB_SECURITY);
         MotechPermission editUserPermission = new MotechPermissionCouchdbImpl("editUser", WEB_SECURITY);
         MotechPermission deleteUserPermission = new MotechPermissionCouchdbImpl("deleteUser", WEB_SECURITY);
-        MotechPermission manageUserPerrmision = new MotechPermissionCouchdbImpl("manageUser", WEB_SECURITY);
-        MotechPermission activeUserPerrmision = new MotechPermissionCouchdbImpl("activateUser", WEB_SECURITY);
+        MotechPermission manageUserPermission = new MotechPermissionCouchdbImpl("manageUser", WEB_SECURITY);
+        MotechPermission activeUserPermission = new MotechPermissionCouchdbImpl("activateUser", WEB_SECURITY);
         MotechPermission manageRolePermission = new MotechPermissionCouchdbImpl("manageRole", WEB_SECURITY);
 
         //initialize startup role
-        MotechRole adminUser = new MotechRoleCouchdbImpl("Admin User", Arrays.asList(addUserPerrmision.getPermissionName(), editUserPermission.getPermissionName(), deleteUserPermission.getPermissionName(), manageUserPerrmision.getPermissionName(), activeUserPerrmision.getPermissionName(), manageRolePermission.getPermissionName()));
+        MotechRole userAdmin = new MotechRoleCouchdbImpl(USER_ADMIN, Arrays.asList(addUserPermission.getPermissionName(), editUserPermission.getPermissionName(), deleteUserPermission.getPermissionName(), manageUserPermission.getPermissionName(), activeUserPermission.getPermissionName(), manageRolePermission.getPermissionName()));
 
         //create all startup security
-        allMotechPermissions.add(addUserPerrmision);
+        allMotechPermissions.add(addUserPermission);
         allMotechPermissions.add(editUserPermission);
         allMotechPermissions.add(deleteUserPermission);
-        allMotechPermissions.add(manageUserPerrmision);
-        allMotechPermissions.add(activeUserPerrmision);
+        allMotechPermissions.add(manageUserPermission);
+        allMotechPermissions.add(activeUserPermission);
         allMotechPermissions.add(manageRolePermission);
 
-        allMotechRoles.add(adminUser);
+        allMotechRoles.add(userAdmin);
+    }
+
+    //This method fixes a bug with role names - renames Admin User role to User Admin, if it exists in the database
+    private void changeRoleNameToUserAdmin() {
+        MotechRole toChange = null;
+        toChange = allMotechRoles.findByRoleName(ADMIN_USER);
+
+        if (toChange != null) {
+            toChange.setRoleName(USER_ADMIN);
+            allMotechRoles.update(toChange);
+
+            //if exisiting user has got the Admin User role, change it to the proper one
+            List<MotechUser> motechUserList = (List<MotechUser>) allMotechUsers.findByRole(ADMIN_USER);
+
+            for (MotechUser user : motechUserList) {
+                List<String> roles = user.getRoles();
+                roles.remove(ADMIN_USER);
+                roles.add(USER_ADMIN);
+                user.setRoles(roles);
+                allMotechUsers.update(user);
+            }
+        }
     }
 
 }
