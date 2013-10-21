bd0bab7b8dc54552e285f89fa08fc69f8f77388a
Ben Heasly <benjamin.heasly@gmail.com>
2013-10-21 13:11:56 -0400

    HTTP ITs for hello world service and for status controller
    Change-Id: I91426a4a68d1ad99aabc4d5231443d243bdca67b

--- utils/archetypes/http-bundle/src/test/java/mini/http/bundle/service/it/HelloWorldServiceIT.java
+++ utils/archetypes/http-bundle/src/test/java/mini/http/bundle/service/it/HelloWorldServiceIT.java
@@ -1,45 +1,51 @@
 package mini.http.bundle.service.it;
 
 import java.io.IOException;
-
-import mini.http.bundle.service.HelloWorldService;
-
 import org.apache.commons.codec.binary.Base64;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.motechproject.testing.osgi.BaseOsgiIT;
+import org.apache.commons.httpclient.HttpStatus;
 import org.osgi.framework.ServiceReference;
+import org.motechproject.testing.osgi.BaseOsgiIT;
 import org.motechproject.testing.utils.PollingHttpClient;
 import org.motechproject.testing.utils.TestContext;
-import org.apache.commons.httpclient.HttpStatus;
+
+import mini.http.bundle.service.HelloWorldService;
 
 /**
- * Verify that HelloWorldService present, functional.
+ * Verify that HelloWorldService is present and functional.
  */
 public class HelloWorldServiceIT extends BaseOsgiIT {
+    private static final String ADMIN_USERNAME = "motech";
+    private static final String ADMIN_PASSWORD = "motech";
 
     private PollingHttpClient httpClient = new PollingHttpClient(new DefaultHttpClient(), 5);
 
     public void testHelloWorldServicePresent() throws Exception {
-
         ServiceReference registryReference = bundleContext.getServiceReference(HelloWorldService.class.getName());
         assertNotNull(registryReference);
         HelloWorldService helloService = (HelloWorldService) bundleContext.getService(registryReference);
         assertNotNull(helloService);
-
         assertNotNull(helloService.sayHello());
     }
 
-    public void testHelloWorldHttpPresent() throws IOException, InterruptedException {
-
-        HttpGet httpGet = new HttpGet(String.format("http://localhost:%d/http-bundle/web-api/status",
+    public void testHelloWorldGetRequest() throws IOException, InterruptedException {
+        HttpGet httpGet = new HttpGet(String.format("http://localhost:%d/http-bundle/sayHello",
                 TestContext.getJettyPort()));
-
-        addAuthHeader(httpGet, "motech", "motech");
+        addAuthHeader(httpGet, ADMIN_USERNAME, ADMIN_PASSWORD);
 
         HttpResponse response = httpClient.execute(httpGet);
-        
+        assertNotNull(response);
+        assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
+    }
+
+    public void testStatusGetRequest() throws IOException, InterruptedException {
+        HttpGet httpGet = new HttpGet(String.format("http://localhost:%d/http-bundle/web-api/status",
+                TestContext.getJettyPort()));
+        addAuthHeader(httpGet, ADMIN_USERNAME, ADMIN_PASSWORD);
+
+        HttpResponse response = httpClient.execute(httpGet);
         assertNotNull(response);
         assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
     }
