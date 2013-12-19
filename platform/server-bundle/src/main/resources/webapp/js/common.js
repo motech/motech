0cdfda7836ffe1c167bbb5a9234c622ba9b0e5bc
Paweł Gesek <pgesek@soldevelo.com>
2013-12-19 11:29:50 +0100

    Created a helper function for the default layout directive
    Change-Id: Id1d9078739c27418bf02044e3b673646255dc0f1

--- platform/server-bundle/src/main/resources/webapp/js/common.js
+++ platform/server-bundle/src/main/resources/webapp/js/common.js
@@ -99,3 +99,48 @@ function captureTyping(callback) {
     typingTimer = setTimeout(callback, doneTypingInterval);
 }
 
+function defaultLayout() {
+    'use strict';
+    return function() {
+        return {
+            restrict: 'EA',
+            link: function(scope, elm, attrs) {
+                var eastSelector;
+                /*
+                * Define options for inner layout
+                */
+                scope.innerLayoutOptions = {
+                    name: 'innerLayout',
+                    resizable: true,
+                    slidable: true,
+                    closable: true,
+                    east__paneSelector: "#inner-east",
+                    center__paneSelector: "#inner-center",
+                    east__spacing_open: 6,
+                    spacing_closed: 35,
+                    east__size: 300,
+                    showErrorMessages: true, // some panes do not have an inner layout
+                    resizeWhileDragging: true,
+                    center__minHeight: 100,
+                    contentSelector: ".ui-layout-content",
+                    togglerContent_open: '',
+                    togglerContent_closed: '<div><i class="icon-caret-left button"></i></div>',
+                    autoReopen: false, // auto-open panes that were previously auto-closed due to 'no room'
+                    noRoom: true,
+                    togglerAlign_closed: "top", // align to top of resizer
+                    togglerAlign_open: "top",
+                    togglerLength_open: 0,
+                    togglerLength_closed: 35,
+                    togglerTip_open: "Close This Pane",
+                    togglerTip_closed: "Open This Pane",
+                    east__initClosed: true,
+                    initHidden: true
+                    //isHidden: true
+                };
+
+                // create the page-layout, which will ALSO create the tabs-wrapper child-layout
+                scope.innerLayout = elm.layout(scope.innerLayoutOptions);
+            }
+        };
+    };
+}
