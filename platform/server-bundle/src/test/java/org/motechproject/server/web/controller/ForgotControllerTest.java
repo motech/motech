f5645c750fd38be336efaaa855cda0f021e2aafe
Sebastian Malecki <smalecki@soldevelo.com>
2014-01-08 15:13:28 +0100

    AUL: transition to AngularJS
    https://trello.com/c/zHhDjr61
    
    *add testValidView in ForgotControllerTest
    *remove repeated "recoveryService" form blueprint.xml
    *remove unused ContextPath from ForgotController
    
    Change-Id: Iff4d166c82be4da3c49cdc0becd2cc5bfb563824

--- platform/server-bundle/src/test/java/org/motechproject/server/web/controller/ForgotControllerTest.java
+++ platform/server-bundle/src/test/java/org/motechproject/server/web/controller/ForgotControllerTest.java
@@ -1,5 +1,6 @@
 package org.motechproject.server.web.controller;
 
+import org.hamcrest.core.Is;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -10,12 +11,14 @@ import org.motechproject.security.service.PasswordRecoveryService;
 import org.motechproject.server.config.SettingsFacade;
 import org.motechproject.server.config.domain.LoginMode;
 import org.motechproject.server.config.domain.MotechSettings;
+import org.motechproject.server.web.dto.ForgotViewData;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Locale;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -31,13 +34,16 @@ public class ForgotControllerTest {
     private CookieLocaleResolver cookieLocaleResolver;
 
     @Mock
-    private HttpServletRequest request;
+    private HttpServletRequest httpServletRequest;
 
     @Mock
     private SettingsFacade settingsFacade;
 
     @Mock
     private MotechSettings motechSettings;
+
+    @Mock
+    private HttpServletRequest request;
 
     @InjectMocks
     private ForgotController controller = new ForgotController();
@@ -52,13 +58,20 @@ public class ForgotControllerTest {
     public void testForgotPost() throws UserNotFoundException {
         when(cookieLocaleResolver.resolveLocale(request)).thenReturn(Locale.ENGLISH);
         when(motechSettings.getLoginMode()).thenReturn(LoginMode.REPOSITORY);
-
         controller.forgotPost(EMAIL);
 
         verify(recoveryService).passwordRecoveryRequest(EMAIL);
     }
 
 
+    @Test
+    public void tesValidView()  {
+        ForgotViewData result = controller.getForgotViewData(httpServletRequest);
+
+        assertEquals(result.getEmail(), "");
+        assertThat(result.isEmailGetter(), Is.is(true));
+        assertThat(result.isProcessed(), Is.is(false));
+    }
 
     @Test
     public void testInvalidEmail() throws UserNotFoundException {
