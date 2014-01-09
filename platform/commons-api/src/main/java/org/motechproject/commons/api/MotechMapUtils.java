44a608935ab7132b33cd48f193de87967d3b0b8a
Sebastian Brudzinski <sbrudzinski@soldevelo.com>
2014-01-09 16:24:25 +0100

    BUG: Properties were always overwritten at server startup in FILE mode
    https://trello.com/c/MKB9o6bx
    
    In file mode, module properties were always getting overwritten by
    default module properties at server startup. From this commit on,
    this should not be a case. At server startup, all existing properties
    should be preserved and if there are any new properties, only the new
    ones should get added with their default values.
    
    Change-Id: Ib61e28cb36e926579de64b0b8276466cd83239f3

--- platform/commons-api/src/main/java/org/motechproject/commons/api/MotechMapUtils.java
+++ platform/commons-api/src/main/java/org/motechproject/commons/api/MotechMapUtils.java
@@ -2,6 +2,7 @@ package org.motechproject.commons.api;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * The <code>MotechMapUtils</code> class contains methods that allow modifications and operations on maps
@@ -32,6 +33,20 @@ public final class MotechMapUtils {
             mergedMap.putAll(overridingMap);
         }
         return mergedMap;
+    }
+
+    /**
+     * Converts java.util.Map into java.util.Properties
+     * @param map Map to convert
+     * @return Properties, created from given map
+     */
+    public static Properties asProperties(Map<Object, Object> map) {
+        Properties properties = new Properties();
+        for (Map.Entry<Object, Object> entry : map.entrySet()) {
+            properties.put(entry.getKey(), entry.getValue());
+        }
+
+        return properties;
     }
 
 }
