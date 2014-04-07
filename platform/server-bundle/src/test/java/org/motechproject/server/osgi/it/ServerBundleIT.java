ff4e5616378ae332a0aa0551a762392d2c348faa
Paweł Gesek <pgesek@soldevelo.com>
2014-04-07 15:36:28 +0200

    Server-bundle done
    Change-Id: I3bb8f64c0876954c3d5cc25ddc296f230cc1b039

--- platform/server-bundle/src/test/java/org/motechproject/server/osgi/it/ServerBundleIT.java
+++ platform/server-bundle/src/test/java/org/motechproject/server/osgi/it/ServerBundleIT.java
@@ -1,33 +1,46 @@
 package org.motechproject.server.osgi.it;
 
 import org.apache.http.impl.client.BasicResponseHandler;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.motechproject.osgi.web.ModuleRegistrationData;
 import org.motechproject.osgi.web.UIFrameworkService;
-import org.motechproject.testing.osgi.BaseOsgiIT;
-import org.motechproject.testing.utils.PollingHttpClient;
-import org.motechproject.testing.utils.TestContext;
+import org.motechproject.testing.osgi.BasePaxIT;
+import org.motechproject.testing.osgi.TestContext;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
 
+import javax.inject.Inject;
 import java.io.IOException;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-public class ServerBundleIT extends BaseOsgiIT {
 
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class ServerBundleIT extends BasePaxIT {
+
+    @Inject
+    private UIFrameworkService uiFrameworkService;
+
+    @Override
+    protected boolean startHttpServer() {
+        return true;
+    }
+
+    @Test
     public void testUIFrameworkService() throws IOException, InterruptedException {
-        UIFrameworkService uiFrameworkService = getService(UIFrameworkService.class);
-
         final ModuleRegistrationData registrationData = new ModuleRegistrationData("testUIFrameworkService", "/testUIFrameworkService");
         uiFrameworkService.registerModule(registrationData);
         assertEquals(registrationData, uiFrameworkService.getModuleData(registrationData.getModuleName()));
     }
 
+    @Test
     public void testThatControllerIsUp() throws IOException, InterruptedException {
-        String response = new PollingHttpClient().get(String.format("http://localhost:%d/server/lang/list",
+        String response = getHttpClient().get(String.format("http://localhost:%d/server/lang/list",
                 TestContext.getJettyPort()), new BasicResponseHandler());
         assertTrue(response.contains("en"));
-    }
-
-    @Override
-    protected String[] getConfigLocations() {
-        return new String[]{"/META-INF/spring/testApplicationPlatformServerBundle.xml"};
     }
 }
