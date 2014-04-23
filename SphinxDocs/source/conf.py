1a43172f10ddba934350f0088b2417b339c1370d
Tobin Holliday <tobstobs@gmail.com>
2014-04-23 14:38:56 -0700

    MOTECH-803 Check in Sphinx configuration and scripts to platform
    Modified conf.py to point to index.txt as the main document, removed references to javasphinx, changed authors to be Grameen Foundation
    
    Change-Id: I2c20a365e1552fed68e7c69eb76b13fe157389c6

--- SphinxDocs/source/conf.py
+++ SphinxDocs/source/conf.py
@@ -53,8 +53,8 @@ source_suffix = '.txt'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Motech'
-copyright = u'2014, Motech'
+project = u'MOTECH'
+copyright = u'2014, Grameen Foundation'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -206,8 +206,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'Motech.tex', u'Motech Documentation',
-   u'motech', 'manual'),
+  ('index', 'Motech.tex', u'MOTECH Documentation',
+   u'Grameen Foundation', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -236,7 +236,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'motech', u'Motech Documentation',
+    ('index', 'MOTECH', u'MOTECH Documentation',
      [u'Motechstatic'], 1)
 ]
 
@@ -250,8 +250,8 @@ man_show_urls = False
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'Motech', u'Motech Documentation',
-   u'Motech', 'Motech', 'One line description of project.',
+  ('index', 'MOTECH', u'MOTECH Documentation',
+   u'Grameen Foundation', 'MOTECH', 'One line description of project.',
    'Miscellaneous'),
 ]
 
