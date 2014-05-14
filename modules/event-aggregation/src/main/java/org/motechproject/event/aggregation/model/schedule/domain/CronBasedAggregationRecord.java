4e1b1dc172d054ac8e8453bda1d46ebc8b8fdd74
Rob LaRubbio <rob@larubbio.org>
2014-05-14 22:41:33 +0000

    MOTECH-1004	| Rationalize Event Aggregation
        Move some domain classes to new packages so they are no longer exposed.
        Also remove one package from the list of exported packages.  I'd also like
        to not export the event package but doing that fails an integration test.
        There wasn't any error message so it was unclear to me what the cause might
        have been.
    
        Also remove the redundant event-aggregation directory
    
    Change-Id: If014c8b2eadd85ec24a38fe6eb8ec64e50d3a3f2

---
+++ modules/event-aggregation/src/main/java/org/motechproject/event/aggregation/model/schedule/domain/CronBasedAggregationRecord.java
@@ -0,0 +1,60 @@
+package org.motechproject.event.aggregation.model.schedule.domain;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.annotate.JsonProperty;
+import org.motechproject.event.aggregation.model.schedule.CronBasedAggregation;
+import org.motechproject.event.aggregation.model.schedule.CronBasedAggregationRequest;
+
+public class CronBasedAggregationRecord extends AggregationScheduleRecord implements CronBasedAggregation {
+    private static final long serialVersionUID = 5729041837895097802L;
+    @JsonProperty
+    private String cronExpression;
+
+    public CronBasedAggregationRecord() {
+    }
+
+    public CronBasedAggregationRecord(CronBasedAggregationRequest request) {
+        this.cronExpression = request.getCronExpression();
+    }
+
+    public CronBasedAggregationRecord(String cronExpression) {
+        this.cronExpression = cronExpression;
+    }
+
+    @JsonIgnore
+    @Override
+    public String getCronExpression() {
+        return cronExpression;
+    }
+
+    @JsonIgnore
+    public void setCronExpression(String cronExpression) {
+        this.cronExpression = cronExpression;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof CronBasedAggregationRecord)) {
+            return false;
+        }
+
+        CronBasedAggregationRecord that = (CronBasedAggregationRecord) o;
+
+        if (!cronExpression.equals(that.cronExpression)) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return cronExpression.hashCode();
+    }
+}
---
+++ modules/event-aggregation/src/main/java/org/motechproject/event/aggregation/model/schedule/domain/PeriodicAggregationRecord.java
@@ -0,0 +1,67 @@
+package org.motechproject.event.aggregation.model.schedule.domain;
+
+import org.codehaus.jackson.annotate.JsonProperty;
+import org.joda.time.DateTime;
+import org.joda.time.Period;
+import org.motechproject.commons.date.util.DateUtil;
+import org.motechproject.event.aggregation.model.schedule.PeriodicAggregation;
+
+public class PeriodicAggregationRecord extends AggregationScheduleRecord implements PeriodicAggregation {
+    private static final long serialVersionUID = 3403337605865847986L;
+
+    private Period period;
+    private DateTime startDate;
+
+    public PeriodicAggregationRecord() {
+    }
+
+    public PeriodicAggregationRecord(Period period, DateTime startDate) {
+        this.period = period;
+        this.startDate = startDate;
+    }
+
+    @Override
+    public Period getPeriod() {
+        return period;
+    }
+
+    @Override
+    @JsonProperty("startDate")
+    public DateTime getStartTime() {
+        return DateUtil.setTimeZoneUTC(startDate);
+    }
+
+    public void setPeriod(Period period) {
+        this.period = period;
+    }
+
+    public void setStartDate(DateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof PeriodicAggregationRecord)) {
+            return false;
+        }
+
+        PeriodicAggregationRecord that = (PeriodicAggregationRecord) o;
+
+        if (!period.equals(that.period)) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return period.hashCode();
+    }
+}
