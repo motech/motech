f228a70b4067a69a150923ed00aa4dc246474a32
haritha <reddy.haritha@gmail.com>
2014-05-28 16:00:47 +0530

    format

--- modules/hub/hub/src/main/java/org/motechproject/hub/service/impl/ContentDistributionServiceImpl.java
+++ modules/hub/hub/src/main/java/org/motechproject/hub/service/impl/ContentDistributionServiceImpl.java
@@ -29,21 +29,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ContentDistributionServiceImpl implements	ContentDistributionService {
+public class ContentDistributionServiceImpl implements
+		ContentDistributionService {
 
 	private final static Logger LOGGER = Logger.getLogger(HubController.class);
-	
 
 	private HubTopicMDSService hubTopicMDSService;
-	
+
 	private HubDistributionErrorMDSService distributionErrorMDSService;
-	
+
 	private HubPublisherTransactionMDSService hubPublisherTransactionMDSService;
-	
+
 	private HubSubscriptionMDSService hubSubscriptionMDSService;
-	
+
 	private HubSubscriberTransactionMDSService hubSubscriberTransactionMDSService;
-	
+
 	private HubDistributionStatusMDSService hubDistributionStatusMDSService;
 
 	public HubSubscriptionMDSService getHubSubscriptionMDSService() {
@@ -62,18 +62,16 @@ public class ContentDistributionServiceImpl implements	ContentDistributionServic
 	public void setHubTopicService(HubTopicMDSService hubTopicService) {
 		this.hubTopicMDSService = hubTopicService;
 	}
-	
-	
+
 	@Autowired
 	private DistributionServiceDelegate distributionServiceDelegate;
 
 	@Value("${max.retry.count}")
 	private String maxRetryCount;
 
-	public void setMaxRetryCount(String count)	{
+	public void setMaxRetryCount(String count) {
 		this.maxRetryCount = count;
 	}
-	
 
 	public DistributionServiceDelegate getDistributionServiceDelegate() {
 		return distributionServiceDelegate;
@@ -92,57 +90,66 @@ public class ContentDistributionServiceImpl implements	ContentDistributionServic
 	public void distribute(String url) {
 		List<HubTopic> hubTopics = hubTopicMDSService.findByTopicUrl(url);
 		int topicId = -1;
-		if(hubTopics == null || hubTopics.isEmpty() ) {
+		if (hubTopics == null || hubTopics.isEmpty()) {
 			LOGGER.error("No Hub topics for the url " + url);
-			
-		} else if (hubTopics.size()  > 1 ) {
+
+		} else if (hubTopics.size() > 1) {
 			LOGGER.error("Multiple hub topics for the url " + url);
 		} else {
-			topicId = (int)hubTopicMDSService.getDetachedField(hubTopics.get(0), "id");
+			topicId = (int) hubTopicMDSService.getDetachedField(
+					hubTopics.get(0), "id");
 		}
-		
+
 		if (hubTopics == null) {
 			HubTopic hubTopic = new HubTopic();
 			hubTopic.setTopicUrl(url);
 			hubTopic = hubTopicMDSService.create(hubTopic);
-			topicId = (int)hubTopicMDSService.getDetachedField(hubTopic, "id");
+			topicId = (int) hubTopicMDSService.getDetachedField(hubTopic, "id");
 			System.out.println(topicId);
-			
+
 		}
-		
+
 		HubPublisherTransaction publisherTransaction = new HubPublisherTransaction();
 		publisherTransaction.setHubTopicId(topicId);
 		publisherTransaction.setNotificationTime(HubUtils.getCurrentDateTime());
 		hubPublisherTransactionMDSService.create(publisherTransaction);
 
 		// Get the content
-		ResponseEntity<String> response = distributionServiceDelegate.getContent(url);
-		
+		ResponseEntity<String> response = distributionServiceDelegate
+				.getContent(url);
+
 		// Ignore any status code other than 2xx
 		if (response != null && response.getStatusCode().value() / 100 == 2) {
 			String content = response.getBody();
 			MediaType contentType = response.getHeaders().getContentType();
-			List<HubSubscription> subscriptionList = 
-					hubSubscriptionMDSService.findByTopic(topicId);
+			List<HubSubscription> subscriptionList = hubSubscriptionMDSService
+					.findByTopic(topicId);
 			for (HubSubscription subscription : subscriptionList) {
-				int subscriptionId = (int)hubSubscriptionMDSService.getDetachedField(subscription, "id");
-				
+				int subscriptionId = (int) hubSubscriptionMDSService
+						.getDetachedField(subscription, "id");
+
 				int retryCount = 0;
 				DistributionStatusLookup statusLookup = DistributionStatusLookup.FAILURE;
-				int subscriptionStatusId = subscription.getHubSubscriptionStatusId();
-				if (subscriptionStatusId == 
-						SubscriptionStatusLookup.INTENT_VERIFIED.getId()) {
-					//distribute the content
+				int subscriptionStatusId = subscription
+						.getHubSubscriptionStatusId();
+				if (subscriptionStatusId == SubscriptionStatusLookup.INTENT_VERIFIED
+						.getId()) {
+					// distribute the content
 					String callbackUrl = subscription.getCallbackUrl();
 					ResponseEntity<String> distributionResponse = null;
 					do {
-						distributionResponse = distributionServiceDelegate.distribute(callbackUrl, content, contentType, url);
-						if (distributionResponse == null || distributionResponse.getStatusCode().value() / 100 != 2) {
+						distributionResponse = distributionServiceDelegate
+								.distribute(callbackUrl, content, contentType,
+										url);
+						if (distributionResponse == null
+								|| distributionResponse.getStatusCode().value() / 100 != 2) {
 							HubDistributionError error = new HubDistributionError();
 							error.setHubSubscriptionId(subscriptionId);
 							String errorDescription = "Unknown error";
-							if (distributionResponse != null && distributionResponse.getBody() != null) {
-								errorDescription = distributionResponse.getBody();
+							if (distributionResponse != null
+									&& distributionResponse.getBody() != null) {
+								errorDescription = distributionResponse
+										.getBody();
 							}
 							error.setErrorDescription(errorDescription);
 							distributionErrorMDSService.create(error);
@@ -152,20 +159,22 @@ public class ContentDistributionServiceImpl implements	ContentDistributionServic
 							break;
 						}
 					} while (retryCount <= Integer.parseInt(maxRetryCount));
-					
+
 					if (statusLookup.equals(DistributionStatusLookup.FAILURE)) {
 						retryCount--;
 					}
-					
+
 				}
 				HubSubscriberTransaction subscriberTransaction = new HubSubscriberTransaction();
 				subscriberTransaction.setHubSubscriptionId(subscriptionId);
-				
-				subscriberTransaction.setHubDistributionStatusId(statusLookup.getId());
+
+				subscriberTransaction.setHubDistributionStatusId(statusLookup
+						.getId());
 				subscriberTransaction.setRetryCount(retryCount);
 				subscriberTransaction.setContentType(contentType.toString());
 				subscriberTransaction.setContent(content);
-				hubSubscriberTransactionMDSService.create(subscriberTransaction);
+				hubSubscriberTransactionMDSService
+						.create(subscriberTransaction);
 			}
 		}
 	}
