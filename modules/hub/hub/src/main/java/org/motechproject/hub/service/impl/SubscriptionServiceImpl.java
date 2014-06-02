fdd58b1306dc3ae75e5e92f6a3d91fe571616224
haritha <reddy.haritha@gmail.com>
2014-06-02 10:59:47 +0530

    moved from hibernate to mds for above 2 servicess..there are some TODO's ..dependancies on mds...

--- modules/hub/hub/src/main/java/org/motechproject/hub/service/impl/SubscriptionServiceImpl.java
+++ modules/hub/hub/src/main/java/org/motechproject/hub/service/impl/SubscriptionServiceImpl.java
@@ -3,13 +3,14 @@ package org.motechproject.hub.service.impl;
 import java.util.Date;
 import java.util.List;
 
-
-
-
+import org.apache.log4j.Logger;
 //import org.hibernate.SessionFactory;
 import org.motechproject.hub.exception.ApplicationErrors;
 import org.motechproject.hub.exception.HubException;
+import org.motechproject.hub.mds.HubSubscription;
+import org.motechproject.hub.mds.HubSubscriptionStatus;
 import org.motechproject.hub.mds.HubTopic;
+import org.motechproject.hub.mds.service.HubSubscriptionMDSService;
 import org.motechproject.hub.mds.service.HubTopicMDSService;
 import org.motechproject.hub.model.Modes;
 import org.motechproject.hub.model.SubscriptionStatusLookup;
@@ -19,6 +20,7 @@ import org.motechproject.hub.model.SubscriptionStatusLookup;
 //import org.motechproject.hub.repository.TopicRepository;
 import org.motechproject.hub.service.SubscriptionService;
 import org.motechproject.hub.util.HubUtils;
+import org.motechproject.hub.web.HubController;
 import org.motechproject.hub.web.IntentVerificationThreadRunnable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,71 +28,32 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 /**
- * This is the implementation class of the interface <code>SubscriptionService</code>
+ * This is the implementation class of the interface
+ * <code>SubscriptionService</code>
+ * 
  * @author Anuranjan
- *
+ * 
  */
 @Service
 public class SubscriptionServiceImpl implements SubscriptionService {
 
 	private HubTopicMDSService hubTopicService;
 	
-//	//@Autowired
-//	private SessionFactory sessionFactory;
-//	
-//	//@Autowired
-//	private TopicRepository topicRepo;
-//	
-//	//@Autowired
-//	private SubscriptionRepository subscriptionRepo;
-//	
-//	//@Autowired
-//	private SubscriptionStatusRepository subscriptionStatusRepo;
-//
-//	//@Autowired
-//	private RestTemplate restTemplate;
-//	
-//	public SessionFactory getSessionFactory() {
-//		return sessionFactory;
-//	}
-//
-//	public void setSessionFactory(SessionFactory sessionFactory) {
-//		this.sessionFactory = sessionFactory;
-//	}
-//	
-//	public TopicRepository getTopicRepo() {
-//		return topicRepo;
-//	}
-//
-//	public void setTopicRepo(TopicRepository topicRepo) {
-//		this.topicRepo = topicRepo;
-//	}
-//	
-//	public SubscriptionRepository getSubscriptionRepo() {
-//		return subscriptionRepo;
-//	}
-//
-//	public void setSubscriptionRepo(SubscriptionRepository subscriptionRepo) {
-//		this.subscriptionRepo = subscriptionRepo;
-//	}
-//	
-//	public SubscriptionStatusRepository getSubscriptionStatusRepo() {
-//		return subscriptionStatusRepo;
-//	}
-//
-//	public void setSubscriptionStatusRepo(
-//			SubscriptionStatusRepository subscriptionStatusRepo) {
-//		this.subscriptionStatusRepo = subscriptionStatusRepo;
-//	}
-//	
-//	public RestTemplate getRestTemplate() {
-//		return restTemplate;
-//	}
-//
-//	public void setRestTemplate(RestTemplate restTemplate) {
-//		this.restTemplate = restTemplate;
-//	}
-//
+	private HubSubscriptionMDSService hubSubscriptionMDSSevice;
+
+	private RestTemplate restTemplate;
+
+	private final static Logger LOGGER = Logger
+			.getLogger(SubscriptionServiceImpl.class);
+
+	public RestTemplate getRestTemplate() {
+		return restTemplate;
+	}
+
+	public void setRestTemplate(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
 	@Autowired
 	public SubscriptionServiceImpl(HubTopicMDSService hubTopicService) {
 		this.hubTopicService = hubTopicService;
@@ -102,81 +65,94 @@ public class SubscriptionServiceImpl implements SubscriptionService {
 			String leaseSeconds, String secret) throws HubException {
 
 		// fetch the HubTopic entity corresponding to the topic url requested
-//		HubTopic hubTopic = topicRepo.findByTopicUrl(topic);
-//		if (mode.equals(Modes.SUBSCRIBE)) { //subscription request
-//			// Create an insert a new topic if it doesnot already exist in the database
-//			if(hubTopic == null) {
-//				hubTopic = new HubTopic();
-//				hubTopic.setTopicUrl(topic);
-//				topicRepo.setAuditFields(hubTopic);
-//				hubTopic.setTopicId(topicRepo.getNextKey());
-//				topicRepo.saveOrUpdate(hubTopic);
-//			} 
-//			
-//			// check if the subscriber is already subscribed to the requested topic. If already subscribed, any failure will leave the previous status unchanged.
-//			HubSubscription hubSubscription = subscriptionRepo.findByCallbackUrlAndTopicUrl(callbackUrl, topic); 
-//			
-//			if (hubSubscription == null) {
-//				
-//				// create a new subscription record
-//				hubSubscription = new HubSubscription();
-//				hubSubscription.setCallbackUrl(callbackUrl);
-//				hubSubscription.setHubTopic(hubTopic);
-//				HubSubscriptionStatus status = subscriptionStatusRepo.findByStatus(SubscriptionStatusLookup.ACCEPTED.toString());
-//				hubSubscription.setHubSubscriptionStatus(status);
-//				subscriptionRepo.setAuditFields(hubSubscription);
-//				hubSubscription.setSubscriptionId(subscriptionRepo.getNextKey()); 
-//			} else {
-//				String host = HubUtils.getNetworkHostName();
-//				Date dateTime = HubUtils.getCurrentDateTime();
-//				subscriptionRepo.setAuditFieldsForUpdate(hubSubscription, host, dateTime);
-//			}
-//			if (!("").equals(secret)) {
-//				hubSubscription.setSecret(secret);
-//			}
-//			if (leaseSeconds != null && !("").equals(leaseSeconds)) {
-//				hubSubscription.setLeaseSeconds(Long.parseLong(leaseSeconds));
-//			}
-//			
-//			subscriptionRepo.saveOrUpdate(hubSubscription); 
-//				
-//		} else if (mode.equals(Modes.UNSUBSCRIBE)) { //unsubscription request
-//			
-//			if (hubTopic == null) {
-//				throw new HubException(ApplicationErrors.TOPIC_NOT_FOUND);
-//			} else { // form the subscription entity and delete this subscription from the database
-//				HubSubscription hubSubscription = new HubSubscription();
-//				hubSubscription.setCallbackUrl(callbackUrl);
-//				hubSubscription.setHubTopic(hubTopic);
-//				subscriptionRepo.delete(hubSubscription);
-//			}
-//			// if no more subscribers exists for this topic, delete it from the database
-//			List<HubSubscription> subscriptionList = subscriptionRepo.findByTopicUrl(topic); 
-//			if (subscriptionList.isEmpty()) {
-//				topicRepo.delete(hubTopic);
-//			}
-//		} 
-//		
-//		// verification of intent of the subscriber running parallelly as part of a separate thread.
-//		IntentVerificationThreadRunnable runnable = new IntentVerificationThreadRunnable(subscriptionRepo, subscriptionStatusRepo, restTemplate);
-//		runnable.setMode(mode.getMode());
-//		runnable.setCallbackUrl(callbackUrl);
-//		runnable.setTopic(topic);
-//		Thread intentVerifiationThread = new Thread(runnable); 
-//		intentVerifiationThread.start(); 
+		List<HubTopic> hubTopics = hubTopicService.findByTopicUrl(topic);
+		HubTopic hubTopic = null;
+		String hubTopicId = "";
+		if(hubTopics != null && hubTopics.size() != 0) {
+			
+				if(hubTopics.size() > 1) {
+			LOGGER.error("why are there 2 topics with the same url");
+			return;
+		}
+		else {
+			hubTopic = hubTopics.get(0);
+			Object topicId = hubTopicService.getDetachedField(hubTopic, "id");
+			hubTopicId = topicId.toString();
+		}}
+		if (mode.equals(Modes.SUBSCRIBE)) { //subscription request
+			//Create an insert a new topic if it doesnot already exist in the database
+			if(hubTopic == null) {
+				hubTopic = new HubTopic();
+				hubTopic.setTopicUrl(topic);
+				hubTopicService.create(hubTopic);
+			} 
+			Object topicId = hubTopicService.getDetachedField(hubTopic, "id");
+			
+			// check if the subscriber is already subscribed to the requested topic. If already subscribed, any failure will leave the previous status unchanged.
+			List<HubSubscription> hubSubscriptions = hubSubscriptionMDSSevice.findSubByTopicId(hubTopicId);
+			//TODO not supported by mds.findByCallbackUrlAndTopicUrl(callbackUrl,topic); 
+			HubSubscription hubSubscription = null;
+			if (hubSubscriptions == null || hubSubscriptions.isEmpty()) {
+				
+				// create a new subscription record
+				hubSubscription = new HubSubscription();
+				hubSubscription.setCallbackUrl(callbackUrl);
+				hubSubscription.setHubTopicId(topicId.toString());
+				hubSubscription.setHubSubscriptionStatusId(String.valueOf(SubscriptionStatusLookup.ACCEPTED.getId()));
+			} else if( hubSubscriptions.size() > 1  ) {
+				LOGGER.error("why are there no subscriptins with same call back url");
+			} else {
+				hubSubscription = hubSubscriptions.get(0);
+				String host = HubUtils.getNetworkHostName();
+				Date dateTime = HubUtils.getCurrentDateTime();
+			}
+			if (!("").equals(secret)) {
+				hubSubscription.setSecret(secret);
+			}
+			if (leaseSeconds != null && !("").equals(leaseSeconds)) {
+				hubSubscription.setLeaseSeconds(leaseSeconds);
+			}
+			
+			hubSubscriptionMDSSevice.create(hubSubscription); 
+				
+		} else if (mode.equals(Modes.UNSUBSCRIBE)) { //unsubscription request
+			
+			if (hubTopic == null) {
+				throw new HubException(ApplicationErrors.TOPIC_NOT_FOUND);
+			} else { // form the subscription entity and delete this subscription from the database
+				HubSubscription hubSubscription = new HubSubscription();
+				hubSubscription.setCallbackUrl(callbackUrl);
+				hubSubscription.setHubTopicId(hubTopicId);
+				hubSubscriptionMDSSevice.delete(hubSubscription);
+			}
+			// if no more subscribers exists for this topic, delete it from the database
+			List<HubSubscription> subscriptionList = 
+					hubSubscriptionMDSSevice.findSubByTopicId(hubTopicId); 
+			if (subscriptionList.isEmpty()) {
+				hubTopicService.delete(hubTopic);
+			}
+		} 
+		
+		// verification of intent of the subscriber running parallelly as part of a separate thread.
+		IntentVerificationThreadRunnable runnable = new IntentVerificationThreadRunnable(hubSubscriptionMDSSevice, restTemplate);
+		runnable.setMode(mode.getMode());
+		runnable.setCallbackUrl(callbackUrl);
+		runnable.setTopic(topic);
+		Thread intentVerifiationThread = new Thread(runnable); 
+		intentVerifiationThread.start(); 
 		
 	}
 
 	@Override
 	public String sayHello() {
-		 HubTopic hubTopic = new HubTopic("topic_url_1");
-		 hubTopicService.create(hubTopic);
-		 hubTopic.setTopicUrl("topic_url_2");
-		 hubTopicService.create(hubTopic);
-		 List<HubTopic> hubTopics = hubTopicService.retrieveAll();
-		 
-	      return String.format("{\"message\":\"%s\"}", "Hello World " + hubTopics.size());
+		HubTopic hubTopic = new HubTopic("topic_url_1");
+		hubTopicService.create(hubTopic);
+		hubTopic.setTopicUrl("topic_url_2");
+		hubTopicService.create(hubTopic);
+		List<HubTopic> hubTopics = hubTopicService.retrieveAll();
 
-		
+		return String.format("{\"message\":\"%s\"}",
+				"Hello World " + hubTopics.size());
+
 	}
 }
--- modules/hub/hub/src/main/java/org/motechproject/hub/web/IntentVerificationThreadRunnable.java
+++ modules/hub/hub/src/main/java/org/motechproject/hub/web/IntentVerificationThreadRunnable.java
@@ -1,9 +1,14 @@
 package org.motechproject.hub.web;
 
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
+import org.apache.log4j.Logger;
+import org.motechproject.hub.mds.HubSubscription;
+import org.motechproject.hub.mds.service.HubSubscriptionMDSService;
 import org.motechproject.hub.model.SubscriptionStatusLookup;
+import org.motechproject.hub.service.impl.SubscriptionServiceImpl;
 //import org.motechproject.hub.repository.SubscriptionRepository;
 //import org.motechproject.hub.repository.SubscriptionStatusRepository;
 import org.motechproject.hub.util.HubUtils;
@@ -16,9 +21,12 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 /**
- * This class implements <code>Runnable</code> which starts a new thread to verify the intent of the subscriber requesting subscription or unsubscription.
+ * This class implements <code>Runnable</code> which starts a new thread to
+ * verify the intent of the subscriber requesting subscription or
+ * unsubscription.
+ * 
  * @author Anuranjan
- *
+ * 
  */
 public class IntentVerificationThreadRunnable implements Runnable {
 
@@ -26,6 +34,9 @@ public class IntentVerificationThreadRunnable implements Runnable {
 	private String callbackUrl;
 	private String mode;
 	private String topic;
+
+	private final static Logger LOGGER = Logger
+			.getLogger(IntentVerificationThreadRunnable.class);
 
 	public String getCallbackUrl() {
 		return callbackUrl;
@@ -50,79 +61,103 @@ public class IntentVerificationThreadRunnable implements Runnable {
 	public void setTopic(String topic) {
 		this.topic = topic;
 	}
-	
+
 	public void setRestTemplate(RestTemplate restTemplate) {
 		this.restTemplate = restTemplate;
 	}
 
-//	private SubscriptionRepository subscriptionRepo;
-//
-//	private SubscriptionStatusRepository subscriptionStatusRepo;
-	
+	private HubSubscriptionMDSService hubSubscriptionMDSService;
+	//
+	// private SubscriptionStatusRepository subscriptionStatusRepo;
+
 	private RestTemplate restTemplate;
-	
-//	public IntentVerificationThreadRunnable(SubscriptionRepository subscriptionRepo, SubscriptionStatusRepository subscriptionStatusRepo, RestTemplate restTemplate) {
-//		this.subscriptionRepo = subscriptionRepo;
-//		this.subscriptionStatusRepo = subscriptionStatusRepo;
-//		this.restTemplate = restTemplate;
-//	}
+
+	public IntentVerificationThreadRunnable(
+			HubSubscriptionMDSService hubSubscriptionMDSService,
+			RestTemplate restTemplate) {
+		this.hubSubscriptionMDSService = hubSubscriptionMDSService;
+		this.restTemplate = restTemplate;
+	}
 
 	/**
-	 * In order to prevent an attacker from creating unwanted subscriptions on behalf of a subscriber (or unsubscribing desired ones), 
-	 * a hub must ensure that the subscriber did indeed send the subscription request. 
-	 * The hub verifies a subscription request by sending an HTTP GET request to the subscriber's callback URL as given in the subscription request. 
+	 * In order to prevent an attacker from creating unwanted subscriptions on
+	 * behalf of a subscriber (or unsubscribing desired ones), a hub must ensure
+	 * that the subscriber did indeed send the subscription request. The hub
+	 * verifies a subscription request by sending an HTTP GET request to the
+	 * subscriber's callback URL as given in the subscription request.
 	 * 
-	 * For this, hub generates a random string token and sends as param <code>hub.challenge</code> which the subscriber must echo in order to confirm its request.
-	 * Other parameters are <code>hub.mode</code> and <code>hub.topic</code> coming from the subscription/unsubscription request
+	 * For this, hub generates a random string token and sends as param
+	 * <code>hub.challenge</code> which the subscriber must echo in order to
+	 * confirm its request. Other parameters are <code>hub.mode</code> and
+	 * <code>hub.topic</code> coming from the subscription/unsubscription
+	 * request
 	 * 
 	 */
 	@Override
 	@Transactional
 	public void run() {
-		
-//		SubscriptionStatusLookup statusLookup = null;
-//		
-//		HttpHeaders headers = new HttpHeaders();
-//		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-//		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-//		//randomly generated UUID
-//		String uuid = UUID.randomUUID().toString();
-//		String contentVerificationUrl = callbackUrl + INTENT_VERIFICATION_PARAMS;
-//		try {
-//			ResponseEntity<String> response = restTemplate.exchange(contentVerificationUrl, HttpMethod.GET, entity, String.class, mode, topic, uuid);
-//	
-//			// Any status code other than 2xx is invalid response. Also, the response body should match the uuid string passed in the GET call
-//			if (response == null || response.getStatusCode().value() / 100 != 2 || response.getBody() == null || !response.getBody().toString().equals(uuid)) {
-//				statusLookup = SubscriptionStatusLookup.INTENT_FAILED;
-//			} else {
-//				statusLookup = SubscriptionStatusLookup.INTENT_VERIFIED;
-//			}
-//		} catch (Exception e) {
-//			statusLookup = SubscriptionStatusLookup.INTENT_FAILED;
-//		}
-//		
-//		// fetch the subscription corresponding to the callbackUrl and the topic
-//		HubSubscription subscription = subscriptionRepo.findByCallbackUrlAndTopicUrl(callbackUrl, topic);
-//		HubSubscriptionStatus hubSubscriptionStatus = subscription.getHubSubscriptionStatus();
-//		String currentStatus = null;
-//		if (hubSubscriptionStatus != null) {
-//			currentStatus = subscription.getHubSubscriptionStatus().getSubscriptionStatusCode();
-//		}
-//		
-//		// any failure should not affect the existing subscription status
-//		if (!SubscriptionStatusLookup.INTENT_VERIFIED.toString().equals(currentStatus)) {
-//			
-//			// fetch the HubSubscriptionStatus enum value
-//			HubSubscriptionStatus status = subscriptionStatusRepo.findByStatus(statusLookup.toString());
-//			String host = HubUtils.getNetworkHostName();
-//			Date dateTime = HubUtils.getCurrentDateTime();
-//			subscription.setHubSubscriptionStatus(status);
-//			subscriptionRepo.setAuditFieldsForUpdate(subscription, host, dateTime);
-//			
-//			// insert a record corresponding to this subscription
-//			subscriptionRepo.saveOrUpdate(subscription); 
-//		}
-		
+
+		SubscriptionStatusLookup statusLookup = null;
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		HttpEntity<String> entity = new HttpEntity<String>("parameters",
+				headers);
+		// randomly generated UUID
+		String uuid = UUID.randomUUID().toString();
+		String contentVerificationUrl = callbackUrl
+				+ INTENT_VERIFICATION_PARAMS;
+		try {
+			ResponseEntity<String> response = restTemplate.exchange(
+					contentVerificationUrl, HttpMethod.GET, entity,
+					String.class, mode, topic, uuid);
+
+			// Any status code other than 2xx is invalid response. Also, the
+			// response body should match the uuid string passed in the GET call
+			if (response == null || response.getStatusCode().value() / 100 != 2
+					|| response.getBody() == null
+					|| !response.getBody().toString().equals(uuid)) {
+				statusLookup = SubscriptionStatusLookup.INTENT_FAILED;
+			} else {
+				statusLookup = SubscriptionStatusLookup.INTENT_VERIFIED;
+			}
+		} catch (Exception e) {
+			statusLookup = SubscriptionStatusLookup.INTENT_FAILED;
+		}
+
+		// fetch the subscription corresponding to the callbackUrl and the
+		// topic
+		List<HubSubscription> subscriptions = hubSubscriptionMDSService
+				.findSubByTopicId(topic);
+		if (subscriptions == null || subscriptions.size() == 0
+				|| subscriptions.size() > 1) {
+			LOGGER.info("not handled earlier, need to check");
+			return;
+		}
+		HubSubscription subscription = subscriptions.get(0);
+		// TODO mds not
+		// supportedhubSubscriptionMDSService.findByCallbackUrlAndTopicUrl(callbackUrl,
+		// topic);
+		String hubSubscriptionStatus = subscription
+				.getHubSubscriptionStatusId();
+		String currentStatus = null;
+		if (hubSubscriptionStatus != null) {
+			currentStatus = subscription.getHubSubscriptionStatusId();
+		}
+
+		// any failure should not affect the existing subscription status
+		if (!SubscriptionStatusLookup.INTENT_VERIFIED.toString().equals(
+				currentStatus)) {
+
+			// fetch the HubSubscriptionStatus enum value
+			String host = HubUtils.getNetworkHostName();
+			subscription.setHubSubscriptionStatusId(String.valueOf(statusLookup
+					.getId()));
+
+			// insert a record corresponding to this subscription
+			hubSubscriptionMDSService.create(subscription);
+		}
+
 	}
 
 }
