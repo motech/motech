341cd79295f37c19fe99589b7031ea17e4cb6268
Rob LaRubbio <rob@larubbio.org>
2014-06-12 22:10:28 +0000

    MOTECH-1106 | Move modules to contrib (event-aggregation)
    Remove event aggregation from platform since it has been moved to contrib
    
    Change-Id: I1e87aade7c1b3be5fe9daa83c009c9ef7b9c2fce

--- modules/event-aggregation/src/main/java/org/motechproject/event/aggregation/service/EventAggregationService.java
+++
@@ -1,8 +0,0 @@
-package org.motechproject.event.aggregation.service;
-
-import org.motechproject.event.aggregation.model.rule.AggregationRuleRequest;
-
-public interface EventAggregationService {
-
-    void createRule(AggregationRuleRequest request);
-}
--- platform/event/src/main/java/org/motechproject/event/listener/impl/EventListenerRegistry.java
+++ platform/event/src/main/java/org/motechproject/event/listener/impl/EventListenerRegistry.java
@@ -4,7 +4,7 @@ import org.motechproject.event.listener.EventListener;
 import org.motechproject.event.listener.EventListenerRegistryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
+ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Set;
