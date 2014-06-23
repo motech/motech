d35f99c389bcb5a04d2712255523f2348e180157
Rob LaRubbio <rob@larubbio.org>
2014-06-23 18:56:31 +0000

    MOTECH-1105 | Move Http Agent & Event Loggins to modules repo
    Remove the modules from the motech repo since they have been migrated to
    the modules repo
    
    Change-Id: Ifbb08220b7274a0fd896617e98595e9e19e9a64b

--- utils/http-agent/src/main/java/org/motechproject/http/agent/service/HttpAgent.java
+++
@@ -1,8 +0,0 @@
-package org.motechproject.http.agent.service;
-
-public interface HttpAgent {
-
-    void execute(String url, Object data, Method method);
-
-    void executeSync(String url, Object data, Method method);
-}
