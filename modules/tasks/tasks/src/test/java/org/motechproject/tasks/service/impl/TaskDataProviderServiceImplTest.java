eb5ac54d2b1f441df49a0bf842af438ecc08b4ed
Sebastian Brudzinski <sbrudzinski@soldevelo.com>
2014-08-06 12:53:44 +0200

    MOTECH-1200 Improve Data Provider registration time in some cases
    Since the amount of provider objects can be really huge with MDS, having
    to update them, even though nothing has changed is a waste of time.
    If there's no change we simply skip the update what significantly
    speeds up some MDS actions and Tasks module startup time.
    
    Change-Id: I07d9b3405b049b85bf4855945cbaa9d3128c815c

--- modules/tasks/tasks/src/test/java/org/motechproject/tasks/service/impl/TaskDataProviderServiceImplTest.java
+++ modules/tasks/tasks/src/test/java/org/motechproject/tasks/service/impl/TaskDataProviderServiceImplTest.java
@@ -140,10 +140,13 @@ public class TaskDataProviderServiceImplTest {
         List<LookupFieldsParameter> lookupFields = asList(new LookupFieldsParameter("lookupField",asList("lookupField")));
         List<FieldParameter> fields = asList(new FieldParameter("displayName", "fieldKey"));
         objects.add(new TaskDataProviderObject("displayName", "type", lookupFields, fields));
-
         TaskDataProvider provider = new TaskDataProvider(PROVIDER_NAME, objects);
 
-        when(motechJsonReader.readFromStream(inputStream, type)).thenReturn(provider);
+        List<TaskDataProviderObject> updatedObjects = new ArrayList<>(objects);
+        updatedObjects.add(new TaskDataProviderObject("displayName2", "type2", lookupFields, fields));
+        TaskDataProvider updatedProvider = new TaskDataProvider(PROVIDER_NAME, updatedObjects);
+
+        when(motechJsonReader.readFromStream(inputStream, type)).thenReturn(updatedProvider);
         when(dataProviderDataService.findByName(PROVIDER_NAME)).thenReturn(provider);
 
         ArgumentCaptor<MotechEvent> captor = ArgumentCaptor.forClass(MotechEvent.class);
