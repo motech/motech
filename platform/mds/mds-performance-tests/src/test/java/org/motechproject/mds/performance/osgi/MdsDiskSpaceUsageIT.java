f85c1e03e6672888983a82c20838a5d3a0c8e1e5
Kamil Madej <kmadej@soldevelo.com>
2014-08-11 15:51:18 +0200

    MOTECH-1174: Inspect space usage by MDS storage
    In this commit I have added test which can create EUDE entities using
    DummyDataGenerator. You can choose number of entities, fields, lookups and
    instances. The main purpose of this test is to check disk space usage by MDS,
    it's why I logged this information at the end of the test.
    
    With huge amount of instances this test can last very long.
    
    Change-Id: I5deb3a5601f1022c9e1408f2156d67c3fad42280

---
+++ platform/mds/mds-performance-tests/src/test/java/org/motechproject/mds/performance/osgi/MdsDiskSpaceUsageIT.java
@@ -0,0 +1,107 @@
+package org.motechproject.mds.performance.osgi;
+
+import org.eclipse.gemini.blueprint.util.OsgiBundleUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.motechproject.mds.dto.EntityDto;
+import org.motechproject.mds.performance.service.MdsDummyDataGenerator;
+import org.motechproject.mds.performance.service.impl.MdsDummyDataGeneratorImpl;
+import org.motechproject.mds.service.EntityService;
+import org.motechproject.mds.service.JarGeneratorService;
+import org.motechproject.mds.service.MotechDataService;
+import org.motechproject.mds.util.Constants;
+import org.motechproject.testing.osgi.BasePaxIT;
+import org.motechproject.testing.osgi.container.MotechNativeTestContainerFactory;
+import org.motechproject.testing.osgi.helper.ServiceRetriever;
+import org.ops4j.pax.exam.ExamFactory;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerSuite;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.orm.jdo.LocalPersistenceManagerFactoryBean;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.inject.Inject;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.datastore.JDOConnection;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.motechproject.mds.util.Constants.BundleNames.MDS_BUNDLE_SYMBOLIC_NAME;
+import static org.motechproject.mds.util.Constants.BundleNames.MDS_ENTITIES_SYMBOLIC_NAME;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerSuite.class)
+@ExamFactory(MotechNativeTestContainerFactory.class)
+public class MdsDiskSpaceUsageIT extends BasePaxIT {
+    private static final Logger LOG = LoggerFactory.getLogger(MdsDiskSpaceUsageIT.class);
+
+    private final static int ENTITIES = 1;
+    private final static int FIELDS = 5;
+    private final static int INSTANCES = 100000;
+    private final static int LOOKUPS = 0;
+
+    private final static String SQLQUERY = "select sum((data_length+index_length)/1024/1024) AS MB from information_schema.tables" +
+            " where table_schema = \"motech_data_services\";";
+
+    private MdsDummyDataGenerator generator;
+    @Inject
+    private EntityService entityService;
+    @Inject
+    private JarGeneratorService jarGeneratorService;
+    @Inject
+    private BundleContext bundleContext;
+
+    @Before
+    public void setUp() {
+        generator = new MdsDummyDataGeneratorImpl(entityService, jarGeneratorService, bundleContext);
+        generator.clearEntities();
+        generator.setUpSecurityContext();
+        generator.setEntityPrefix("DiskSpaceUsageITEntity");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        generator.clearEntities();
+    }
+
+    @Test
+    public void testEudeDiskSpaceUsage ()
+            throws IOException, IllegalAccessException, ClassNotFoundException, InstantiationException, SQLException {
+
+        LOG.info("Creating entity");
+        generator.generateDummyEntities(ENTITIES, FIELDS, LOOKUPS, true);
+
+        EntityDto entityDto = entityService.getEntityByClassName
+                (Constants.Packages.ENTITY.concat(".").concat(generator.getEntityPrefix()).concat("0"));
+
+        LOG.info("Creating {} instances for entity", INSTANCES);
+        generator.generateDummyInstances(entityDto.getId(), INSTANCES);
+
+        WebApplicationContext context = ServiceRetriever.getWebAppContext(bundleContext, MDS_BUNDLE_SYMBOLIC_NAME);
+        LocalPersistenceManagerFactoryBean localPersistenceManagerFactoryBean = context.getBean(LocalPersistenceManagerFactoryBean.class);
+        PersistenceManagerFactory persistenceManagerFactory = localPersistenceManagerFactoryBean.getObject();
+
+        JDOConnection con = persistenceManagerFactory.getPersistenceManager().getDataStoreConnection();
+        Connection nativeCon = (Connection) con.getNativeConnection();
+
+        Statement stmt = nativeCon.createStatement();
+        ResultSet resultSet = stmt.executeQuery(SQLQUERY);
+        resultSet.absolute(1);
+        Double spaceUsage =  resultSet.getDouble("MB");
+
+        LOG.info("Disk space usage of Motech Data Services database after creating {} instances is {} MB",INSTANCES, spaceUsage);
+
+        Bundle entitiesBundle = OsgiBundleUtils.findBundleBySymbolicName(bundleContext, MDS_ENTITIES_SYMBOLIC_NAME);
+        MotechDataService service = generator.getService(entitiesBundle.getBundleContext(), entityDto.getClassName());
+        service.deleteAll();
+    }
+}
