b975c450119baf465e0197f8d8961e7f83272900
Sebastian Brudzinski <sbrudzinski@soldevelo.com>
2014-08-14 10:02:25 +0200

    Disabled exec-maven-plugin in platform-server module
    Currently the jenkins jobs block during execution of
    "tomcat-shutdown.sh" script. This commit disables temporarily execution
    of the script.
    
    Change-Id: Iaccf787316b40997b30b2ffbbb0d01c8fd7c29f8

--- platform/server/tomcat-shutdown.sh
+++ platform/server/tomcat-shutdown.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
-FILE=${PWD}/target/tomcat-processes-list
+echo "~~~ Starting tomcat-shutdown.sh ~~~"
 
-if [ -f ${FILE} ]
-then
-    rm -f ${FILE}
-fi
+echo "~~~ Current working directory ~~~"
+echo ${PWD}
 
-ps ux | grep 'target/tomcat' | grep -v grep > ${FILE}
+echo "~~~ List of running processes ~~~"
+ps ux
 
-TOMCAT_PROCESSES_COUNT=$(wc -l < ${FILE})
+echo "~~~ List of running tomcat processes ~~~"
+ps ux | grep 'target/tomcat' | grep -v grep
 
+echo "~~~ Number of running tomcat processes ~~~"
+TOMCAT_PROCESSES_COUNT=$(ps ux | grep 'target/tomcat' | grep -v grep | wc -l)
+echo ${TOMCAT_PROCESSES_COUNT}
+
+echo "~~~ WARNING ~~~"
 echo "There are $TOMCAT_PROCESSES_COUNT running Tomcat processes"
-echo "You can find Tomcat processes list in $FILE"
 
 exit ${TOMCAT_PROCESSES_COUNT}
