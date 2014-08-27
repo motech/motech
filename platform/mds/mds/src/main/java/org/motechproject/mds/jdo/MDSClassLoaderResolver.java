75c846241c5e83b70b86fbc957f1c07ebd165c0b
Paweł Gesek <pgesek@soldevelo.com>
2014-08-27 09:58:20 +0200

    MOTECH-1164: Fix for random NPEs
    The underlying issue lies at Felix. When the Nucleus context is created
    it is given ClassLoaders coming from the old bundle version, which leads
    to NPE when we try to load resources from those ClasslLoaders. This is a
    pretty ugly fix, but it works and seems the best we can do right now. We
    catch the NPE and then recreate the ClassLoaderResolver instance(its
    classloader field is final).
    
    Change-Id: I77e860578e117c800e273d3c195f24e1a61169ab

--- platform/mds/mds/src/main/java/org/motechproject/mds/jdo/MDSClassLoaderResolver.java
+++ platform/mds/mds/src/main/java/org/motechproject/mds/jdo/MDSClassLoaderResolver.java
@@ -1,40 +1,130 @@
 package org.motechproject.mds.jdo;
 
+import org.apache.felix.framework.BundleWiringImpl;
 import org.datanucleus.ClassLoaderResolver;
-import org.datanucleus.ClassLoaderResolverImpl;
-import org.datanucleus.exceptions.ClassNotResolvedException;
-import org.motechproject.mds.util.MDSClassLoader;
+import org.eclipse.gemini.blueprint.util.OsgiBundleUtils;
+import org.motechproject.mds.util.Constants;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.wiring.BundleWiring;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
 
 /**
- * The main purpose of the <code>MDSClassLoaderResolver</code> class is to avoid situation in which
- * standard datanucleus class loader resolver does not see classes that are saved in database.
+ * This is a wrapper for {@link org.motechproject.mds.jdo.MDSClassLoaderResolverImpl}.
+ * All calls for the {@link org.datanucleus.ClassLoaderResolver} interface are passed to
+ * the current instance of the ClassLoaderResolver implementation.
+ * When we hit a NullPointerException originating in Felix, we can determine it is due to
+ * a synchronization bug after bundle updates - as a result of this DataNucleus has passed us
+ * ClassLoaders from the former Bundle version. In that case we reload the instance passing it the
+ * ClassLoaders from the new bundle.
  */
-public class MDSClassLoaderResolver extends ClassLoaderResolverImpl implements ClassLoaderResolver {
+public class MDSClassLoaderResolver implements ClassLoaderResolver {
+
+    private MDSClassLoaderResolverImpl instance;
 
     public MDSClassLoaderResolver() {
-        this(null);
+        instance = new MDSClassLoaderResolverImpl();
     }
 
     public MDSClassLoaderResolver(ClassLoader pmLoader) {
-        super(pmLoader);
+        instance = new MDSClassLoaderResolverImpl(pmLoader);
     }
 
     @Override
-    @SuppressWarnings("PMD.PreserveStackTrace")
     public Class classForName(String name, ClassLoader primary) {
+        return instance.classForName(name, primary);
+    }
+
+    @Override
+    public Class classForName(String name, ClassLoader primary, boolean initialize) {
+        return instance.classForName(name, primary, initialize);
+    }
+
+    @Override
+    public Class classForName(String name) {
+        return instance.classForName(name);
+    }
+
+    @Override
+    public Class classForName(String name, boolean initialize) {
+        return instance.classForName(name, initialize);
+    }
+
+    @Override
+    public boolean isAssignableFrom(String className, Class clazz) {
+        return instance.isAssignableFrom(className, clazz);
+    }
+
+    @Override
+    public boolean isAssignableFrom(Class clazz, String className) {
+        return instance.isAssignableFrom(clazz, className);
+    }
+
+    @Override
+    public boolean isAssignableFrom(String className1, String className2) {
+        return instance.isAssignableFrom(className1, className2);
+    }
+
+    @Override
+    public Enumeration<URL> getResources(String resourceName, ClassLoader primary) throws IOException {
         try {
-            return super.classForName(name, primary);
-        } catch (ClassNotResolvedException e) {
-            try {
-                return Class.forName(name);
-            } catch (ClassNotFoundException | NoClassDefFoundError ex) {
-                try {
-                    return MDSClassLoader.getInstance().loadClass(name);
-                } catch (ClassNotFoundException exp) {
-                    throw new ClassNotResolvedException(LOCALISER.msg("001000", name), exp);
-                }
-            }
+            return instance.getResources(resourceName, primary);
+        } catch (NullPointerException e) {
+            // MOTECH-1164 fix
+            // This is a bug originating in Felix after bundle updates
+            // Datanucleus passes us classloaders coming from old bundles,
+            // so we have to retrieve the new ClassLoader and recreate the acutal
+            // resolver instance.
+            ClassLoader newCl = getNewClassLoader();
+            instance = new MDSClassLoaderResolverImpl(newCl);
+            return instance.getResources(resourceName, newCl);
         }
     }
 
+    @Override
+    public URL getResource(String resourceName, ClassLoader primary) {
+        try {
+            return instance.getResource(resourceName, primary);
+        } catch (NullPointerException e) {
+            // MOTECH-1164 fix
+            // This is a bug originating in Felix after bundle updates
+            // Datanucleus passes us classloaders coming from old bundles,
+            // so we have to retrieve the new ClassLoader and recreate the acutal
+            // resolver instance.
+            ClassLoader newCl = getNewClassLoader();
+            instance = new MDSClassLoaderResolverImpl(newCl);
+            return instance.getResource(resourceName, newCl);
+        }
+    }
+
+    @Override
+    public void setRuntimeClassLoader(ClassLoader loader) {
+        instance.setRuntimeClassLoader(loader);
+    }
+
+    @Override
+    public void registerUserClassLoader(ClassLoader loader) {
+        instance.registerUserClassLoader(loader);
+    }
+
+    @Override
+    public void setPrimary(ClassLoader primary) {
+        instance.setPrimary(primary);
+    }
+
+    @Override
+    public void unsetPrimary() {
+        instance.unsetPrimary();
+    }
+
+    private ClassLoader getNewClassLoader() {
+        BundleContext bc = FrameworkUtil.getBundle(this.getClass()).getBundleContext();
+        Bundle bundle = OsgiBundleUtils.findBundleBySymbolicName(bc, Constants.BundleNames.MDS_ENTITIES_SYMBOLIC_NAME);
+        BundleWiringImpl bundleWiring = (BundleWiringImpl) bundle.adapt(BundleWiring.class);
+        return bundleWiring.getClassLoader();
+    }
 }
