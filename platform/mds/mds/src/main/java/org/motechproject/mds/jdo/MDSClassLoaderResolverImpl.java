c37ff5269a9cb61ed74148d67e2386707176d504
Paweł Gesek <pgesek@soldevelo.com>
2014-08-27 09:58:20 +0200

    MOTECH-1164: Fix for random NPEs
    The underlying issue lies at Felix. When the Nucleus context is created
    it is given ClassLoaders coming from the old bundle version, which leads
    to NPE when we try to load resources from those ClasslLoaders. This is a
    pretty ugly fix, but it works and seems the best we can do right now. We
    catch the NPE and then recreate the ClassLoaderResolver instance(its
    classloader field is final).
    
    Change-Id: I77e860578e117c800e273d3c195f24e1a61169ab

---
+++ platform/mds/mds/src/main/java/org/motechproject/mds/jdo/MDSClassLoaderResolverImpl.java
@@ -0,0 +1,41 @@
+package org.motechproject.mds.jdo;
+
+import org.datanucleus.ClassLoaderResolverImpl;
+import org.datanucleus.exceptions.ClassNotResolvedException;
+import org.motechproject.mds.util.MDSClassLoader;
+
+/**
+ * The main purpose of the <code>MDSClassLoaderResolverImpl</code> class is to avoid situation in which
+ * standard datanucleus class loader resolver does not see classes that are saved in database.
+ * This is the main implementation that extends the standard ClassLoaderResolverImpl from datanucleus.
+ * Due to a synchronization bug in Felix, there are cases when we will instantiate this more then once
+ * (after we hit the bug).
+ */
+class MDSClassLoaderResolverImpl extends ClassLoaderResolverImpl {
+
+    public MDSClassLoaderResolverImpl() {
+        this(null);
+    }
+
+    public MDSClassLoaderResolverImpl(ClassLoader pmLoader) {
+        super(pmLoader);
+    }
+
+    @Override
+    @SuppressWarnings("PMD.PreserveStackTrace")
+    public Class classForName(String name, ClassLoader primary) {
+        try {
+            return super.classForName(name, primary);
+        } catch (ClassNotResolvedException e) {
+            try {
+                return Class.forName(name);
+            } catch (ClassNotFoundException | NoClassDefFoundError ex) {
+                try {
+                    return MDSClassLoader.getInstance().loadClass(name);
+                } catch (ClassNotFoundException exp) {
+                    throw new ClassNotResolvedException(LOCALISER.msg("001000", name), exp);
+                }
+            }
+        }
+    }
+}
