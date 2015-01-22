c8b16d334a1d342fc2ba3f53e221005e90d8e73b
Lauren <llavoie@grameenfoundation.org>
2015-01-22 09:58:24 -0800

    Update copyright year for 2015 (platform repo)
    Update LICENSE.txt and documentation copyright
    
    Change-Id: Id8a6c058d2c867c77568490f39e969fbee67837f

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,7 +61,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'MOTECH'
-copyright = u'2014, Grameen Foundation'
+copyright = u'2015, Grameen Foundation'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
