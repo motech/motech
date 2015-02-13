c85cb2467f9ed54cae6ea65a659b6488f8901363
Paweł Gesek <pgesek@soldevelo.com>
2015-02-13 15:58:30 +0100

    Add var for load
    Change-Id: Ia58640092b6e2aea6fddc46d9e3c9bde6c2d875d

--- platform/server-bundle/src/main/resources/webapp/js/rest-docs-app.js
+++ platform/server-bundle/src/main/resources/webapp/js/rest-docs-app.js
@@ -11,29 +11,35 @@
 
     restDocModule.controller('ServerRestDocsCtrl', function ($scope, $location, $http) {
 
-        // start when location is available
-        $scope.$on("$locationChangeSuccess", function() {
-            // get the url for the docs, then get the url
-            $http.get("../server/module/rest-docs/" + $scope.getRestModuleName()).success(function(data) {
-                // call that url, send the server prefix in the param
-                window.swaggerUi = new SwaggerUi({
-                    url: "../" + data + "?serverPrefix=" + getServerPrefix(),
-                    dom_id: "swagger-ui-container",
-                    supportedSubmitMethods: ['get', 'post', 'put', 'delete'],
-                    onFailure: function(data) {
-                        motechAlert(data, "error");
-                    }
-                });
-
-                window.swaggerUi.load();
-            }).error(alertHandler('server.error', 'server.error.rest.url'));
-        });
+        // this variable is here since we use $scope.$on below
+        $scope.loaded = false;
 
         $scope.getRestModuleName = function() {
             $scope.before = $location.path();
             var splitPath = $location.path().split('/');
             return splitPath[splitPath.length - 1];
         };
+
+        // start when location is available
+        $scope.$on("$locationChangeSuccess", function() {
+            if (!$scope.loaded) {
+                $scope.loaded = true;
+                // get the url for the docs, then get the url
+                $http.get("../server/module/rest-docs/" + $scope.getRestModuleName()).success(function(data) {
+                    // call that url, send the server prefix in the param
+                    window.swaggerUi = new SwaggerUi({
+                        url: "../" + data + "?serverPrefix=" + getServerPrefix(),
+                        dom_id: "swagger-ui-container",
+                        supportedSubmitMethods: ['get', 'post', 'put', 'delete'],
+                        onFailure: function(data) {
+                            motechAlert(data, "error");
+                        }
+                    });
+
+                    window.swaggerUi.load();
+                }).error(alertHandler('server.error', 'server.error.rest.url'));
+            }
+        });
     });
 }());
 
