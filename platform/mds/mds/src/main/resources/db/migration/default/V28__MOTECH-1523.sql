f337d6756a9ead7bde86da666d5dbfcbacba44a7
Sebastian Brudzinski <sbrudzinski@soldevelo.com>
2015-02-26 15:20:54 +0100

    MOTECH-1523 Adjusted latest migration
    I've adjusted the latest migrtion to prevent it from failing,
    in case the table and record already exist. This is required
    due to different versions of the migrations on the branches
    and therefore possibility of the same migration getting called
    twice.
    
    Change-Id: I689f2234e889bce95f633a3abf23f48e16eb8bf8

--- platform/mds/mds/src/main/resources/db/migration/default/V28__MOTECH-1523.sql
+++ platform/mds/mds/src/main/resources/db/migration/default/V28__MOTECH-1523.sql
@@ -1,6 +1,11 @@
-CREATE TABLE "SchemaChangeLock" (
+CREATE TABLE IF NOT EXISTS "SchemaChangeLock" (
     "id" bigint PRIMARY KEY,
-    "lockId" int
+    "lockId" int UNIQUE
 );
 
-INSERT INTO "SchemaChangeLock" VALUES (1, 1);
+INSERT INTO "SchemaChangeLock" ("id", "lockId")
+SELECT 1, 1
+WHERE
+    NOT EXISTS (
+        SELECT id FROM "SchemaChangeLock" WHERE id = 1
+    );
