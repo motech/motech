f7e2f170f59cb1f69c5f734de79702c695575366
Kamil Knitter <kknitter@soldevelo.com>
2015-03-20 12:58:55 +0100

    MOTECH-1245: Dependencies among MDS Entities defined in different bundles
    This commit changes the way in which classes are loaded during annotations
    processing. From now, the loader that is responsible for loading unmodified
    (not yet enhanced) classes directly from bundle jar is included in
    classloading chain.
    
    Change-Id: I31be2cf97644516ab8cdc423e2a0db260ebe759d

---
+++ platform/mds/mds-secondary-test-bundle/src/main/java/org/motechproject/mds/test/secondary/service/EntityADataService.java
@@ -0,0 +1,7 @@
+package org.motechproject.mds.test.secondary.service;
+
+import org.motechproject.mds.service.MotechDataService;
+import org.motechproject.mds.test.secondary.domain.EntityA;
+
+public interface EntityADataService extends MotechDataService<EntityA> {
+}
--- platform/mds/mds-test-bundle/src/test/java/org/motechproject/mds/test/it/IntegrationTests.java
+++ platform/mds/mds-test-bundle/src/test/java/org/motechproject/mds/test/it/IntegrationTests.java
@@ -3,9 +3,9 @@ package org.motechproject.mds.test.it;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.motechproject.mds.test.osgi.MdsDdeBundleIT;
-import org.motechproject.mds.test.osgi.MdsDdeValidationIT;
+import org.motechproject.mds.test.osgi.MdsDdeValidationContextIT;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({MdsDdeBundleIT.class, MdsDdeValidationIT.class})
+@Suite.SuiteClasses({MdsDdeBundleIT.class, MdsDdeValidationContextIT.class})
 public class IntegrationTests {
 }
