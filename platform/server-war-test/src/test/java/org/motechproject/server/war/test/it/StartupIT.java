73fb660d2f1b534d09da598dc9b4d0f088b405f2
Nikodem Graczewski <ngraczewski@soldevelo.com>
2015-07-23 09:34:29 +0200

    MOTECH-1646: Create/extend an integration test to test in a WAR only environment
    Created seperate mondule that runs integration tests in a WAR only enviroment.
    
    Change-Id: I9b26c8a94ca696ac844c26aa096c6595ddeff19c

--- platform/server-war-test/src/test/java/org/motechproject/server/war/test/it/StartupIT.java
+++ platform/server-war-test/src/test/java/org/motechproject/server/war/test/it/StartupIT.java
@@ -1,11 +1,18 @@
-package org.motechproject.server.it;
+package org.motechproject.server.war.test.it;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.motechproject.testing.tomcat.BaseTomcatIT;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 public class StartupIT extends BaseTomcatIT {
+
+    private static final String LOCATION = "location";
+    private static final String JNDI_PREFIX = "jndi:";
 
     @Before
     public void setUp() throws Exception {
@@ -20,7 +27,16 @@ public class StartupIT extends BaseTomcatIT {
         waitForBundles(bundles);
 
         for (int i = 0; i < bundles.length(); i++) {
+            assertBundleComesFromJndi(bundles.getJSONObject(i));
             assertBundleStatus(bundles.getJSONObject(i));
         }
+    }
+
+    private void assertBundleComesFromJndi(JSONObject object) {
+
+        String location = object.getString(LOCATION);
+
+        assertNotNull(location);
+        assertTrue(location.startsWith(JNDI_PREFIX));
     }
 }
