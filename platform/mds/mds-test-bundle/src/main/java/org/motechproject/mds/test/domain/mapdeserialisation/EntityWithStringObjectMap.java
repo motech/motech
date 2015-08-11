5d0d6c03d66c164347be5de951547fe5897ef757
Tomasz Stalka <tstalka@soldevelo.com>
2015-08-11 11:51:14 +0200

    MOTECH-1695: Can retrieve objects from parameters map in EventLog entity.
    Now when some errors will occur during blob deserialisation then mds will try
    use context class loader, class loader from mds-entities bundle and class loader
    from joda-time bundle.
    
    Change-Id: Ieec4f18cf93298de8b7e24e612f469631b38ace8

---
+++ platform/mds/mds-test-bundle/src/main/java/org/motechproject/mds/test/domain/mapdeserialisation/EntityWithStringObjectMap.java
@@ -0,0 +1,23 @@
+package org.motechproject.mds.test.domain.mapdeserialisation;
+
+import org.motechproject.mds.annotations.Entity;
+import org.motechproject.mds.annotations.Field;
+import org.motechproject.mds.domain.MdsEntity;
+
+import java.util.Map;
+
+@Entity
+public class EntityWithStringObjectMap extends MdsEntity {
+
+    @Field
+    private Map<String, Object> params;
+
+    public Map<String, Object> getParams() {
+        return params;
+    }
+
+    public void setParams(Map<String, Object> params) {
+        this.params = params;
+    }
+
+}
---
+++ platform/mds/mds-test-bundle/src/main/java/org/motechproject/mds/test/service/mapdeserialisation/EntityWithStringObjectMapDataService.java
@@ -0,0 +1,7 @@
+package org.motechproject.mds.test.service.mapdeserialisation;
+
+import org.motechproject.mds.service.MotechDataService;
+import org.motechproject.mds.test.domain.mapdeserialisation.EntityWithStringObjectMap;
+
+public interface EntityWithStringObjectMapDataService extends MotechDataService<EntityWithStringObjectMap> {
+}
