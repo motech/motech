1fe21e91d2319d3b38f807b1f709df9c8c7d56fe
Waldemar Strzelczyk <wstrzelczyk@soldevelo.com>
2015-09-24 11:04:58 +0200

    Removed duplicate methods for combobox

--- platform/mds/mds-web/src/main/resources/webapp/js/controllers.js
+++ platform/mds/mds-web/src/main/resources/webapp/js/controllers.js
@@ -3503,54 +3503,6 @@
             $("#instanceBrowserModal").modal('hide');
         };
 
-        /**
-        * Return available values for combobox field.
-        *
-        * @param {Array} setting A array of field settings.
-        * @return {Array} A array of possible combobox values.
-        */
-        $scope.getComboboxValues = function (settings) {
-            var labelValues = MDSUtils.find(settings, [{field: 'name', value: 'mds.form.label.values'}], true).value, keys = [], key;
-            // Check the user supplied flag, if true return string set
-            if (MDSUtils.find(settings, [{field: 'name', value: 'mds.form.label.allowUserSupplied'}], true).value === true){
-                return labelValues;
-            } else {
-                if (labelValues !== undefined && labelValues[0].indexOf(":") !== -1) {
-                    labelValues =  $scope.getAndSplitComboboxValues(labelValues);
-                    for(key in labelValues) {
-                        keys.push(key);
-                    }
-                    return keys;
-                } else {        // there is no colon, so we are dealing with a string set, not a map
-                    return labelValues;
-                }
-            }
-        };
-
-        $scope.getComboboxDisplayName = function (settings, value) {
-            var labelValues = MDSUtils.find(settings, [{field: 'name', value: 'mds.form.label.values'}], true).value;
-            // Check the user supplied flag, if true return string set
-            if (MDSUtils.find(settings, [{field: 'name', value: 'mds.form.label.allowUserSupplied'}], true).value === true){
-                return value;
-            } else {
-                if (labelValues !== undefined && labelValues[0].indexOf(":") !== -1) {
-                    labelValues =  $scope.getAndSplitComboboxValues(labelValues);
-                    return labelValues[value];
-                } else {         // there is no colon, so we are dealing with a string set, not a map
-                    return value;
-                }
-            }
-        };
-
-        $scope.getAndSplitComboboxValues = function (labelValues) {
-            var doublet, i, map = {};
-            for (i = 0; i < labelValues.length; i += 1) {
-                doublet = labelValues[i].split(":");
-                map[doublet[0]] = doublet[1];
-            }
-            return map;
-        };
-
         $scope.addRelatedInstance = function(id, entity, field) {
             blockUI();
             $http.get('../mds/instances/' + $scope.selectedEntity.id + '/field/' + field.id + '/instance/' + id)
