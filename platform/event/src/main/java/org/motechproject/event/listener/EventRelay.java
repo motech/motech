5ce4a550791ef86b79145a32a72b939d8e93fbae
Paweł Gesek <pgesek@soldevelo.com>
2015-10-05 13:46:23 +0200

    MOTECH-1908: Improved javadocs in EventRelay
    * Moved the part about having to have listeners registered lower
    * Started the javadoc with the queue/topic explanation

--- platform/event/src/main/java/org/motechproject/event/listener/EventRelay.java
+++ platform/event/src/main/java/org/motechproject/event/listener/EventRelay.java
@@ -11,22 +11,26 @@ import org.motechproject.event.MotechEvent;
 public interface EventRelay {
 
     /**
-     * Publishes the event message in a queue. The message will only go to ActiveMQ if there are listeners
-     * registered for the subject (in this instance). Meaning if you have clustered Motech instances,
-     * you must ensure they both have the listeners registered. The message is then handled by exactly one
-     * Motech instance, by calling {@link org.motechproject.event.listener.impl.ServerEventRelay#relayQueueEvent(org.motechproject.event.MotechEvent)}
-     * service method.
+     * Publishes the event message in a queue. The message goes a JMS queue, so if you have multiple Motech instances,
+     * only one of them will be chosen by ActiveMQ as the recipient of the event. This mechanism allows achieving scalability in a cluster.
+     * This is the method to use for sending your event, unless you are absolutely sure you want the event being processed
+     * by all your nodes in the cluster simultaneously. The message is then handled by exactly one Motech instance, by calling
+     * {@link org.motechproject.event.listener.impl.ServerEventRelay#relayQueueEvent(MotechEvent)} service method.
+     * The message will only go to ActiveMQ if there are listeners registered for the subject (in this instance),
+     * meaning if you have clustered Motech instances, you must ensure they all have the listeners registered.
      *
      * @param motechEvent the event to be sent
      */
     void sendEventMessage(MotechEvent motechEvent);
 
     /**
-     * Publishes the event message in a topic. The message will only go to ActiveMQ if there are listeners
-     * registered for the subject (in this instance). Meaning if you have clustered Motech instances,
-     * you must ensure they both have the listeners registered. The message is then handled by all Motech instances
-     * that are subscribed to a topic, by calling {@link org.motechproject.event.listener.impl.ServerEventRelay#relayTopicEvent(org.motechproject.event.MotechEvent)}
-     * service method. This method should only be used when it is desired to broadcast an event to all Motech instances.
+     * Publishes the event message in a topic. The message goes to a JMS topic, so if you have multiple Motech instances, they will
+     * all receive the event. This allows broadcasting administration-type events that should be handled by each node separately.
+     * This method should be only used if you are absolutely sure that the event should get processed by all your nodes in the cluster simultaneously.
+     * The message is then handled by all Motech instances, by calling
+     * {@link org.motechproject.event.listener.impl.ServerEventRelay#relayTopicEvent(MotechEvent)} service method.
+     * The message will only go to ActiveMQ if there are listeners registered for the subject (in this instance),
+     * meaning if you have clustered Motech instances, you must ensure they all have the listeners registered.
      *
      * @param motechEvent the event to be broadcast
      */
