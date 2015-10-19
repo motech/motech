454d383c51d3135addcc499a89653c8744a7467f
Kamil <kmadej@soldevelo.com>
2015-10-19 17:50:12 +0200

    MOTECH-1965: History button on the UI doesn't work

--- platform/mds/mds-web/src/main/java/org/motechproject/mds/web/controller/InstanceController.java
+++ platform/mds/mds-web/src/main/java/org/motechproject/mds/web/controller/InstanceController.java
@@ -140,9 +140,9 @@ public class InstanceController extends MdsController {
         List<HistoryRecord> historyRecordsList = instanceService.getInstanceHistory(entityId, instanceId, queryParams);
 
         long recordCount = instanceService.countHistoryRecords(entityId, instanceId);
-        int rowCount = (int) Math.ceil(recordCount / (double) settings.getRows());
+        int rowCount = (int) Math.ceil(recordCount / (double) queryParams.getPageSize());
 
-        return new Records<>(settings.getPage(), rowCount, (int) recordCount, historyRecordsList);
+        return new Records<>(queryParams.getPage(), rowCount, (int) recordCount, historyRecordsList);
     }
 
     @RequestMapping(value = "/instances/{entityId}/{instanceId}/previousVersion/{historyId}", method = RequestMethod.GET)
@@ -200,9 +200,9 @@ public class InstanceController extends MdsController {
         List<EntityRecord> trashRecordsList = instanceService.getTrashRecords(entityId, queryParams);
 
         long recordCount = instanceService.countTrashRecords(entityId);
-        int rowCount = (int) Math.ceil(recordCount / (double) settings.getRows());
+        int rowCount = (int) Math.ceil(recordCount / (double) queryParams.getPageSize());
 
-        return new Records<>(settings.getPage(), rowCount, (int) recordCount, trashRecordsList);
+        return new Records<>(queryParams.getPage(), rowCount, (int) recordCount, trashRecordsList);
 
     }
 
@@ -271,9 +271,9 @@ public class InstanceController extends MdsController {
             recordCount = instanceService.countRecords(entityId);
         }
 
-        int rowCount = (int) Math.ceil(recordCount / (double) settings.getRows());
+        int rowCount = (int) Math.ceil(recordCount / (double) queryParams.getPageSize());
 
-        return new Records<>(settings.getPage(), rowCount, (int) recordCount, entityRecords);
+        return new Records<>(queryParams.getPage(), rowCount, (int) recordCount, entityRecords);
     }
 
     @RequestMapping(value = "/instances/{entityId}/csvimport", method = RequestMethod.POST)
