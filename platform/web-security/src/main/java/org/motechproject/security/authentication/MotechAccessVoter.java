2e0e5608d86d7f1c8ba2c91e401a364abb591e39
Sebastian Brudzinski <sbrudzinski@soldevelo.com>
2015-11-09 17:57:40 +0100

    MOTECH-1961 Added logging in MotechAccessVoter

--- platform/web-security/src/main/java/org/motechproject/security/authentication/MotechAccessVoter.java
+++ platform/web-security/src/main/java/org/motechproject/security/authentication/MotechAccessVoter.java
@@ -2,6 +2,8 @@ package org.motechproject.security.authentication;
 
 import org.apache.commons.lang.StringUtils;
 import org.motechproject.security.constants.SecurityConfigConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.access.AccessDecisionVoter;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.core.Authentication;
@@ -19,6 +21,8 @@ import java.util.Collection;
  * permission, otherwise they are granted access.
  */
 public class MotechAccessVoter implements AccessDecisionVoter<Object> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MotechAccessVoter.class);
 
     @Override
     public boolean supports(ConfigAttribute attribute) {
@@ -38,6 +42,7 @@ public class MotechAccessVoter implements AccessDecisionVoter<Object> {
      * ACCESS_DENIED, otherwise return ACCESS_GRANTED
      *
      * @param authentication to be used for check
+     * @param object the resource to which the access is being voted on
      * @param attributes that contains information about access for users
      * @return ACCESS_ABSTAIN, ACCESS_DENIED or ACCESS_GRANTED
      */
@@ -45,8 +50,11 @@ public class MotechAccessVoter implements AccessDecisionVoter<Object> {
     public int vote(Authentication authentication, Object object, Collection<ConfigAttribute> attributes) {
         int result = ACCESS_ABSTAIN;
 
+        LOGGER.debug("Voting on the access to the {} for user {} with permissions {}", object, authentication.getPrincipal(), authentication.getAuthorities());
+
         for (ConfigAttribute attribute : attributes) {
             if (this.supports(attribute)) {
+                LOGGER.debug("Verifying supported attribute: " + attribute.getAttribute());
                 result = ACCESS_DENIED;
 
                 if (StringUtils.equalsIgnoreCase(SecurityConfigConstants.USER_ACCESS_PREFIX + authentication.getName(),
@@ -56,6 +64,17 @@ public class MotechAccessVoter implements AccessDecisionVoter<Object> {
             }
         }
 
+        logAccessResult(result, object);
         return result;
+    }
+
+    private void logAccessResult(int result, Object object) {
+        if (result == ACCESS_GRANTED) {
+            LOGGER.info("Access to the resource {} GRANTED", object);
+        } else if (result == ACCESS_DENIED) {
+            LOGGER.info("Access to the resource {} DENIED", object);
+        } else {
+            LOGGER.info("Access to the resource {} ABSTAIN", object);
+        }
     }
 }
