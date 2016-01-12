4d511c3371968d5a3965632dc6848d8b0c54df8d
Nick Reid <nickreid@nickreid.com>
2016-01-12 10:52:02 -0800

    MOTECH-2022 Static UI (0.8) Changed development mode detection to use ApplicationEnvironment class

--- platform/server-bundle/src/main/java/org/motechproject/server/web/DevelopmentModeInterceptor.java
+++ platform/server-bundle/src/main/java/org/motechproject/server/web/DevelopmentModeInterceptor.java
@@ -6,6 +6,8 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.motechproject.osgi.web.ext.ApplicationEnvironment;
+
 /**
  * This class sets and updates the path used for static resources in JSP views
  */
@@ -14,9 +16,7 @@ public class DevelopmentModeInterceptor extends HandlerInterceptorAdapter {
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView){
         if(modelAndView != null) {
-            String devString = "DEVELOPMENT";
-            Boolean developmentMode = devString.equals(System.getenv("ENVIRONMENT"));
-            modelAndView.addObject("developmentMode", developmentMode);
+            modelAndView.addObject("developmentMode", ApplicationEnvironment.isInDevelopmentMode());
         }
     }
 }
