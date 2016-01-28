c46ae160a3bf587b7b4a476662cffd254d689685
atish <atish@beehyv.com>
2016-01-28 18:07:02 +0530

    MOTECH-1659: Add support for float, short and other potential unsupported basic types in MDS
    - Resolved Test failures
    - Corrected Type validations

--- platform/mds/mds/src/main/resources/db/migration/mysql/V3__MOTECH-1659.sql
+++ platform/mds/mds/src/main/resources/db/migration/mysql/V3__MOTECH-1659.sql
@@ -27,17 +27,17 @@ LIMIT 1;
 
 -- Adding Type Validations to float
 
-INSERT INTO TypeValidation VALUES (12,'mds.field.validation.minValue',23), (13,'mds.field.validation.maxValue',23);
+INSERT INTO TypeValidation VALUES (12,'mds.field.validation.minValue',22), (13,'mds.field.validation.maxValue',22);
 
 -- Adding Type Validations to short
 
-INSERT INTO TypeValidation VALUES (14,'mds.field.validation.minValue',24), (15,'mds.field.validation.maxValue',24);
+INSERT INTO TypeValidation VALUES (14,'mds.field.validation.minValue',23), (15,'mds.field.validation.maxValue',23);
 
 -- Joining Type and TypeValidation tables using TYPE_TYPE_VALIDATION table for float, short
 
-INSERT INTO TYPE_TYPE_VALIDATION VALUES (23, 12, 0), (23, 13, 1), (23, 3, 2), (23, 4, 3);
+INSERT INTO TYPE_TYPE_VALIDATION VALUES (22, 12, 0), (22, 13, 1), (22, 3, 2), (22, 4, 3);
 
-INSERT INTO TYPE_TYPE_VALIDATION VALUES (24, 14, 0), (24, 15, 1), (24, 3, 2), (24, 4, 3);
+INSERT INTO TYPE_TYPE_VALIDATION VALUES (23, 14, 0), (23, 15, 1), (23, 3, 2), (23, 4, 3);
 
 -- Adding content to TypeValidation_annotations table
 
@@ -45,6 +45,6 @@ INSERT INTO TypeValidation_annotations VALUES (12,'javax.validation.constraints.
 
 INSERT INTO TypeValidation_annotations VALUES (14,'javax.validation.constraints.DecimalMin',0), (14,'javax.validation.constraints.Min',1), (15,'javax.validation.constraints.DecimalMin',0), (15,'javax.validation.constraints.Min',1);
 
--- Adding Settings to types
+-- Adding Settings to Float Type
 
-INSERT INTO TYPE_TYPE_SETTING VALUES (23, 1, 0), (23, 2, 1);
+INSERT INTO TYPE_TYPE_SETTING VALUES (22, 1, 0), (22, 2, 1);
