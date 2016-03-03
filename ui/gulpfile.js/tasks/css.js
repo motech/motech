4ed03853648de3b5990ed6d1196de3864cc3c089
pkornowski <pkornowski@soldevelo.com>
2016-03-03 08:40:40 +0100

    MOTECH-2131: Remove gradients created with image file

--- ui/gulpfile.js/tasks/css.js
+++ ui/gulpfile.js/tasks/css.js
@@ -3,10 +3,11 @@ var gulp = require('gulp');
 var lib = require('bower-files')();
 var concat = require('gulp-concat');
 var bless = require('gulp-bless');
+var sass = require('gulp-sass');
 var path = require('path');
 
 var paths = {
-    src: path.join(config.assets.src, '**/*.css'),
+    src: path.join(config.assets.src, '**/*.scss'),
     dest: path.join(config.assets.dest, 'css')
 };
 
@@ -15,6 +16,7 @@ gulp.task('css', function () {
     files.push(paths.src);
     gulp.src(files)
         .pipe(concat('motech.css'))
+        .pipe(sass().on('error', sass.logError))
         .pipe(bless())
         .pipe(gulp.dest(paths.dest));
 });
