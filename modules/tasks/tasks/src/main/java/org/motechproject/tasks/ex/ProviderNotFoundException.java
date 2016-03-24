77e44c61ee993c8cbda9c5182ff882eae5b8e913
Paweł Gesek <pgesek@soldevelo.com>
2016-03-24 14:00:48 +0100

    MOTECH-2316: Improve the ProviderNotFoundException

--- modules/tasks/tasks/src/main/java/org/motechproject/tasks/ex/ProviderNotFoundException.java
+++ modules/tasks/tasks/src/main/java/org/motechproject/tasks/ex/ProviderNotFoundException.java
@@ -5,7 +5,9 @@ package org.motechproject.tasks.ex;
  */
 public class ProviderNotFoundException extends IllegalArgumentException {
 
-    public ProviderNotFoundException(long providerId) {
-        super("Unable to migrate task. Data provider with id " + providerId + " not found");
+    private static final long serialVersionUID = -5419738008480651643L;
+
+    public ProviderNotFoundException(String taskName, long providerId) {
+        super("Unable to migrate task: " + taskName + ". Data provider with id " + providerId + " not found");
     }
 }
