90f9f8971b4626502c6e1896d257e525471d1a17
pkornowski <pkornowski@soldevelo.com>
2016-03-25 14:06:31 +0100

    MOTECH-2292: Corrections after review

--- platform/mds/mds/src/test/java/org/motechproject/mds/it/service/EntityServiceContextIT.java
+++ platform/mds/mds/src/test/java/org/motechproject/mds/it/service/EntityServiceContextIT.java
@@ -220,11 +220,13 @@ public class EntityServiceContextIT extends BaseIT {
         entityDto.setName("myEntity");
         entityDto = entityService.createEntity(entityDto);
 
+        //add a new field to draft
         EntityDraft entityDraft = entityService.getEntityDraft(entityDto.getId());
+        entityService.saveDraftEntityChanges(entityDraft.getId(), DraftBuilder.forNewField("disp", "testFieldName", Long.class.getName()));
+        FieldDto field = selectFirst(entityService.getFields(entityDraft.getId()), having(on(FieldDto.class).getBasic().getName(), equalTo("testFieldName")));
+
         LookupDto lookup = new LookupDto("lookup", false, false, null, true);
         entityService.addLookups(entityDraft.getId(), Collections.singletonList(lookup));
-        entityService.saveDraftEntityChanges(entityDto.getId(), DraftBuilder.forNewField("disp", "testFieldName", Long.class.getName()));
-        FieldDto field = selectFirst(entityService.getFields(entityDto.getId()), having(on(FieldDto.class).getBasic().getName(), equalTo("testFieldName")));
 
         Map<String, Object> values = new HashMap<>();
         values.put("path", "indexes.0.$addField");
@@ -234,10 +236,10 @@ public class EntityServiceContextIT extends BaseIT {
         draftData.setEdit(true);
         draftData.setValues(values);
 
-        entityService.saveDraftEntityChanges(entityDto.getId(), draftData);
+        entityService.saveDraftEntityChanges(entityDraft.getId(), draftData);
         entityService.commitChanges(entityDto.getId());
 
-        assertEquals("testFieldName", entityService.getLookupByName(entityDto.getId(),"lookup").getLookupField("testFieldName").getName());
+        assertNotNull(entityService.getLookupByName(entityDto.getId(),"lookup").getLookupField("testFieldName"));
     }
 
     @Test
