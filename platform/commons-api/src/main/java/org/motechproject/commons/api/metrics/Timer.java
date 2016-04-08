7912f69495e92ed9b62dd6c2f5d397e2ab6ccf7f
Weronika Ciecierska <wciecierska@soldevelo.com>
2016-04-08 16:33:03 +0200

    MOTECH-2338: Moved methods in correct orders and added javadoc in Timer class

--- platform/commons-api/src/main/java/org/motechproject/commons/api/metrics/Timer.java
+++ platform/commons-api/src/main/java/org/motechproject/commons/api/metrics/Timer.java
@@ -2,8 +2,11 @@ package org.motechproject.commons.api.metrics;
 
 import java.text.DecimalFormat;
 
-
+/**
+ * Class for displaying information about measured time and frequency.
+ */
 public class Timer {
+
     private static final DecimalFormat FMT_SEC = new DecimalFormat("#,##0.000");
     private static final DecimalFormat FMT_INT = new DecimalFormat("#,##0");
     private static final DecimalFormat FMT_DEC = new DecimalFormat("#,##0.000");
@@ -16,33 +19,73 @@ public class Timer {
     private String obj;
     private String objs;
 
-
-    private void resetTimer() {
-        start = System.currentTimeMillis();
-    }
-
-
+    /**
+     * Constructor.
+     */
     public Timer() {
         resetTimer();
     }
 
-
+    /**
+     * Constructor.
+     * @param obj - singular noun of counted object
+     * @param objs - plural form of counted object
+     */
     public Timer(String obj, String objs) {
         this();
         this.obj = obj;
         this.objs = objs;
     }
 
-
+    /**
+     * Resets the timer.
+     */
     public void reset() {
         resetTimer();
     }
 
+    /**
+     * Returns information about measured time.
+     * @return difference between the current and initial time in "hours minutes seconds" format
+     */
+    public String time() {
+        return durationString(System.currentTimeMillis() - start);
+    }
+
+    /**
+     * Returns information about measured time in milliseconds.
+     * @return difference between the current and initial time in milliseconds
+     */
+    public String millis() {
+        return milliString(System.currentTimeMillis() - start);
+    }
+
+    /**
+     * Returns information about frequency.
+     * @param count how many objects were counted
+     * @return information about how many objects were counted between the current and initial time and frequency
+     */
+    public String frequency(long count) {
+        long duration = System.currentTimeMillis() - start;
+        double freq = (count * MILLIS_PER_SEC) / ((duration) * 1.0);
+
+        return String.format(
+                "%s %s in %s at %s%s/s",
+                FMT_INT.format(count),
+                count == 1 ? obj : objs,
+                durationString(duration),
+                FMT_DEC.format((count * MILLIS_PER_SEC) / ((duration) * 1.0)),
+                freq == 1.0 ? obj : objs
+        );
+    }
+
+    private void resetTimer() {
+        start = System.currentTimeMillis();
+    }
 
     private String milliString(long duration) {
         return String.format("%sms", FMT_INT.format(duration));
     }
-
 
     private String durationString(long duration) {
         long millis = duration;
@@ -65,30 +108,5 @@ public class Timer {
         }
 
         return milliString(millis);
-    }
-
-
-    public String time() {
-        return durationString(System.currentTimeMillis() - start);
-    }
-
-
-    public String millis() {
-        return milliString(System.currentTimeMillis() - start);
-    }
-
-
-    public String frequency(long count) {
-        long duration = System.currentTimeMillis() - start;
-        double freq = (count * MILLIS_PER_SEC) / ((duration) * 1.0);
-
-        return String.format(
-                "%s %s in %s at %s%s/s",
-                FMT_INT.format(count),
-                count == 1 ? obj : objs,
-                durationString(duration),
-                FMT_DEC.format((count * MILLIS_PER_SEC) / ((duration) * 1.0)),
-                freq == 1.0 ? obj : objs
-        );
     }
 }
