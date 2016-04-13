492c104226a539d79505cf07348b83d5d93e9f08
kmadej <kmadej@soldevelo.com>
2016-04-13 17:33:25 +0200

    MOTECH-2094: Added a migration to scheduler module
    After scheduler refactor, class service.impl.MotechScheduledJob changed package
    to service.MotechScheduledJob. This class name is stored in motechquartz database,
    so we lost backward compatibility. This migration should fix the problem and now
    old jobs can be properly fired after scheduler API clean up.

---
+++ modules/scheduler/scheduler/src/main/resources/db/migration/scheduler/default/V3__MOTECH-2094.sql
@@ -0,0 +1,2 @@
+
+UPDATE "QRTZ_JOB_DETAILS" SET "JOB_CLASS_NAME"='org.motechproject.scheduler.service.MotechScheduledJob' WHERE "JOB_CLASS_NAME" LIKE 'org.motechproject.scheduler.service.impl.MotechScheduledJob'
