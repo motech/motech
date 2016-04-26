ddc98fc8dcb62ff59536affc5c6b12adb6fd1e45
shanmukhm <shanmukh111@gmail.com>
2016-04-26 18:48:15 +0530

    Motech-2423 Added IT to check cascade delete

---
+++ platform/mds/mds-test-bundle/src/main/java/org/motechproject/mds/test/service/cascadedelete/EntityZDataService.java
@@ -0,0 +1,8 @@
+package org.motechproject.mds.test.service.cascadedelete;
+
+import org.motechproject.mds.service.MotechDataService;
+import org.motechproject.mds.test.domain.cascadedelete.EntityZ;
+
+public interface EntityZDataService extends MotechDataService<EntityZ>{
+
+}
