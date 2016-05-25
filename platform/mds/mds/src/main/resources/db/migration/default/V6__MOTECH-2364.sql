9ae6c346f9462a38275be4af6c1ff8bb4c1b9742
pkornowski <pkornowski@soldevelo.com>
2016-05-25 12:32:08 +0200

    MOTECH-2364: Corrected db migration.

--- platform/mds/mds/src/main/resources/db/migration/default/V6__MOTECH-2364.sql
+++ platform/mds/mds/src/main/resources/db/migration/default/V6__MOTECH-2364.sql
@@ -1,22 +1,18 @@
 -- Changing serviceInterface path from org.motechproject.openmrs19.tasks.OpenMRSActionProxyService to org.motechproject.openmrs.tasks.OpenMRSActionProxyService
 
-CREATE TABLE IF NOT EXISTS "motechdata.MOTECH_TASKS_ACTIONEVENT" (
-	"id" bigint(20) NOT NULL AUTO_INCREMENT,
-	"serviceInterface" varchar(255),
-	"serviceMethod" varchar(255),
-	"serviceMethodCallManner" varchar(255),
-	"creationDate" datetime,
-	"creator" varchar(255),
-	"description" varchar(255),
-	"displayName" varchar(255),
-	"modificationDate" datetime,
-	"modifiedBy" varchar(255),
-	"name" varchar(255),
-	"owner" varchar(255),
-	"subject" varchar(255),
-	"actionTaskEvents_id_OWN" bigint(20),
-	"actionTaskEvents_INTEGER_IDX" int(11),
-	PRIMARY KEY ("id")
-);
+CREATE OR REPLACE FUNCTION updateServiceInt()
+RETURNS void AS $$
+BEGIN
+    IF (SELECT count(*) FROM information_schema.columns WHERE table_name = 'MOTECH_TASKS_ACTIONEVENT') > 0 THEN
 
-UPDATE "motechdata.MOTECH_TASKS_ACTIONEVENT" SET "serviceInterface"='org.motechproject.openmrs.tasks.OpenMRSActionProxyService' WHERE "serviceInterface" LIKE 'org.motechproject.openmrs19.tasks.OpenMRSActionProxyService'
+        UPDATE "MOTECH_TASKS_ACTIONEVENT" set "serviceInterface"='org.motechproject.openmrs.tasks.OpenMRSActionProxyService' where "serviceInterface"='org.motechproject.openmrs19.tasks.OpenMRSActionProxyService';
+
+    END IF;
+
+END
+
+$$ LANGUAGE plpgsql;
+
+SELECT updateServiceInt();
+
+DROP FUNCTION updateServiceInt();
