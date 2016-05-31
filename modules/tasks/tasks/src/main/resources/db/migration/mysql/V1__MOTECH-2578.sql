265405688ab42a7ba89c5efc7d9e481da58be7e1
pkornowski <pkornowski@soldevelo.com>
2016-05-31 17:11:34 +0200

    MOTECH-2578: Corrected DB migration.

--- modules/tasks/tasks/src/main/resources/db/migration/mysql/V1__MOTECH-2578.sql
+++ modules/tasks/tasks/src/main/resources/db/migration/mysql/V1__MOTECH-2578.sql
@@ -1,23 +1,3 @@
 -- Changing serviceInterface path from org.motechproject.openmrs19.tasks.OpenMRSActionProxyService to org.motechproject.openmrs.tasks.OpenMRSActionProxyService
 
-DROP PROCEDURE IF EXISTS updateServiceInt;
-
-DELIMITER //
-
-CREATE PROCEDURE updateServiceInt()
-
-BEGIN
-
-IF (select count(*) from information_schema.columns where (table_schema = 'motechdata' AND table_name = 'MOTECH_TASKS_ACTIONEVENT')) > 0 THEN
-
 Update motechdata.MOTECH_TASKS_ACTIONEVENT set serviceInterface='org.motechproject.openmrs.tasks.OpenMRSActionProxyService' where (serviceInterface='org.motechproject.openmrs19.tasks.OpenMRSActionProxyService' AND id <> 0);
-
-END IF;
-
-END//
-
-DELIMITER ;
-
-CALL updateServiceInt();
-
-DROP PROCEDURE IF EXISTS updateServiceInt;
