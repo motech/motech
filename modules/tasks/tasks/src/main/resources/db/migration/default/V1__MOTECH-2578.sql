b622459cb37d8ba8f5b0d6be9b1ff9dcda503f39
Przemyslaw Kornowski <pkornowski@soldevelo.com>
2016-06-01 16:24:01 +0200

    MOTECH-2578: Corrected DB migration.

--- modules/tasks/tasks/src/main/resources/db/migration/default/V1__MOTECH-2578.sql
+++ modules/tasks/tasks/src/main/resources/db/migration/default/V1__MOTECH-2578.sql
@@ -1,3 +1,3 @@
 -- Changing serviceInterface path from org.motechproject.openmrs19.tasks.OpenMRSActionProxyService to org.motechproject.openmrs.tasks.OpenMRSActionProxyService
 
-UPDATE "MOTECH_TASKS_ACTIONEVENT" set "serviceInterface"='org.motechproject.openmrs.tasks.OpenMRSActionProxyService' where ("serviceInterface"='org.motechproject.openmrs19.tasks.OpenMRSActionProxyService' AND "id" <> 0);
+UPDATE "MOTECH_TASKS_ACTIONEVENT" set "serviceInterface"='org.motechproject.openmrs.tasks.OpenMRSActionProxyService' where "serviceInterface"='org.motechproject.openmrs19.tasks.OpenMRSActionProxyService';
