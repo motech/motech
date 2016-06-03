9f77458181bbc4e35e797a12bc496f6e57fef1a6
Nikodem Graczewski <ngraczewski@soldevelo.com>
2016-06-03 10:46:56 +0200

    MOTECH-2543: Fixed problem with key:value map in the Tasks module (#566)
    * MOTECH-2543: Fixed problem with key:value map in the Tasks module
    
    * MOTECH-2543: Added test

--- modules/tasks/tasks/src/test/java/org/motechproject/tasks/service/impl/TaskActionExecutorTest.java
+++ modules/tasks/tasks/src/test/java/org/motechproject/tasks/service/impl/TaskActionExecutorTest.java
@@ -6,29 +6,33 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.motechproject.event.MotechEvent;
 import org.motechproject.event.listener.EventRelay;
-import org.motechproject.tasks.domain.mds.channel.builder.ActionEventBuilder;
 import org.motechproject.tasks.domain.mds.channel.ActionEvent;
 import org.motechproject.tasks.domain.mds.channel.ActionParameter;
+import org.motechproject.tasks.domain.mds.channel.builder.ActionEventBuilder;
 import org.motechproject.tasks.domain.mds.task.Task;
 import org.motechproject.tasks.domain.mds.task.TaskActionInformation;
 import org.motechproject.tasks.domain.mds.task.builder.TaskBuilder;
 import org.motechproject.tasks.exception.ActionNotFoundException;
 import org.motechproject.tasks.exception.TaskHandlerException;
 import org.motechproject.tasks.service.TaskActivityService;
-import org.motechproject.tasks.service.util.TaskContext;
 import org.motechproject.tasks.service.TaskService;
+import org.motechproject.tasks.service.util.TaskContext;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedSet;
 import java.util.TreeSet;
 
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.motechproject.tasks.domain.mds.ParameterType.MAP;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TaskActionExecutorTest {
@@ -176,6 +180,70 @@ public class TaskActionExecutorTest {
         taskActionExecutor.execute(task, actionInformation, new TaskContext(task, new HashMap(), activityService));
 
         verify(activityService).addWarning(task, "task.warning.serviceUnavailable", "serviceInterface");
+    }
+
+    @Test
+    public void shouldExecuteTaskIfActionMapParameterHasValueWithMixedTypes() throws Exception {
+        TaskActionInformation actionInformation = prepareTaskActionInformation();
+        ActionEvent actionEvent = prepareActionEvent();
+
+        when(taskService.getActionEventFor(actionInformation)).thenReturn(actionEvent);
+
+        Task task = new TaskBuilder().addAction(new TaskActionInformation("Action", "channel", "module", "0.1", "actionSubject")).build();
+
+        TaskActionExecutor taskActionExecutor = new TaskActionExecutor(taskService, activityService, eventRelay);
+        taskActionExecutor.setBundleContext(bundleContext);
+
+        taskActionExecutor.execute(task, actionInformation, prepareTaskContext(task));
+
+        verify(eventRelay).sendEventMessage(eq(prepareMotechEvent()));
+    }
+
+    private MotechEvent prepareMotechEvent() {
+        Map<String, Object> parameters = new HashMap<>();
+        Map<String, Object> map = new HashMap<>();
+        map.put("key1", "value123");
+        parameters.put("map", map);
+        return new MotechEvent("actionSubject", parameters);
+    }
+
+    private TaskContext prepareTaskContext(Task task) {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("id", 123);
+        return new TaskContext(task, parameters, activityService);
+    }
+
+    private TaskActionInformation prepareTaskActionInformation() {
+        TaskActionInformation actionInformation = new TaskActionInformation();
+        actionInformation.setDisplayName("action");
+        actionInformation.setChannelName("channel");
+        actionInformation.setModuleName("module");
+        actionInformation.setModuleVersion("0.1");
+        actionInformation.setSubject("actionSubject");
+
+        Map<String, String> values = new HashMap<>();
+        values.put("map", "key1:value{{trigger.id}}");
+        actionInformation.setValues(values);
+
+        return actionInformation;
+    }
+
+    private ActionEvent prepareActionEvent() {
+        ActionEvent actionEvent = new ActionEvent();
+        actionEvent.setDisplayName("Action");
+        actionEvent.setSubject("actionSubject");
+        actionEvent.setDescription("");
+
+        SortedSet<ActionParameter> parameters = new TreeSet<>();
+        ActionParameter parameter = new ActionParameter();
+        parameter.setDisplayName("Map");
+        parameter.setKey("map");
+        parameter.setType(MAP);
+        parameter.setOrder(1);
+        parameters.add(parameter);
+        actionEvent.setActionParameters(parameters);
+
+        return actionEvent;
     }
 
     private class TestService {
