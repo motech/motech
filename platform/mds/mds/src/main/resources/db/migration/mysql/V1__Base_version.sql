69d40cef1edca60b24746a29b8791b52eb7569fc
Przemysław Kornowski <pkornowski@soldevelo.com>
2016-06-06 16:56:21 +0200

    Added new migration to fixed module migrations feature. (#573)
    * Added new migration to fixed module migrations feature.
    
    * MOTECH-2578: Corrected postgres DB migration.
    
    * MOTECH-2578: Corrections after review.

--- platform/mds/mds/src/main/resources/db/migration/mysql/V1__Base_version.sql
+++ platform/mds/mds/src/main/resources/db/migration/mysql/V1__Base_version.sql
@@ -331,7 +331,7 @@ CREATE TABLE "SchemaChangeLock" (
 
 DROP TABLE IF EXISTS "MigrationMapping";
 CREATE TABLE "MigrationMapping" (
-  "flywayMigrationVersion" int NOT NULL AUTO_INCREMENT,
+  "flywayMigrationVersion" int NOT NULL,
   "moduleMigrationVersion" int NOT NULL,
   "moduleName" varchar(255) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
   PRIMARY KEY ("flywayMigrationVersion")
