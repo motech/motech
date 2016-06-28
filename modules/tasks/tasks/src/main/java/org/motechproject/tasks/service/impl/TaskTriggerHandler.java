8b8d33a5f7dcd085fc4dad01b102799a092df2e7
Mateusz Kwiatkowski <1stmateusz@gmail.com>
2016-06-28 15:57:44 +0200

    MOTECH-2601: fix executing job with metadata

--- modules/tasks/tasks/src/main/java/org/motechproject/tasks/service/impl/TaskTriggerHandler.java
+++ modules/tasks/tasks/src/main/java/org/motechproject/tasks/service/impl/TaskTriggerHandler.java
@@ -219,21 +219,25 @@ public class TaskTriggerHandler implements TriggerHandler {
 
     private void scheduleTaskRetry(Task task, Map<String, Object> parameters) {
         Map<String, Object> eventParameters = new HashMap<>();
+        Map<String, Object> eventMetadata = new HashMap<>();
+
         eventParameters.putAll(parameters);
 
-        eventParameters.put(TASK_ID, task.getId());
-        eventParameters.put(REPEAT_COUNT, task.getNumberOfRetries());
-        eventParameters.put(REPEAT_INTERVAL_TIME, task.getRetryIntervalInMilliseconds() / 1000);
-        eventParameters.put(JOB_SUBJECT, task.getTrigger().getEffectiveListenerRetrySubject());
+        eventMetadata.put(TASK_ID, task.getId());
+        eventMetadata.put(REPEAT_COUNT, task.getNumberOfRetries());
+        eventMetadata.put(REPEAT_INTERVAL_TIME, task.getRetryIntervalInMilliseconds() / 1000);
+        eventMetadata.put(JOB_SUBJECT, task.getTrigger().getEffectiveListenerRetrySubject());
 
-        eventRelay.sendEventMessage(new MotechEvent(SCHEDULE_REPEATING_JOB, eventParameters));
+        eventRelay.sendEventMessage(new MotechEvent(SCHEDULE_REPEATING_JOB, eventParameters, null, eventMetadata));
     }
 
     private void unscheduleTaskRetry(String jobSubject) {
         Map<String, Object> eventParameters = new HashMap<>();
-        eventParameters.put(JOB_SUBJECT, jobSubject);
+        Map<String, Object> eventMetadata = new HashMap<>();
 
-        eventRelay.sendEventMessage(new MotechEvent(UNSCHEDULE_REPEATING_JOB, eventParameters));
+        eventMetadata.put(JOB_SUBJECT, jobSubject);
+
+        eventRelay.sendEventMessage(new MotechEvent(UNSCHEDULE_REPEATING_JOB, eventParameters, null, eventMetadata));
     }
 
     private void handleError(Map<String, Object> params, Task task, TaskHandlerException e) {
