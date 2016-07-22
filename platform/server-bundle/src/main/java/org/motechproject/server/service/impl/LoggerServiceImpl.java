22ed2a5a33a1e3260d07795af32e0b4c19c78b27
Artur Fijał <afijal@soldevelo.com>
2016-07-22 13:20:10 +0200

    MOTECH-2245 Corrections after reviews

--- platform/server-bundle/src/main/java/org/motechproject/server/service/impl/LoggerServiceImpl.java
+++ platform/server-bundle/src/main/java/org/motechproject/server/service/impl/LoggerServiceImpl.java
@@ -4,28 +4,32 @@ import org.motechproject.server.service.LoggerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * The <code>LoggerServiceImpl</code> class provides API for logging messages to server logs
  */
 @Service("loggerService")
-public class LoggerServiceImpl implements LoggerService{
+public class LoggerServiceImpl implements LoggerService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggerServiceImpl.class);
 
     @Override
-    @Transactional
     public void logMessage(String level, String message) {
         switch (level) {
-            case "error":
-                LOGGER.error(message);
+            case "logger.log.level.trace":
+                LOGGER.trace(message);
                 break;
-            case "info":
+            case "logger.log.level.debug":
+                LOGGER.debug(message);
+                break;
+            case "logger.log.level.info":
                 LOGGER.info(message);
                 break;
-            case "debug":
-                LOGGER.debug(message);
+            case "logger.log.level.warn":
+                LOGGER.warn(message);
+                break;
+            default:
+                LOGGER.error(message);
                 break;
         }
     }
