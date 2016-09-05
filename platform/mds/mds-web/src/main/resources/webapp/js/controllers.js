117848d33b0b4fb18dbbb96a62033dcc90dda76b
tomaszzawadzki <tzawadzki@soldevelo.com>
2016-09-05 10:36:46 +0200

    Changed defaultName to typeClass

--- platform/mds/mds-web/src/main/resources/webapp/js/controllers.js
+++ platform/mds/mds-web/src/main/resources/webapp/js/controllers.js
@@ -4196,7 +4196,7 @@
 
             var values = $scope.currentRecord.fields;
             angular.forEach (values, function(value, key) {
-                if(value.type.defaultName === "str" && value.value === ""){
+                if(value.type.typeClass === "java.lang.String" && value.value === ""){
                     value.value = null;
                 }
                 value.value = value.value === 'null' ? null : value.value;
