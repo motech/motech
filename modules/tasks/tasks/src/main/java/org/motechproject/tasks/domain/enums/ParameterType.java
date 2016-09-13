cce96fbeb0a3658e7ad6ee00b34d1919f8e5467b
Artur Fijał <afijal@soldevelo.com>
2016-09-13 17:30:37 +0200

    MOTECH-2861: Created toDto methods and constructors with dto object parameter in domain classes
    I also had to move enums from domain to domain.enums package because of package cycle detection between domain and dto

--- modules/tasks/tasks/src/main/java/org/motechproject/tasks/domain/enums/ParameterType.java
+++ modules/tasks/tasks/src/main/java/org/motechproject/tasks/domain/enums/ParameterType.java
@@ -1,4 +1,4 @@
-package org.motechproject.tasks.domain.mds;
+package org.motechproject.tasks.domain.enums;
 
 import org.codehaus.jackson.annotate.JsonIgnore;
 import org.codehaus.jackson.annotate.JsonValue;
