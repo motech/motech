eac37913724047cf79b5f2b568de0d6a31eea619
mkruszynski <mkruszynski@soldevelo.com>
2016-09-27 15:33:32 +0200

    MOTECH-2922: Changed build flow and deploy configuration

--- config_services.sh
+++ config_services.sh
@@ -8,14 +8,11 @@ sudo ln -s /etc/activemq/instances-available/main /etc/activemq/instances-enable
 sudo sed -e 's/<broker /<broker schedulerSupport="true" /' -i /etc/activemq/instances-enabled/main/activemq.xml
 sudo service activemq restart
 
-#Install and configure Tomcat, only if IT's for MOTECH will be fire
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    #Installation and configuration of Tomcat
-    sudo apt-get install -y tomcat7 -qq
-    sudo service tomcat7 stop
-    echo 'CATALINA_OPTS="-Xms512m -Xmx512m"' | sudo tee --append /usr/share/tomcat7/bin/setenv.sh
-    sudo chown -R tomcat7:tomcat7 /var/lib/tomcat7/ /usr/share/tomcat7/ /var/log/tomcat7/
-fi
+#Installation and configuration of Tomcat
+sudo apt-get install -y tomcat7 -qq
+sudo service tomcat7 stop
+echo 'CATALINA_OPTS="-Xms512m -Xmx512m"' | sudo tee --append /usr/share/tomcat7/bin/setenv.sh
+sudo chown -R tomcat7:tomcat7 /var/lib/tomcat7/ /usr/share/tomcat7/ /var/log/tomcat7/
 
 #Change root password in mysql
 if [ "$DB" = "mysql" ]; then
