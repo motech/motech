be73f457b1266b69700c7ff435bfb75a50c620bf
mkruszynski <mkruszynski@soldevelo.com>
2016-10-03 17:50:57 +0200

    MOTECH-2935: Travis mds performance tests

---
+++ run_build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ "$TRAVIS_EVENT_TYPE" != "cron" ]; then
+    git clone https://github.com/motech/motech.git ../motech-master -b master --single-branch
+
+    mkdir ~/.motech
+    cp ../motech-master/testdata/config-locations.properties ~/.motech/
+
+    cd ../motech-master/platform/mds/mds-performance-tests/
+    mvn -Dmds.performance.quantity=10000 clean install -U -PMDSP
+fi
