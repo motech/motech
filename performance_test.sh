11fe22d04ee693068334707c107bdba54b0f7ea4
mkruszynski <mkruszynski@soldevelo.com>
2016-10-04 10:03:27 +0200

    MOTECH-2935: fixed script syntax

--- performance_test.sh
+++ performance_test.sh
@@ -2,31 +2,30 @@
 
 if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
 
-	set -e
+    set -e
 
-	PERF_DIR=../motech-master/platform/mds/mds-performance-tests
+    PERF_DIR=../motech-master/platform/mds/mds-performance-tests
 
-	PERF_RES_DIR=$PERF_DIR/src/test/resources
+    PERF_RES_DIR=$PERF_DIR/src/test/resources
 
-	TRESHOLDS_FILE=/tmp/tresholds.csv
+    TRESHOLDS_FILE=/tmp/tresholds.csv
 
-	touch $TRESHOLDS_FILE
+    touch $TRESHOLDS_FILE
 
-	cat > $TRESHOLDS_FILE <<EOL
-	MdsStressIT,stressTestCreating,40000
-	MdsStressIT,stressTestRetrieval,300
-	MdsStressIT,stressTestUpdating,40000
-	MdsStressIT,stressTestDeleting,250000
-	MdsDiskSpaceUsageIT,testEudeDiskSpaceUsage,25
-	EOL
+    cat > $TRESHOLDS_FILE <<EOL
+MdsStressIT,stressTestCreating,40000
+MdsStressIT,stressTestRetrieval,300
+MdsStressIT,stressTestUpdating,40000
+MdsStressIT,stressTestDeleting,250000
+MdsDiskSpaceUsageIT,testEudeDiskSpaceUsage,25
+EOL
 
-	$PERF_RES_DIR/performanceCheck.sh -d ~/perf $PERF_DIR/target/performanceTestResult.log $TRESHOLDS_FILE
-	RESULT=$?
+    $PERF_RES_DIR/performanceCheck.sh -d ~/perf $PERF_DIR/target/performanceTestResult.log $TRESHOLDS_FILE
+    RESULT=$?
 
-	rm -f $TRESHOLDS_FILE
+    rm -f $TRESHOLDS_FILE
 
-	cat $PERF_DIR/target/performanceTestResult.log
-
-	exit $RESULT
+    cat $PERF_DIR/target/performanceTestResult.log
+    exit $RESULT
 
 fi
