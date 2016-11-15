e4783678a6204b385cfdda6bb5799a140f8be83f
mkruszynski <mkruszynski@soldevelo.com>
2016-11-15 11:37:39 +0100

    Added release plugin execution

--- run_build.sh
+++ run_build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 #Release
-if [ "$TRAVIS_EVENT_TYPE" = "api" ]; then
+if [ "$TRAVIS_EVENT_TYPE" = "api" ] && [ ! -z "$developmentVersion" ] && [ ! -z "$scmTag" ] && [ ! -z "$releaseVersion" ]; then
     mvn --settings deploy-settings.xml clean deploy -e -PIT,DEB,RPM -B -U
+    mvn -DdevelopmentVersion=$developmentVersion -Dscm.tag=$scmTag -DreleaseVersion=$releaseVersion -Dmaven.test.failure.ignore=false -Dscm.developerConnection=scm:git:git@github.com:motech/motech.git -Dscm.connection=scm:git:git@github.com:motech/motech.git release:clean release:prepare release:perform
 fi
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
