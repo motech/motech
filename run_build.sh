91216b4896b1ea65251bb3acaa9185d81b81defb
mkruszynski <mkruszynski@soldevelo.com>
2016-11-22 17:23:46 +0100

    Travis release build

--- run_build.sh
+++ run_build.sh
@@ -9,7 +9,12 @@ if [ "$TRAVIS_EVENT_TYPE" = "api" ] && [ ! -z "$developmentVersion" ] && [ ! -z 
     git checkout -f $TRAVIS_BRANCH
     git reset --hard $TRAVIS_BRANCH
 
-    mvn -DdevelopmentVersion=$developmentVersion -Dscm.tag=$scmTag -DreleaseVersion=$releaseVersion -Dmaven.test.failure.ignore=false -Dscm.developerConnection=scm:git:git@github.com:motech/motech.git -Dscm.connection=scm:git:git@github.com:motech/motech.git release:clean release:prepare release:perform
+    openssl aes-256-cbc -K $GITHUB_SHH_KEY -iv $GITHUB_SHH_IV -in github_ssh.enc -out id_rsa -d
+    mv id_rsa ~/.ssh/id_rsa
+    chmod 600 ~/.ssh/id_rsa
+    echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+
+    mvn --settings deploy-settings.xml -DdevelopmentVersion=$developmentVersion -Dscm.tag=$scmTag -DreleaseVersion=$releaseVersion -Dmaven.test.failure.ignore=false -Dscm.developerConnection=scm:git:git@github.com:motech/motech.git -Dscm.connection=scm:git:git@github.com:motech/motech.git release:clean release:prepare release:perform
 
 fi
 
