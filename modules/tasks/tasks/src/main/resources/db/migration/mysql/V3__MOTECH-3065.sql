b93b777f4ad704263ea16cacba6f7a8b2acb1853
mkruszynski <mkruszynski@soldevelo.com>
2016-12-13 13:23:08 +0100

    MOTECH-3065: Description fixed

--- modules/tasks/tasks/src/main/resources/db/migration/mysql/V3__MOTECH-3065.sql
+++ modules/tasks/tasks/src/main/resources/db/migration/mysql/V3__MOTECH-3065.sql
@@ -1,3 +1,3 @@
--- Changing column 'VALUE' from VARCHAR(500) to VARCHAR(20000)
+-- Changing column 'MESSAGE' from VARCHAR(500) to VARCHAR(20000)
 
 alter table MOTECH_TASKS_TASKERROR modify MESSAGE varchar(20000) NOT NULL;
