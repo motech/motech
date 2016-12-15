12259c027174598b76523668e04457e4e34d4857
mkruszynski <mkruszynski@soldevelo.com>
2016-12-15 11:46:27 +0100

    MOTECH-3065: Fixed column names in migration files

--- modules/tasks/tasks/src/main/resources/db/migration/default/V3__MOTECH-3065.sql
+++ modules/tasks/tasks/src/main/resources/db/migration/default/V3__MOTECH-3065.sql
@@ -1,3 +1,3 @@
 -- Changing column 'MESSAGE' from VARCHAR(500) to VARCHAR(20000)
 
-alter table "MOTECH_TASKS_TASKERROR" alter column "MESSAGE" type varchar(20000);
+alter table "MOTECH_TASKS_TASKERROR" alter column "message" type varchar(20000);
