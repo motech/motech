db09191330bf37f51136f2201741882e77919262
mkruszynski <mkruszynski@soldevelo.com>
2016-12-19 15:09:13 +0100

    MOTECH-3065: Changed new column types

--- modules/admin/src/main/resources/db/migration/mysql/V4__MOTECH-3065.sql
+++ modules/admin/src/main/resources/db/migration/mysql/V4__MOTECH-3065.sql
@@ -1,3 +1,3 @@
 -- Changing column 'TEXT' from VARCHAR(255) to VARCHAR(20000)
 
-alter table MOTECH_ADMIN_STATUSMESSAGE modify text varchar(20000) NOT NULL;
+alter table MOTECH_ADMIN_STATUSMESSAGE modify text MEDIUMTEXT NOT NULL;
--- modules/tasks/tasks/src/main/resources/db/migration/mysql/V3__MOTECH-3065.sql
+++ modules/tasks/tasks/src/main/resources/db/migration/mysql/V3__MOTECH-3065.sql
@@ -1,3 +1,3 @@
 -- Changing column 'MESSAGE' from VARCHAR(500) to VARCHAR(20000)
 
-alter table MOTECH_TASKS_TASKERROR modify message varchar(20000) NOT NULL;
+alter table MOTECH_TASKS_TASKERROR modify message MEDIUMTEXT NOT NULL;
