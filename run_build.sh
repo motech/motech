efa3dad4eee0b5d13cab0e0650dd86e5d31bf3e5
mkruszynski <mkruszynski@soldevelo.com>
2016-12-20 17:22:58 +0100

    MOTECH-3076: Reduction of travis logs length

--- run_build.sh
+++ run_build.sh
@@ -26,8 +26,8 @@ fi
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     if [ "$DB" = "mysql" ]; then
         echo "USE mysql;\nUPDATE user SET password=PASSWORD('password') WHERE user='root';\nFLUSH PRIVILEGES;\n" | mysql -u root
-        mvn clean install -PIT -U
+        mvn clean install -PIT -U -q
     elif [ "$DB" = "psql" ]; then
-        mvn -Dmotech.sql.password=password -Dmotech.sql.user=postgres -Dmaven.test.failure.ignore=false -Dmotech.sql.driver=org.postgresql.Driver -Dmotech.sql.dbtype=psql -Dmotech.sql.url=jdbc:postgresql://localhost:5432/ clean install -PIT -U
+        mvn -Dmotech.sql.password=password -Dmotech.sql.user=postgres -Dmaven.test.failure.ignore=false -Dmotech.sql.driver=org.postgresql.Driver -Dmotech.sql.dbtype=psql -Dmotech.sql.url=jdbc:postgresql://localhost:5432/ clean install -PIT -U -q
     fi
 fi
